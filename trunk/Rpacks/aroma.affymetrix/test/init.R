8fc2a5639294af2d7f5ac61be9fc9edfdf0babaf
hb <unknown@noreply.com>
2006-08-25 19:37:45 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/Rpacks/aroma.affymetrix/test/init.R
@@ -0,0 +1,25 @@
+savehistory();
+closeAllConnections();
+
+library(R.oo)
+library(R.utils)
+library(affxparser)
+library(aroma.apd)
+library(aroma.affymetrix)
+
+# Patching during development
+#source("~/braju.com.R/affxparser/affxparser/R/updateCel.R")
+#source("~/braju.com.R/affxparser/affxparser/R/private.readCelHeaderV4.R")
+
+source("~/braju.com.R/aroma.affymetrix/aroma.affymetrix/R/AffymetrixFile.R")
+source("~/braju.com.R/aroma.affymetrix/aroma.affymetrix/R/AffymetrixFileSet.R")
+source("~/braju.com.R/aroma.affymetrix/aroma.affymetrix/R/AffymetrixCdfFile.R")
+source("~/braju.com.R/aroma.affymetrix/aroma.affymetrix/R/AffymetrixCelFile.R")
+source("~/braju.com.R/aroma.affymetrix/aroma.affymetrix/R/AffymetrixCelSet.R")
+
+source("~/braju.com.R/aroma.affymetrix/aroma.affymetrix/R/ParameterFile.R")
+source("~/braju.com.R/aroma.affymetrix/aroma.affymetrix/R/ParameterCelFile.R")
+source("~/braju.com.R/aroma.affymetrix/aroma.affymetrix/R/LiWongProbeAffinityFile.R")
+
+source("~/braju.com.R/aroma.affymetrix/aroma.affymetrix/R/AffymetrixUnitGroupsModel.R")
+source("~/braju.com.R/aroma.affymetrix/aroma.affymetrix/R/AffymetrixLiWongModel.R")
