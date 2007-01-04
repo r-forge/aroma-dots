4e4654c7d9669bea49fd23db0e4edee669179b0e
hb <unknown@noreply.com>
2007-01-04 12:09:05 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@267 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/ChromosomeExplorer.R
@@ -0,0 +1,200 @@
+###########################################################################/**
+# @RdocClass ChromosomeExplorer
+#
+# @title "The ChromosomeExplorer class"
+#
+# \description{
+#  @classhierarchy
+#
+# }
+# 
+# @synopsis
+#
+# \arguments{
+#   \item{model}{A @see "CopyNumberModel" object.}
+#   \item{...}{Not used.}
+# }
+#
+# \section{Fields and Methods}{
+#  @allmethods "public"
+# }
+#
+# @author
+# 
+# \seealso{
+#  @see "GladModel".
+# }
+#*/###########################################################################
+setConstructorS3("ChromosomeExplorer", function(model=NULL, tags="*", ...) {
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Argument 'model':
+  if (!is.null(model)) {
+    if (!inherits(model, "MultiGladModel")) {
+        throw("Argument 'model' is not a 'MultiGladModel': ", class(model));
+    }
+  }
+
+  # Argument 'tags':
+  if (!is.null(tags)) {
+    tags <- Arguments$getCharacters(tags);
+    tags <- trim(unlist(strsplit(tags, split=",")));
+
+    # Update default tags
+    tags[tags == "*"] <- "";
+  }
+
+
+  extend(Object(), "ChromosomeExplorer",
+    .model = model
+  )
+})
+
+
+setMethodS3("as.character", "ChromosomeExplorer", function(this, ...) {
+  s <- sprintf("%s:", class(this)[1]);
+  s <- c(s, paste("Name:", getName(this)));
+  s <- c(s, paste("Tags:", paste(getTags(this), collapse=",")));
+  s <- c(s, sprintf("Path: %s", getPath(this)));
+  s <- c(s, sprintf("RAM: %.2fMb", objectSize(this)/1024^2));
+  class(s) <- "GenericSummary";
+  s;
+})
+
+
+setMethodS3("getModel", "ChromosomeExplorer", function(this, ...) {
+  this$.model;
+})
+
+setMethodS3("nbrOfArrays", "ChromosomeExplorer", function(this, ...) {
+  model <- getModel(this);
+  nbrOfArrays(model);
+})
+
+setMethodS3("getArrays", "ChromosomeExplorer", function(this, ...) {
+  model <- getModel(this);
+  getArrays(model, ...);
+})
+
+
+setMethodS3("getName", "ChromosomeExplorer", function(this, collapse="+", ...) {
+  model <- getModel(this);
+  getName(model);
+})
+
+
+
+setMethodS3("getTags", "ChromosomeExplorer", function(this, ...) {
+  model <- getModel(this);
+  tags <- getTags(model);
+  tags <- c(tags, this$.tags);
+  tags <- unique(tags);
+  tags;
+})
+
+
+setMethodS3("getFullName", "ChromosomeExplorer", function(this, ...) {
+  name <- getName(this);
+  tags <- getTags(this);
+  fullname <- paste(c(name, tags), collapse=",");
+  fullname <- gsub("[,]$", "", fullname);
+  fullname;
+})
+
+
+setMethodS3("getRootPath", "ChromosomeExplorer", function(this, ...) {
+  "ce";
+})
+
+setMethodS3("getPath", "ChromosomeExplorer", function(this, ...) {
+  # Create the (sub-)directory tree for the dataset
+
+  # Root path
+  rootPath <- getRootPath(this);
+  mkdirs(rootPath);
+
+  # Full name
+  name <- getName(this);
+
+  # Tags
+  tags <- getTags(this);
+  tags <- paste(tags, collapse=",");
+
+  # Chip type    
+  model <- getModel(this);
+  chipType <- getChipType(model);
+
+  # The full path
+  path <- filePath(rootPath, name, tags, chipType, expandLinks="any");
+  if (!isDirectory(path)) {
+    mkdirs(path);
+    if (!isDirectory(path))
+      throw("Failed to create output directory: ", path);
+  }
+
+  path;
+})
+
+
+###########################################################################/**
+# @RdocMethod getChromosomes
+#
+# @title "Gets the chromosomes available"
+#
+# \description{
+#  @get "title".
+# }
+#
+# @synopsis
+#
+# \arguments{
+#   \item{...}{Not used.}
+# }
+#
+# \value{
+#  Returns a @character @vector.
+# }
+#
+# @author
+#
+# \seealso{
+#   @seeclass
+# }
+#*/###########################################################################
+setMethodS3("getChromosomes", "ChromosomeExplorer", function(this, ...) {
+  model <- getModel(this);
+  getChromosomes(model);
+})
+
+
+setMethodS3("writeGraphs", "ChromosomeExplorer", function(x, ...) {
+  # To please R CMD check.
+  this <- x;
+
+  path <- getPath(this);
+  model <- getModel(this);
+  plot(model, path=path, imageFormat="png", ...);
+
+  invisible(path);
+}, protected=TRUE)
+
+setMethodS3("writeRegions", "ChromosomeExplorer", function(this, ...) {
+  model <- getModel(this);
+  writeRegions(model, ...);
+}, protected=TRUE)
+
+
+setMethodS3("process", "ChromosomeExplorer", function(this, ...) {
+  model <- getModel(this);
+  fit(model, ...);
+  writeGraphs(this, ...);
+  writeRegions(this, ...);
+})
+
+
+##############################################################################
+# HISTORY:
+# 2007-01-04
+# o Created.
+##############################################################################
