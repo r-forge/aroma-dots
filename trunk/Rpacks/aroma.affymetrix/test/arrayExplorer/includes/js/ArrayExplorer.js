e71b505d80ee5f1e56515e5324895f9b989c062e
hb <unknown@noreply.com>
2007-01-28 16:28:03 +0000

    o Added trial version of ArrayExplorer.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@311 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/Rpacks/aroma.affymetrix/test/arrayExplorer/includes/js/ArrayExplorer.js
@@ -0,0 +1,234 @@
+function ArrayExplorer() {
+  this.showIndicator = function(state) {
+    var statusImage = document.getElementById('statusImage');
+    if (state) {
+      statusImage.style.visibility = 'visible';
+    } else {
+      statusImage.style.visibility = 'hidden';
+    }
+  }
+
+  this.setStatus = function(state) {
+    if (state == "") {
+      this.showIndicator(false);
+      this.image2d.image.style.filter = "alpha(opacity=100)";
+      this.image2d.image.style.opacity = 1.0;
+    } else if (state == "wait") {
+      this.showIndicator(true);
+      this.image2d.image.style.filter = "alpha(opacity=50)";
+      this.image2d.image.style.opacity = 0.50;
+    }
+  }
+
+  this.getImageUrl = function() {
+    var imgName = this.sample + "," + this.colorMap + ".png";
+    var pathname = this.chipType + '/' + imgName;
+    return(pathname);
+  }
+
+  this.updateImage = function() {
+    var pathname = this.getImageUrl();
+    this.loadCount = 2;
+    this.setStatus('wait');
+    this.nav2d.setImage(pathname);
+    this.image2d.setImage(pathname);
+
+    /* Update the image link */
+    var imageUrl = document.getElementById('imageUrl');
+    imageUrl.href = pathname;
+    updateText(imageUrl, pathname);
+
+    /* Update the title of the page */
+    var title = location.href;
+    title = title.substring(0, title.lastIndexOf('\/'));
+    title = title.substring(title.lastIndexOf('\/')+1);
+    title = title + '/' + pathname;
+    document.title = title;
+	}
+
+  this.decreaseLoadCount = function() {
+    this.loadCount = this.loadCount - 1;
+    if (this.loadCount <= 0) {
+      this.loadCount = 0;
+      this.setStatus("");
+    }
+  }
+
+  this.setColorMap = function(map) {
+    if (this.colorMap == map)
+      return(false);
+
+    clearById('colorMap' + this.colorMap);
+    highlightById('colorMap' + map);
+    this.colorMap = map;
+    this.updateImage();
+    return(true);
+  }
+
+  this.setChipType = function(chipType) {
+    if (this.chipType == chipType)
+      return(false);
+
+    clearById('chipType' + this.chipType);
+    highlightById('chipType' + chipType);
+    this.chipType = chipType;
+    return(true);
+  }
+
+  this.setSample = function(sample) {
+    if (this.sample == sample)
+      return(false);
+
+    clearById('sample' + this.sample);
+    highlightById('sample' + sample);
+    updateLabel('sampleLabel', sample);
+    this.sample = sample;
+    return(true);
+  }
+
+  this.setScale = function(scale) {
+    if (this.scale == scale)
+      return(false);
+
+    clearById('zoom' + this.scale);
+    this.scale = scale;
+    this.nav2d.setSize(1/scale);
+    this.image2d.setSize(scale);
+    highlightById('zoom' + scale);
+    return(true);
+  }
+
+  this.setSamples = function(samples) {
+    this.samples = samples;
+    if (samples.length > 1) {
+      var s = 'Samples: ';
+      for (var kk=0; kk < samples.length; kk++) {
+        var sample = samples[kk];
+        var name = sample;
+        if (this.aliases != null)
+          name = this.aliases[kk];
+        s = s + '[<span id="sample' + sample + '"><a href="javascript:changeSample(\'' + sample + '\');">' + name + '</a></span>]<span style="font-size:1%"> </span>';
+      }
+      s = s + ' ';
+      updateLabel('samplesLabel', s);
+    }
+  }
+
+  this.setAliases = function(aliases) {
+    this.aliases = aliases;
+  }
+
+  this.setChipTypes = function(chipTypes) {
+    this.chipTypes = chipTypes;
+
+    if (chipTypes.length > 1) {
+      var s = 'Chip types: ';
+      for (var kk=0; kk < chipTypes.length; kk++) {
+        var chipType = chipTypes[kk];
+        s = s + '[<span id="chipType' + chipType + '"><a href="javascript:changeChipType(\'' + chipType + '\');">' + chipType + '</a></span>]'; 
+      }
+      s = s + '<br>';
+      updateLabel('chipTypeLabel', s);
+    }
+  }
+
+
+  this.setScales = function(scales) {
+    function padWidthZeros(x, width) {
+      var str = "" + x;
+      while (width - str.length > 0)
+        str = "0" + str;
+      return(str);
+    }
+   
+    this.scales = scales;
+    var zWidth = Math.round(Math.log(Math.max(scales)) / Math.log(10) + 0.5);
+    var s = 'Zoom: ';
+    for (var kk=0; kk < scales.length; kk++) {
+      var scale = scales[kk];
+      s = s + '[<span id="zoom' + scale + '"><a href="javascript:changeZoom(' + scale + ');">x' + padWidthZeros(scale, zWidth) + '</a></span>]'; 
+    }
+    s = s + '<br>';
+    updateLabel('zoomLabel', s);
+  }
+
+  this.setColorMaps = function(colorMaps) {
+    this.colorMaps = colorMaps;
+
+    if (colorMaps.length > 1) {
+      var s = 'Color map: ';
+      for (var kk=0; kk < colorMaps.length; kk++) {
+        var colorMap = colorMaps[kk];
+        s = s + '[<span id="colorMap' + colorMap + '"><a href="javascript:changeColorMap(\'' + colorMap + '\');">' + colorMap + '</a></span>]'; 
+      }
+      s = s + '<br>';
+      updateLabel('colorMapLabel', s);
+    }
+  }
+
+  this.samples = new Array();
+  this.aliases = null;
+  this.chipTypes = new Array();
+  this.colorMaps = new Array();
+  this.scales = new Array();
+
+  this.loadCount = 0;
+  this.scale = 1;
+  this.sample = '';
+  this.chipType = '';
+  this.colorMap = '';
+
+  this.setupEventHandlers = function() {
+    var owner = this;
+
+    this.nav2d.onLoad = function() {
+      owner.decreaseLoadCount();
+    }
+
+    this.image2d.onLoad = function() {
+      owner.decreaseLoadCount();
+    }
+
+    this.nav2d.onScroll = function() {
+	  	owner.image2d.setRelXY(this.x, this.y);
+      var pos = owner.image2d.getXY();
+      updateLabel('nav2dInfo', '('+Math.round(pos.x)+','+Math.round(pos.y)+')');
+		  owner.image2d.update();
+      return(false);    
+	  }
+
+    this.image2d.onScroll = function() {
+      var pos = this.getXY();
+      updateLabel('nav2dInfo', '('+Math.round(pos.x)+','+Math.round(pos.y)+')');
+      owner.nav2d.setRelXY(this.x, this.y);
+      owner.nav2d.update();
+      return(false);    
+	  }
+  }
+
+  this.update = function() {
+    this.updateImage();
+  }
+
+  this.onLoad = function() { }
+
+  this.start = function() {
+    this.nav2d = new Scrollbar2d("nav2d");
+    this.image2d = new ScrollImage2d("panel");
+    this.setupEventHandlers();
+
+    /* Default settings */
+    this.setScales(new Array('1', '2', '4', '8', '16'));
+    this.setColorMaps(new Array('gray'));
+
+    this.onLoad();
+
+    this.setSample(this.samples[0]);
+    this.setScale(this.scales[0]);
+    this.setChipType(this.chipTypes[0]);
+    this.setColorMap(this.colorMaps[0]);
+
+    this.update();
+  }
+} /* ArrayExplorer() */
+
---
+++ trunk/Rpacks/aroma.affymetrix/test/arrayExplorer/includes/js/ScrollImage2d.js
@@ -0,0 +1,293 @@
+/****************************************************************
+ *
+ ****************************************************************/
+function Scrollbar2d(id) {
+  this.setImage = function(url) {
+    var owner = this;
+
+    /* Define onload() function */
+    this.image.onload = function() {
+      owner.onLoad();
+      owner.update();
+      owner.imageIsLoaded = true;
+    }
+
+    /* Define onload() function */
+    this.image.onerror = function() {
+      alert('Image not loaded: ' + url);
+    }
+
+    /* Start loading image */
+    this.image.src = url;
+  }
+
+  this.getImageWidth = function() {
+    return(this.image.width-6);
+  }
+
+  this.getImageHeight = function() {
+    return(this.image.height-6);
+	}
+
+  this.update = function() {
+    var pos = findXY(this.image);
+    this.xOffset = pos.x;
+    this.yOffset = pos.y;
+    var w = this.getImageWidth();
+    var h = this.getImageHeight();
+    this.marker.style.width = Math.round(w*this.width) + "px";
+    this.marker.style.height = Math.round(h*this.height) + "px";
+    this.marker.style.left = Math.round(this.xOffset + w*this.x) + "px";
+    this.marker.style.top = Math.round(this.yOffset + h*this.y) + "px";
+    this.marker.style.border = 'solid; black; 2px';
+  }
+
+  this.setRelXY = function(x,y) {
+    x = Math.max(0, x);
+    x = Math.min(x, 1-this.width);
+    y = Math.max(0, y);
+    y = Math.min(y, 1-this.height);
+    this.x = x;
+    this.y = y;
+  }
+
+  this.setSize = function(scale) {
+    var xMid = this.x + this.width/2;
+    var yMid = this.y + this.height/2;
+    this.setRelDimension(scale, scale);
+    this.setRelXY(xMid - this.width/2, yMid - this.height/2);
+    this.update();
+  }
+
+  this.setRelDimension = function(width, height) {
+    width = Math.max(0, width);
+    height = Math.max(0, height);
+    width = Math.min(width, 1);
+    height = Math.min(height, 1);
+    this.width = width;
+    this.height = height;
+  }
+
+  this.setCursor = function(status) {
+    this.marker.style.cursor = status;
+    this.container.style.cursor = status;
+    this.image.style.cursor = status;
+  }
+
+  this.setupEventHandlers = function() {
+    var owner = this;
+
+    var containerOnClick = function() {
+      var w = owner.getImageWidth();
+      var h = owner.getImageHeight();
+      var e = arguments[0] || event;
+      var pos = findXY(owner.marker);
+      var dx = (e.clientX - pos.x)/w - owner.width;
+  		var dy = (e.clientY - pos.y)/h - owner.height;
+			owner.setRelXY(dx,dy);
+      owner.update();
+    }
+
+		/*
+    this.container.onclick = containerOnClick;
+    */
+
+    this.marker.onmousedown = function() {
+      var x0 = owner.x;
+      var y0 = owner.y;
+      var w = owner.getImageWidth();
+      var h = owner.getImageHeight();
+      var e = arguments[0] || event;
+      var xStart = e.clientX;
+      var yStart = e.clientY;
+      owner.setCursor('move');
+      owner.onScrollBegin();
+  
+      document.onmousemove = function() {
+        var e = arguments[0] || event;
+        var dx = (e.clientX - xStart)/w;
+        var dy = (e.clientY - yStart)/h;
+        owner.setRelXY(x0+dx, y0+dy);
+        owner.update();
+        owner.onScroll();
+        return false;
+      }
+  
+      document.onmouseup = function() {
+        document.onmousemove = null;
+        owner.setCursor('default');
+        owner.onScrollEnd();
+        return false;
+      }
+  
+      return false;
+    }
+  }
+
+  this.onLoad = function() {}
+  this.onScrollBegin = function() {}
+  this.onScroll = function() {}
+  this.onScrollEnd = function() {}
+
+
+  /* Initialize */
+  this.x = 0;
+  this.y = 0;
+  this.xOffset = 0;
+  this.yOffset = 0;
+  this.width = 1;
+  this.height = 1;
+  
+  this.container = document.getElementById(id);
+  this.image = document.getElementById(id + 'Image');
+  this.imageIsLoaded = false;
+  this.marker = document.getElementById(id + 'Marker');
+
+  this.setupEventHandlers();
+} /* Scrollbar2d() */
+
+
+
+
+/****************************************************************
+ *
+ ****************************************************************/
+function ScrollImage2d(id) {
+  this.setImage = function(url) {
+    var owner = this;
+
+    /* Define onload() function */
+    this.image.onload = function() {
+      owner.onLoad();
+      owner.update();
+      owner.imageIsLoaded = true;
+    }
+
+    /* Define onload() function */
+    this.image.onerror = function() {
+      alert('Image not loaded: ' + url);
+    }
+
+    /* Start loading image */
+    this.image.src = url;
+  }
+
+  this.getImageWidth = function() {
+    return(this.image.width);
+  }
+
+  this.getImageHeight = function() {
+  	return(this.image.height);
+	}
+
+  this.update = function() {
+    this.container.scrollLeft = this.x * this.container.scrollWidth;
+    this.container.scrollTop = this.y * this.container.scrollHeight;
+    var pos = findXY(this.image);
+    this.xOffset = pos.x;
+    this.yOffset = pos.y;
+    var w = this.getImageWidth();
+    var h = this.getImageHeight();
+    this.image.style.left = Math.round(this.xOffset + w*this.x) + "px";
+    this.image.style.top = Math.round(this.yOffset + h*this.y) + "px";
+    this.image.width = this.width * this.container.clientWidth;
+    this.image.height = this.height * this.container.clientHeight;
+    this.container.style.height = this.container.clientWidth;
+  }
+
+  this.getXY = function() {
+    var w = this.getImageWidth()/this.width;
+    var h = this.getImageHeight()/this.height;
+    var res = Object();
+    res.x = w*this.x;
+    res.y = w*this.y;
+    return(res);
+  }
+
+  this.setRelXY = function(x,y) {
+    x = Math.max(0, x);
+    x = Math.min(x, 1);
+    y = Math.max(0, y);
+    y = Math.min(y, 1);
+    this.x = x;
+    this.y = y;
+  }
+
+  this.setSize = function(scale) {
+    this.setRelDimension(scale, scale);
+    this.update();
+  }
+
+  this.setRelDimension = function(width, height) {
+    width = Math.max(0, width);
+    height = Math.max(0, height);
+    this.width = width;
+    this.height = height;
+  }
+
+  this.setCursor = function(status) {
+    this.container.style.cursor = status;
+    this.image.style.cursor = status;
+  }
+
+  this.setupEventHandlers = function() {
+    var owner = this;
+
+    this.image.onmousedown = function() {
+      var e = arguments[0] || event;
+      var x0 = owner.x;
+      var y0 = owner.y;
+      var w = owner.getImageWidth();
+      var h = owner.getImageHeight();
+      var xStart = e.clientX;
+      var yStart = e.clientY;
+      var x = owner.container.scrollLeft + e.clientX;
+      var y = owner.container.scrollTop + e.clientY;
+      owner.setCursor("move");
+      owner.image.onmousemove = null;
+      owner.onScrollBegin();
+  
+      document.onmousemove = function() {
+        var e = arguments[0] || event;
+        var dx = (e.clientX - xStart)/w;
+        var dy = (e.clientY - yStart)/h;
+        owner.setRelXY(x0-dx, y0-dy);
+        owner.update();
+        owner.onScroll();
+        return false;
+      }
+  
+      document.onmouseup = function() {
+        document.onmousemove = null;
+        owner.image.onmousemove = null;
+        owner.setCursor("default");
+        owner.onScrollEnd();
+        return false;
+      }
+  
+      return false;
+    }
+  }
+
+  this.onLoad = function() {}
+  this.onScrollBegin = function() {}
+  this.onScroll = function() {}
+  this.onScrollEnd = function() {}
+
+
+  /* Initialize */
+  this.scale = 1;
+  this.x = 0;
+  this.y = 0;
+  this.xOffset = 0;
+  this.yOffset = 0;
+  this.width = 1;
+  this.height = 1;
+  
+  this.container = document.getElementById(id);
+  this.image = document.getElementById(id + 'Image');
+  this.imageIsLoaded = false;
+
+  this.setupEventHandlers();
+} /* ScrollImage2d() */
+
