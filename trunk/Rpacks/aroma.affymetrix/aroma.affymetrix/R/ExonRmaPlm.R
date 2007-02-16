6f23492d4d370cf3c8c384870f3928c2e5efe060
ksimpson <unknown@noreply.com>
2007-02-16 11:33:54 +0000

    Modified exon array related code.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@377 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/ExonRmaPlm.R
+++ trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/ExonRmaPlm.R
@@ -60,21 +60,21 @@ setConstructorS3("ExonRmaPlm", function(..., tags="*", mergeGroups=TRUE) {
   
 })
 
+# utility function - keep here for now
+
+cdfMergeGroups <- function(groups, ...) {
+  nbrOfGroups <- length(groups);
+  
+  nbrOfFields <- length(.subset2(groups,1));
+  newGroup <- vector("list", nbrOfFields);
+  for (ff in seq(length=nbrOfFields)) {
+    newGroup[[ff]] <- unlist(lapply(groups, .subset2, ff), use.names=FALSE);
+  }
+  names(newGroup) <- names(.subset2(groups,1));
+  return(list(newGroup));
+}
 
 setMethodS3("getCellIndices", "ExonRmaPlm", function(this, ...) {
-
-# internal functions  
-  cdfMergeGroups <- function(groups, ...) {
-    nbrOfGroups <- length(groups);
-
-    nbrOfFields <- length(.subset2(groups,1));
-    newGroup <- vector("list", nbrOfFields);
-    for (ff in seq(length=nbrOfFields)) {
-      newGroup[[ff]] <- unlist(lapply(groups, .subset2, ff), use.names=FALSE);
-    }
-    names(newGroup) <- names(.subset2(groups,1));
-    return(list(newGroup));
-  }
 
   cells <- NextMethod("getCellIndices", this, ...);
 
@@ -111,4 +111,132 @@ setMethodS3("setMergeGroups", "ExonRmaPlm", function(this, ...) {
   setMergeGroups(paf, ...);
 })
 
+
+###########################################################################/**
+# @RdocMethod getFitFunction
+#
+# @title "Gets the low-level function that fits the Exon PLM"
+#
+# \description{
+#  @get "title".
+# }
+#
+# @synopsis
+#
+# \arguments{
+#   \item{...}{Not used.}
+# }
+#
+# \value{
+#  Returns a @function.
+# }
+#
+# \author{
+#   Henrik Bengtsson and Ken Simpson (WEHI) utilizing Ben Bolstad's 
+#   \pkg{affyPLM} package.
+# }
+#
+# \seealso{
+#   @seeclass
+# }
+#*/###########################################################################
+setMethodS3("getFitFunction", "ExonRmaPlm", function(this, ...) {
+  # This should not be need, but for some reason is the package not loaded
+  # although it is listed in DESCRIPTION. /HB 2007-02-09
+  require(affyPLM) || throw("Package not loaded: affyPLM");
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # rmaModelAffyPlm()
+  # Author: Henrik Bengtsson, UC Berkeley. 
+  # Requires: affyPLM() by Ben Bolstad.
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  exonRmaModel <- function(y, psiCode=0, psiK=1.345, useMedianPolish=FALSE, medianPolishThreshold=100){
+    # Assert right dimensions of 'y'.
+
+    # If input data are dimensionless, return NAs. /KS 2006-01-30
+    if (is.null(dim(y))) {
+      nbrOfArrays <- nbrOfArrays(getDataSet(this));
+      return(list(theta=rep(NA, nbrOfArrays),
+                  sdTheta=rep(NA, nbrOfArrays),
+                  thetaOutliers=rep(NA, nbrOfArrays), 
+                  phi=c(), 
+                  sdPhi=c(), 
+                  phiOutliers=c()));
+      
+    }
+
+    if (length(dim(y)) != 2) {
+      str(y);
+      stop("Argument 'y' must have two dimensions: ", 
+                                                paste(dim(y), collapse="x"));
+    }
+
+    # Log-additive model
+    y <- log(y, base=2);
+
+    # do we want to use median polish for large matrices?
+
+    if (useMedianPolish) {
+      nbrOfVariables <- dim(y)[1]+dim(y)[2]-1;
+      if (nbrOfVariables > medianPolishThreshold) {
+        mp <- medpolish(y, trace.iter=FALSE);
+        fit <- list(Estimates=c(mp$overall+mp$col, mp$row), StdErrors=rep(0, length(c(mp$row,mp$col))));
+      } else {    
+      # Fit model using affyPLM code
+        fit <- .Call("R_rlm_rma_default_model", y, psiCode, psiK, PACKAGE="affyPLM");
+      }
+    } else {
+      fit <- .Call("R_rlm_rma_default_model", y, psiCode, psiK, PACKAGE="affyPLM");      
+    }
+        
+    # Extract probe affinities and chip estimates
+    J <- ncol(y);  # Number of arrays
+    I <- nrow(y);  # Number of probes
+    est <- fit$Estimates;
+    se <- fit$StdErrors;
+
+    # Chip effects
+    beta <- est[1:J];
+
+    # Probe affinities
+    alpha <- est[(J+1):length(est)];
+    alpha[length(alpha)] <- -sum(alpha[1:(length(alpha)-1)]);
+
+    # Estimates on the intensity scale
+    theta <- 2^beta;
+    phi <- 2^alpha;
+
+    # The RMA model is fitted with constraint sum(alpha) = 0, that is,
+    # such that prod(phi) = 1.
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    # A fit function must return: theta, sdTheta, thetaOutliers, 
+    # phi, sdPhi, phiOutliers.
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    if (is.null(se)) {
+      # For affyPLM v1.10.0 (2006-09-26) or older.
+      sdTheta <- rep(1, J);
+      sdPhi <- rep(1, I);
+    } else {
+      # For affyPLM v1.11.6 (2006-11-01) or newer.
+      sdTheta <- 2^(se[1:J]);
+      sdPhi <- 2^(se[(J+1):length(se)]);
+    }
+    thetaOutliers <- rep(FALSE, J);
+    phiOutliers <- rep(FALSE, I);
+
+    # Return data on the intensity scale
+    list(theta=theta, sdTheta=sdTheta, thetaOutliers=thetaOutliers, 
+         phi=phi, sdPhi=sdPhi, phiOutliers=phiOutliers);   
+  } # rmaModelAffyPlm()
+  attr(exonRmaModel, "name") <- "exonRmaModel";
+
+  exonRmaModel;
+}, private=TRUE)
+
+
+setMethodS3("findUnitsTodo", "ExonRmaPlm", function(this, ...) {
+  eces <- getChipEffects(this);
+  findUnitsTodo(eces, ...);
+}, private=TRUE)
 
