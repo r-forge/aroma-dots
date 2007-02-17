f96a48b7294008b4e995b98838eeb4712d3e53f0
hb <unknown@noreply.com>
2007-02-17 04:33:36 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@387 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/getCommonListElements.R
@@ -0,0 +1,47 @@
+getCommonListElements <- function(lst, ignoreEmpty=TRUE) {
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # 1. Scan list for common elements
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Vector to store common elements
+  common <- c();
+  n <- length(lst);
+  first <- TRUE;
+  for (kk in seq_len(n)) {
+    value <- lst[[kk]];
+
+    # Ignoring empty list elements or not?
+    if (is.null(value)) {
+      if (ignoreEmpty)
+        next;
+
+      # We know for sure that the rest will be empty too
+      common <- c();
+      break;
+    }
+
+    if (first) {
+      common <- value;
+      first <- FALSE;
+    } else {
+      common <- intersect(common, value);
+
+      # Done?
+      if (length(common) == 0)
+        break;     
+    }
+  } # for (kk ...)
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # 2. Keep only common elements
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  lst <- lapply(lst, FUN=intersect, common);
+
+  lst;
+} # getCommonListElements()
+
+
+############################################################################
+# HISTORY:
+# 2007-02-16
+# o Created.
+############################################################################
