34488856877a3cfdfd32bcd5ed9acf29e688f431
hb <unknown@noreply.com>
2007-02-28 06:45:17 +0000

    o BUG FIX: process() of ChromosomeExplorer would give an error saying 'Error in file(file, ifelse(append, "a", "w")) ... cannot open file /*****************...'.  This was due to the update from appendVarArgs(write) to appendVarArgs(base::write).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@412 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/006.fixVarArgs.R
+++ trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/006.fixVarArgs.R
@@ -1,15 +1,20 @@
 # Added '...' to some base functions. These will later be
 # turned into default functions by setMethodS3().
 
-colSums <- appendVarArgs(base::colSums)
-colMeans <- appendVarArgs(base::colMeans)
-write <- appendVarArgs(base::write)
-append <- appendVarArgs(base::append)
-getPackageName <- appendVarArgs(methods::getPackageName)
+colSums <- appendVarArgs(colSums)
+colMeans <- appendVarArgs(colMeans)
+write <- appendVarArgs(write)
+append <- appendVarArgs(append)
+getPackageName <- appendVarArgs(getPackageName)
 
 
 ############################################################################
 # HISTORY:
+# 2007-02-27 [HB]
+# o BUG FIX: Removed explicit reference to 'base' etc again. The reason is 
+#   that if a previous package already modified, say, write(), to become a 
+#   generic function, that was overwritten again when this package was 
+#   loaded.
 # 2007-02-23 [KS]
 # o Make explicit reference to 'base' - this is safer, in case of colMeans()
 #   defined by user or other packages.
