87eedf52aacd4646560e0e84dfa08a5da50daab4
hb <hb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-03-07 22:32:33 +0000

    o Patches digest() so that it gives the same result regardless of R version running.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@435 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/digest.faster.R
+++ trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/digest.faster.R
@@ -5,12 +5,13 @@ digest <- function(object, algo=c("md5", "sha1", "crc32"), serialize=TRUE, file=
 
   if (serialize && !file) {
     object <- serialize(object, connection=NULL, ascii=TRUE);
-#    object <- paste(object, collapse="");
+    # Exclude serialize "header", because it looks like it contains
+    # R version specific information.
+    object <- object[-(1:10)];
     object <- rawToChar(object);
   } else if (!is.character(object)) {
     stop("Argument object must be of type character if serialize is FALSE");
   }
-  object <- as.character(object);
   algoint <- switch(algo, md5=1, sha1=2, crc32=3);
   if (file) {
     algoint <- algoint + 100;
@@ -26,6 +27,12 @@ digest <- function(object, algo=c("md5", "sha1", "crc32"), serialize=TRUE, file=
 
 ############################################################################
 # HISTORY:
+# 2007-03-07
+# o BUG FIX: serialize() gives different results depending on R version.
+#   The difference seems to be in raw bytes 8, 9 and 10.  In other words,
+#   if those are excluded before passing the stream on to the "digester"
+#   we get the same results.  To be on the safe side, we exclude the
+#   first ten raw bytes.
 # 2007-01-06
 # o Created.
 ############################################################################
