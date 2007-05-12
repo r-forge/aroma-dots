9d472466c1cea74f889c2c59f0f1b7c7e1a5cdaa
hb <hb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-05-12 00:53:43 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@531 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/Rpacks/aroma.apd/aroma.apd/R/cdfToApdMap.R
@@ -0,0 +1,92 @@
+#########################################################################/**
+# @RdocDefault cdfToApdMap
+#
+# @title "Generates an APD read map file from an Affymetrix CDF file"
+#
+# @synopsis 
+# 
+# \description{
+#   @get "title".
+# }
+# 
+# \arguments{
+#   \item{filename}{The pathname of the CDF file.}
+#   \item{mapType}{A @character string naming the type of the map.
+#     If @NULL, the chip type will be used.}
+#   \item{mapFile}{The filename of the resulting APD map file. If @NULL,
+#     the map type with filename extension \code{apm} will be used.}
+#   \item{mapPath}{An optional path where to the map file will be stored.}
+#   \item{...}{Additional arguments passed to
+#      @see "affxparser::readCdfUnitsWriteMap", e.g. \code{units}.}
+#   \item{verbose}{A @see "R.utils::Verbose" object.}
+# }
+# 
+# \value{
+#   Returns (invisibly) a @list structure with elements:
+#   \itemize{
+#    \item{pathname}{The pathname of the generated APD map file.}
+#    \item{mapType}{The map type @character string.}
+#    \item{chipType}{The chip type @character string.}
+#    \item{readMap}{The generated read map @integer @vector.}
+#   }
+# }
+#
+# @author
+# 
+# \seealso{
+#   To read an APD map file, see @see "readApdMap".
+# }
+# 
+# @keyword "file"
+# @keyword "IO"
+#*/#########################################################################
+setMethodS3("cdfToApdMap", "default", function(filename, mapType=NULL, mapFile=NULL, mapPath=NULL, ..., verbose=FALSE) {
+  # Argument 'filename':
+  filename <- Arguments$getReadablePathname(filename, mustExist=TRUE);
+
+  # Get chip type from CDF header
+  header <- readCdfHeader(filename);
+  chipType <- header$chiptype; 
+
+  # Argument 'mapType':
+  if (is.null(mapType)) {
+    mapType <- chipType;   
+  }
+
+  # Argument 'mapFile':
+  if (is.null(mapFile)) {
+    mapFile <- paste(mapType, ".apm", sep="");
+  }
+
+  # Argument 'mapPath':
+  if (!is.null(mapPath)) {
+    mapFile <- file.path(mapPath, mapFile);
+  }
+
+  # Argument 'mapFile':
+  mapFile <- Arguments$getWritablePathname(mapFile, mustNotExist=TRUE);
+
+  verbose && enter(verbose, "Creating read map from CDF file");
+  writeMap <- readCdfUnitsWriteMap(filename, ...);
+  readMap <- invertMap(writeMap);
+  verbose && exit(verbose);
+
+ 
+  verbose && cat(verbose, "Saving map file '", mapFile, "'");
+  writeApdMap(mapFile, chipType=chipType, map=readMap);
+  verbose && exit(verbose);
+
+  invisible(list(
+    pathname=mapFile,
+    mapType=mapType,
+    chipType=chipType,
+    readMap=readMap
+  ));
+}) # cdfToApdMap()
+
+
+############################################################################
+# HISTORY:
+# 2006-03-30
+# o Created.
+############################################################################  
---
+++ trunk/Rpacks/aroma.apd/aroma.apd/R/writeApd.R
@@ -0,0 +1,81 @@
+#########################################################################/**
+# @RdocDefault writeApd
+#
+# @title "Writes an APD probe data file"
+#
+# @synopsis 
+# 
+# \description{
+#   @get "title".
+# }
+# 
+# \arguments{
+#   \item{filename}{The filename of the APD file.}
+#   \item{data}{A @numeric @vector of elements to be written.}
+#   \item{...}{Arguments passed to @see "createApd", e.g. \code{chipType}, 
+#      \code{mapType} etc.}
+#   \item{writeMap}{A @vector of indicies used to change the order how
+#      data elements are \emph{written} (by default).}
+# }
+# 
+# \value{
+#   Returns (invisibly) the pathname to the created file.
+# }
+#
+# @author
+# 
+# \seealso{
+#   To create an APD map file, see @see "readApdMap".
+# }
+# 
+# @keyword "file"
+# @keyword "IO"
+#*/#########################################################################
+setMethodS3("writeApd", "default", function(filename, data, ..., writeMap=NULL) {
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Argument 'filename':
+  filename <- Arguments$getWritablePathname(filename, mustNotExist=TRUE);
+
+  # Argument 'data':
+  data <- Arguments$getNumerics(data);
+
+  nbrOfCells <- length(data);
+
+  # Argument 'writeMap':
+  if (!is.null(writeMap)) {
+    writeMap <- Arguments$getNumerics(writeMap, range=c(1,nbrOfCells));
+  }
+
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Reorder data elements according to the write map
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  if (!is.null(writeMap)) {
+    data <- data[writeMap];
+  }
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Write APD file
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Create the APD file
+  createApd(filename, nbrOfCells=nbrOfCells, ...);
+
+  # Write the values
+  apd <- FileVector(filename);
+  on.exit(close(apd));
+
+  writeAllValues(apd, data);
+
+  invisible(filename);
+})
+
+
+############################################################################
+# HISTORY:
+# 2006-03-28
+# o Renamed argument 'map' to 'writeMap'.  Removed argument 'isWriteMap'.
+# 2006-03-14
+# o Created.
+############################################################################  
