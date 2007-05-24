dd611d7fedbdb91670b0b8328ad21db3655d8beb
hb <unknown@noreply.com>
2007-05-24 22:08:11 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@534 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Rpacks/aroma.apd/aroma.apd/R/zzz.R
+++ trunk/Rpacks/aroma.apd/aroma.apd/R/zzz.R
@@ -3,10 +3,19 @@
 .conflicts.OK <- TRUE
 
 
-.First.lib <- function(libname, pkgname) {
+.onAttach <- function(libname, pkgname) {
   pkg <- Package(pkgname);
   assign(pkgname, pkg, pos=getPosition(pkg));
   cat(getName(pkg), " v", getVersion(pkg), " (", getDate(pkg), ")",
       " successfully loaded. See ?", pkgname, " for help.\n", sep="");
 }
+
+## .First.lib <- function(libname, pkgname) {
+##   pkg <- Package(pkgname);
+##   assign(pkgname, pkg, pos=getPosition(pkg));
+##   cat(getName(pkg), " v", getVersion(pkg), " (", getDate(pkg), ")",
+##       " successfully loaded. See ?", pkgname, " for help.\n", sep="");
+## }
+
+
 
