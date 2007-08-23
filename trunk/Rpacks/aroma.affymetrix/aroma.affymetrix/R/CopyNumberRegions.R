aee93dc68e26f2f642d07a832fc22f4c9ea9a5b7
hb <hb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-08-23 20:23:17 +0000

    Working on generic classes representing raw CNs and CNRs.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@572 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/CopyNumberRegions.R
@@ -0,0 +1,160 @@
+setConstructorS3("CopyNumberRegions", function(start=NULL, stop=NULL, mean=NULL, count=NULL, call=NULL, ...) {
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  extend(Object(), "CopyNumberRegions", 
+    start = start,
+    stop = stop,
+    mean = mean,
+    count = count,
+    call= call,
+    ...
+  )
+})
+
+
+setMethodS3("as.character", "CopyNumberRegions", function(x, ...) {
+  # To please R CMD check
+  this <- x;
+
+  s <- sprintf("%s:", class(this)[1]);
+  s <- c(s, sprintf("Number of regions: %d", nbrOfRegions(this)));
+  s <- c(s, sprintf("RAM: %.2fMB", objectSize(this)/1024^2));
+  class(s) <- "GenericSummary";
+  s;
+}, private=TRUE) 
+
+
+setMethodS3("nbrOfRegions", "CopyNumberRegions", function(this, ...) {
+  length(this$start);
+})
+
+
+setMethodS3("as.data.frame", "CopyNumberRegions", function(this, ...) {
+  data <- cbind(start=this$start, stop=this$stop, mean=this$mean, count=this$count, call=this$call);
+  data;
+})
+
+
+setMethodS3("applyRegions", "CopyNumberRegions", function(this, FUN, ...) {
+  data <- as.data.frame(this);
+  o <- order(data[,"start"]);
+  data <- data[o,];
+  apply(data, MARGIN=1, FUN=FUN);
+})
+
+
+setMethodS3("drawLevels", "CopyNumberRegions", function(this, col="red", lwd=2, lty=1, ...) {
+  col0 <- col;
+  lwd0 <- lwd;
+  lty0 <- lty;
+  applyRegions(this, FUN=function(cnr) {
+    x <- c(cnr["start"], cnr["stop"]);
+    y <- rep(cnr["mean"], times=2);
+    if (is.function(col0))
+      col <- col0(cnr);
+    if (is.function(lwd0))
+      lwd <- lwd0(cnr);
+    if (is.function(lty0))
+      lty <- lty0(cnr);
+    lines(x=x, y=y, col=col, lwd=lwd, lty=lty, ...);
+  })
+})
+
+
+
+setMethodS3("lines", "CopyNumberRegions", function(this, col="red", lwd=2, ...) {
+  data <- as.data.frame(this);
+  o <- order(data[,"start"]);
+  data <- data[o,];
+  xx <- t(data[,c("start", "stop")]);
+  yy <- rep(this$mean[o], each=2);
+  lines(x=xx, y=yy, col=col, lwd=lwd, ...);
+})
+
+
+
+setMethodS3("extractCNRs", "default", function(...) {
+  extractCopyNumberRegions(...);
+})
+
+
+setMethodS3("extractCopyNumberRegions", "profileCGH", function(object, ...) {
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  pv <- object$profileValues;
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Allocate result table
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Identify unique regions
+  uRegions <- unique(pv$Region);
+  nbrOfRegions <- length(uRegions);
+
+  # Columns
+  colClasses <- c(chromosome="character", start="integer", 
+                  stop="integer", mean="double", nbrOfLoci="integer",
+                  call="character");
+  df <- dataFrame(colClasses, nrow=nbrOfRegions);
+
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Extract each region
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  for (rr in seq(along=uRegions)) {
+    # Get the region ID
+    region <- uRegions[rr];
+
+    # Get the first and last position of each region
+    idx <- which(region == pv$Region);
+    idx <- idx[c(1,length(idx))];
+    idx1 <- idx[1];
+
+    # Chromosome
+    df[rr,"chromosome"] <- pv$Chromosome[idx1];
+
+    # (start, stop, length)
+    df[rr,c("start", "stop")] <- as.integer(pv$PosBase[idx]);
+
+    # Number of SNPs
+    df[rr,"nbrOfLoci"] <- as.integer(diff(idx)+1);
+
+    # Smoothing
+    df[rr,"mean"] <- pv$Smoothing[idx1];
+
+    # Call
+    df[rr,"call"] <- c("loss", "neutral", "gain")[pv$ZoneGNL[idx1]+2];
+  }
+
+
+  CopyNumberRegions(
+    start=df$start, 
+    stop=df$stop, 
+    mean=df$mean, 
+    count=df$nbrOfLoci,
+    call=df$call
+  );
+})
+
+
+setMethodS3("extractCopyNumberRegions", "DNAcopy", function(object, ...) {
+  output <- object$output;
+
+  CopyNumberRegions(
+    start=output[["loc.start"]], 
+    stop=output[["loc.end"]], 
+    mean=output[["seg.mean"]],
+    count=output[["num.mark"]]
+  );
+})
+
+
+
+
+############################################################################
+# HISTORY:
+# 2007-08-22
+# o Created.  Need a generic container for holding copy number regions and
+#   to plot them nicely.
+############################################################################
---
+++ trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/profileCGH.newPlot.R
@@ -0,0 +1,190 @@
+setMethodS3("drawCytoBand", "default", function(cytoband, chromosome=1, y=-1, labels=TRUE, height=1, colCytoBand=c("white", "darkblue"), colCentro="red", ...) {
+  opar <- par(xpd=NA);
+  on.exit(par(opar));
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Cytoband colors
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  color <- unique(cytoband$Col);
+  pal <- GLAD::myPalette(low=colCytoBand[1], high=colCytoBand[2], k=length(color));
+
+  info <- data.frame(Color=color, ColorName=I(pal));
+  cytoband <- merge(cytoband, info, by="Color");
+  rm(info);
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Extract cytoband information for current chromosome
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  keep <- which(cytoband$Chromosome == chromosome);
+  cytoband <- cytoband[keep, ];
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Cytoband positions
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  CytoPos <- 0.5 * (cytoband$Start + cytoband$End);
+  CytoLength <- (cytoband$End - cytoband$Start);
+  NbCyto <- length(cytoband[, 1]);
+  HeightPlot <- rep(height, NbCyto);
+  sizeCyto <- matrix(c(CytoLength, HeightPlot), nrow=NbCyto, ncol=2);
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Draw cytobands
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  y0 <- min(unique(y));
+  yC <- y0+height/2;
+  y1 <- y0+height;
+  symbols(x=CytoPos, y=rep(yC, NbCyto), rectangles=sizeCyto,
+      inches=FALSE, bg=cytoband$ColorName, add=TRUE, ...);
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Highlight the centromere
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # The inverted arrow indicating where the centromere is.
+  idxs <- which(cytoband$Centro == 1);
+  centroPos <- min(cytoband$End[idxs]);
+  arrows(centroPos, y0, centroPos, y1, col=colCentro, code=2, angle=120, 
+                                                               length=0.1);
+
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Labels, e.g. 20q12
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  if (labels) {
+    labels <- paste(cytoband$Chromosome, cytoband$Band, sep="");
+#    axis(side=3, at=CytoPos, labels=labels, las=2);
+    dy <- par("cxy")[2];
+    text(x=CytoPos, y=y1+dy/2, labels=labels, srt=90, adj=c(0,0.5));
+  }
+}, private=TRUE)
+
+
+# Patch for plotProfile() of class profileCGH so that 'ylim' argument works.
+# Added also par(cex=0.8) - see code.
+setMethodS3("newPlot", "profileCGH", function(fit, chromosome=NULL, GNL="ZoneGNL", cytobandLabels=TRUE, plotband=TRUE, unit=0, colCytoBand=c("white", "darkblue"), colCentro="red", xlim=NULL, ylim=c(-1,1)*2.5, xlab="Physical position", ylab=variable, flavor=c("glad", "ce", "minimal"), xmargin=c(50,50), resScale=1, ...) {
+  require("GLAD") || stop("Package not loaded: GLAD");  # data(cytoband)
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Argument 'fit':
+  if (!"PosBase" %in% names(fit$profileValues))
+    throw("Argument 'fit' does not contain a 'PosBase' field.");
+
+  # Argument 'chromosome':
+  if (is.null(chromosome)) {
+    chromosome <- unique(fit$profileValues$Chromosome);
+    if (length(chromosome) > 1) {
+      throw("Argument 'chromosome' must not be NULL if 'fit' contains more than one chromosome: ", paste(chromosome, collapse=", "));
+    }
+  }
+  if (length(chromosome) > 1) {
+    throw("Argument 'chromosome' must not contain more than one chromosome: ", paste(chromosome, collapse=", "));
+  }
+
+  # Argument 'flavor':
+  flavor <- match.arg(flavor);
+
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Reset graphical parameters when done
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  opar <- par();
+  on.exit(par(opar));
+
+
+  xScale <- 1/(10^unit);
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Get chromosome lengths
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Load data
+  cytoband <- NULL; rm(cytoband); # To please R CMD check R v2.6.0 dev.
+  data(cytoband);  # Package 'GLAD'
+  genomeInfo <- aggregate(cytoband$End, list(Chromosome=cytoband$Chromosome, 
+                          ChrNumeric=cytoband$ChrNumeric), max, na.rm=TRUE);
+  names(genomeInfo) <- c("Chromosome", "ChrNumeric", "Length");
+  genomeInfo$Chromosome <- as.character(genomeInfo$Chromosome);
+  genomeInfo$ChrNumeric <- as.integer(as.character(genomeInfo$ChrNumeric));
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Plotting flavor
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  opar <- par(no.readonly=TRUE);  
+  on.exit(opar);
+
+  # x-range
+  if (is.null(xlim)) {
+    xlim <- c(0, xScale*genomeInfo$Length[chromosome]);
+  }
+
+  if (flavor == "glad") {
+    par(mar=c(3,3,5,3)+0.1, mgp=c(2,0.6,0.3));
+    axes <- TRUE;
+  } else if (flavor == "ce") {
+    # Margins in pixels-to-inches
+
+    par(mar=c(3,3,5,3)+0.1, mgp=c(2,0.6,0.3), xaxs="i");
+
+    # Set the horizontal margins to 'xmargin'.
+    dim <- getDeviceResolution(resScale) * par("din");
+    plt <- par("plt");    
+    plt[1:2] <- c(xmargin[1], dim[1]-xmargin[2]) / dim[1];
+    par("plt"=plt);
+
+    axes <- TRUE;
+  } else if (flavor == "minimal") {
+    # No margins
+    	par(mar=c(2,0,0.5,0), mgp=c(2,0.6,0.3), xaxs="i");
+    # No axis
+    axes <- FALSE;
+    # No cytobands
+    plotband <- FALSE;
+  }
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Plot main data
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Extract the data to plot
+  plot.window(xlim=xlim, ylim=ylim, xlab=xlab, ylab=ylab, 
+                                                 axes=axes, xaxt="n", ...);
+
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Get the cytoband details for the chromosome of interest
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Drop column 'Chromosome'
+  ## Gives a NOTE in R CMD check R v2.6.0, which is nothing, but we'll
+  ## use a workaround to get a clean result. /HB 2007-06-12
+  Chromosome <- NULL; # dummy
+  cytobandNew <- subset(cytoband, select=-Chromosome); 
+##  cytobandNew <- cytoband[,setdiff(names(cytoband), "Chromosome"),drop=FALSE];
+
+  LabelChr <- data.frame(Chromosome=chromosome);
+  LabelChr <- merge(LabelChr, genomeInfo[, c("ChrNumeric", "Length")], 
+                         by.x="Chromosome", by.y="ChrNumeric", all.x=TRUE);
+
+  LabelChr$Length <- 0;
+
+
+  cytobandNew <- merge(LabelChr, cytobandNew, by.x="Chromosome", 
+                                                        by.y="ChrNumeric");
+  if (plotband) {
+    # Rescale x positions according to units
+    cytobandNew$Start <- xScale*cytobandNew$Start;
+    cytobandNew$End <- xScale*cytobandNew$End;
+
+    usr <- par("usr");
+    dy <- diff(usr[3:4]);
+
+    drawCytoBand(cytobandNew, chromosome=chromosome, 
+      labels=cytobandLabels, y=usr[4]+0.02*dy, height=0.03*dy, 
+      colCytoBand=colCytoBand, colCentro=colCentro);
+  }
+}, private=TRUE) # newPlot()
+
+
+############################################################################
+# HISTORY:
+# 2007-08-23
+# o Created from profileCGH.plotProfile2.R.
+############################################################################
