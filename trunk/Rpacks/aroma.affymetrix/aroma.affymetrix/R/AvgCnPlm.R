c78c9bce3e692771f302a99d1503f45272bd38ed
hb <unknown@noreply.com>
2007-09-08 23:19:47 +0000

    Starting to add support for a probe-level model that simply calculates the average probe signal across replicated probes.  This will be used for the GenomeWideSNP_5/6 chip types which got replicated probes with identical sequences.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@596 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/AvgCnPlm.R
@@ -0,0 +1,55 @@
+###########################################################################/**
+# @RdocClass AvgCnPlm
+#
+# @title "The AvgCnPlm class"
+#
+# \description{
+#  @classhierarchy
+# }
+# 
+# @synopsis
+#
+# \arguments{
+#   \item{...}{Arguments passed to @see "AvgSnpPlm".}
+#   \item{combineAlleles}{If @FALSE, allele A and allele B are treated 
+#      seperately, otherwise together.}
+#   \item{tags}{A @character @vector of tags.}
+# }
+#
+# \section{Fields and Methods}{
+#  @allmethods "public"
+# }
+#
+# @author
+#*/###########################################################################
+setConstructorS3("AvgCnPlm", function(..., combineAlleles=FALSE, tags="*") {
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Argument 'tags':
+  if (!is.null(tags)) {
+    tags <- Arguments$getCharacters(tags);
+    tags <- trim(unlist(strsplit(tags, split=",")));
+
+    # Update default tags
+    idx <- which(tags == "*");
+    if (length(idx) > 0) {
+      if (combineAlleles)
+        tags <- R.utils::insert.default(tags, idx+1, "A+B");
+    }
+  }
+
+
+  extend(AvgSnpPlm(..., tags=tags), c("AvgCnPlm", uses(CnPlm())),
+    combineAlleles = combineAlleles
+  )
+})
+
+
+
+
+############################################################################
+# HISTORY:
+# 2007-09-08
+# o Created from MbeiCnPlm.R.
+############################################################################
