349311dba8678fd5659ffed2b37bdde2383d2d19
hb <hb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-09-10 08:08:58 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@602 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/Rpacks/aroma.affymetrix/redundancyTests/testScripts/system/chipTypes/Mapping10K_Xba142/test20070726,10K,ACC.R
@@ -0,0 +1,34 @@
+library(aroma.affymetrix)
+log <- Arguments$getVerbose(-4);
+timestampOn(log);
+.Machine$float.eps <- sqrt(.Machine$double.eps);
+
+dataSetName <- "Jeremy_2007-10k";
+chipType <- "Mapping10K_Xba142";
+
+# Expected sample names
+sampleNames <- c("0001-7", "0002-10", "0004-13", "0005-14", "0007-18", 
+                      "0008-19", "0010-22", "2-DPrrr", "MH12", "MH18");
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Tests for setting up CEL sets and locating the CDF file
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+cs <- AffymetrixCelSet$fromName(dataSetName, chipType=chipType, verbose=log);
+keep <- 1:6;
+cs <- extract(cs, keep);
+sampleNames <- sampleNames[keep];
+print(cs);
+stopifnot(identical(getNames(cs), sampleNames));
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Allelic cross-talk calibration tests
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+acc <- AllelicCrosstalkCalibration(cs);
+print(acc);
+
+csC <- process(acc, verbose=log);
+print(csC);
+
+stopifnot(identical(getNames(csC), getNames(cs)));
+
