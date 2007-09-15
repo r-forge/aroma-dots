1bef4466c56727bf6141b4c369a55e9c540fe75b
hb <hb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-09-15 08:51:11 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@621 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/Rpacks/aroma.affymetrix/redundancyTests/testScripts/system/chipTypes/GenomeWideSNP_6/test20070816,6.0,ACC.R
@@ -0,0 +1,40 @@
+library(aroma.affymetrix)
+
+log <- Arguments$getVerbose(-50);
+timestampOn(log);
+.Machine$float.eps <- sqrt(.Machine$double.eps);
+
+dataSetName <- "HapMap270,100K,CEU,testSet";
+chipType <- "GenomeWideSNP_6";
+
+# Expected sample names
+sampleNames <- c("NA06985", "NA06991", "NA06993", 
+                 "NA06994", "NA07000", "NA07019");
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setting up CEL set and locating the CDF file
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+cs <- AffymetrixCelSet$fromName(dataSetName, chipType=chipType, verbose=log);
+print(cs);
+stopifnot(identical(getNames(cs), sampleNames));
+cdf <- getCdf(cs);
+print(cdf);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Allelic-crosstalk calibration
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Identify cells *not* for chromosome X
+cdf <- getCdf(cs);
+gi <- getGenomeInformation(cdf);
+units <- getUnitsOnChromosome(gi, 23);
+cells <- getCellIndices(cdf, units=units, useNames=FALSE, unlist=TRUE);
+rm(units);
+cells <- setdiff(1:nbrOfCells(cdf), cells);
+acc <- AllelicCrosstalkCalibration(cs, subsetToAvg=cells, tags=c("*", "-X"));
+rm(cells);
+print(acc);
+
+csC <- process(acc, verbose=log);
+print(csC);
+stopifnot(identical(getNames(csC), getNames(cs)));
---
+++ trunk/Rpacks/aroma.affymetrix/redundancyTests/testScripts/system/chipTypes/GenomeWideSNP_6/test20070816,6.0,spatial.R
@@ -0,0 +1,32 @@
+library(aroma.affymetrix)
+
+log <- Arguments$getVerbose(-50);
+timestampOn(log);
+.Machine$float.eps <- sqrt(.Machine$double.eps);
+
+dataSetName <- "HapMap270,100K,CEU,testSet";
+chipType <- "GenomeWideSNP_6";
+
+# Expected sample names
+sampleNames <- c("NA06985", "NA06991", "NA06993", 
+                 "NA06994", "NA07000", "NA07019");
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setting up CEL set and locating the CDF file
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+cs <- AffymetrixCelSet$fromName(dataSetName, chipType=chipType, verbose=log);
+print(cs);
+stopifnot(identical(getNames(cs), sampleNames));
+cdf <- getCdf(cs);
+print(cdf);
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Spatial intensity plots
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ae <- ArrayExplorer(cs);
+setColorMaps(ae, c("sqrt,yellow", "log2,yellow"));
+print(ae);
+if (interactive())
+  process(ae, verbose=log);
