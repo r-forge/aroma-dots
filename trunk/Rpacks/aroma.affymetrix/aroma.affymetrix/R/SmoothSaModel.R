30a4395626f645f41e61ebcda3c60fd919c8ea77
hb <hb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-09-29 23:10:22 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@653 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/SmoothSaModel.R
@@ -0,0 +1,47 @@
+###########################################################################/**
+# @RdocClass SmoothRmaModel
+#
+# @title "The SmoothRmaModel class"
+#
+# \description{
+#  @classhierarchy
+#
+#  This class represents the Chromosomal Smoothing Robust Multichip Analysis
+#  method.
+# }
+# 
+# @synopsis
+#
+# \arguments{
+#   \item{...}{Arguments passed to the constructor of 
+#              @see "SmoothMultiarrayModel".}
+# }
+#
+# \section{Fields and Methods}{
+#  @allmethods "public"
+# }
+#
+# @author
+#*/###########################################################################
+setConstructorS3("SmoothSaModel", function(...) {
+  extend(SmoothMultiarrayModel(...), "SmoothSaModel");
+})
+
+
+setMethodS3("getAsteriskTag", "SmoothSaModel", function(this, ...) {
+  tags <- NextMethod("getAsteriskTag", this, ...);
+  tags[1] <- "SA";
+  tags;
+}, protected=TRUE)
+
+
+setMethodS3("getFitFunction", "SmoothSaModel", function(this, ...) {
+  smoothWSA;
+})
+
+
+##############################################################################
+# HISTORY:
+# 2007-09-20
+# o Created.
+##############################################################################
