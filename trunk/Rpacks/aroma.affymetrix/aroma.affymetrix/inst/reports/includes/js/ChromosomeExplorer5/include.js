2f3d5a6e508fea888cc3aac2253c421542e4895b
hb <hb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-10-16 19:42:30 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@686 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/inst/reports/includes/js/ChromosomeExplorer5/include.js
+++ trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/inst/reports/includes/js/ChromosomeExplorer5/include.js
@@ -9,21 +9,21 @@ function includeDom(url) {
     return false;
 }
 
-includeDom("../../includes/js/core/jquery-1.2.1.min.js");
-includeDom("../../includes/js/core/jquery.cookie.js");
-includeDom("../../includes/js/core/jquery.blockUI.js");
+includeDom("../../includes/js/jquery/jquery-1.2.1.min.js");
+includeDom("../../includes/js/jquery/jquery.cookie.js");
+includeDom("../../includes/js/jquery/jquery.blockUI.js");
 includeDom("../../includes/js/jquery/jquery.dimensions.js");
 includeDom("../../includes/js/jquery/ui.dialog.js");
 includeDom("../../includes/js/jquery/ui.resizable.js");
 includeDom("../../includes/js/jquery/ui.mouse.js");
 includeDom("../../includes/js/jquery/ui.draggable.js");
-includeDom("../../includes/js/core/jquery.getUrlParameters.js");
-includeDom("../../includes/js/core/prototype.js");
-includeDom("../../includes/js/core/webtoolkit.sprintf.js");
-includeDom("../../includes/js/aroma/aroma.ExplorerSettings.js");
+includeDom("../../includes/js/jquery/jquery.getUrlParameters.js");
+includeDom("../../includes/js/prototype/prototype.js");
+includeDom("../../includes/js/webtoolkit/webtoolkit.sprintf.js");
 includeDom("../../includes/js/Webcuts.js");
 includeDom("../../includes/js/domUtils.js");
 includeDom("../../includes/js/aroma/log.js");
 includeDom("../../includes/js/aroma/ImageLayer.js");
+includeDom("../../includes/js/aroma/aroma.ExplorerSettings.js");
 includeDom("../../includes/js/ChromosomeExplorer5/ChromosomeExplorer.js");
 
