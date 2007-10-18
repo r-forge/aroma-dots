bec128af671eae9dcfd1b7c2d4e434f30f6f9999
hb <hb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-10-18 18:14:50 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@692 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/CopyNumberChromosomalModel.plotSampleLayers.R
+++ trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/CopyNumberChromosomalModel.plotSampleLayers.R
@@ -101,6 +101,7 @@ setMethodS3("plotSampleLayers", "CopyNumberChromosomalModel", function(this, arr
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # Array by array
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  xlim0 <- xlim;
   res <- list();
   arrayFullNames <- getFullNames(this)[arrays];
   arrayNames <- getNames(this)[arrays];
@@ -123,6 +124,7 @@ setMethodS3("plotSampleLayers", "CopyNumberChromosomalModel", function(this, arr
       nbrOfBases <- genome$nbrOfBases[chromosome];
       widthMb <- nbrOfBases / 10^unit;
 
+      xlim <- xlim0;
       # Argument 'xlim' missing?
       if (is.null(xlim)) {
         xlim <- c(0, getChromosomeLength(chromosome))/10^unit;
@@ -182,63 +184,6 @@ setMethodS3("plotSampleLayers", "CopyNumberChromosomalModel", function(this, arr
 
   invisible();
 }) # plotSampleLayers()
-
-
-
-setMethodS3("plotRawCopyNumbers", "CopyNumberChromosomalModel", function(this, path=NULL, col="black", ...) {
-  # The report path
-  if (is.null(path)) {
-    path <- getReportPath(this);
-    path <- filePath(getParent(path), "rawCNs,sampleLayer");
-    path <- Arguments$getWritablePath(path);
-  }
-
-  # Argument 'verbose':
-  verbose <- Arguments$getVerbose(verbose);
-  if (verbose) {
-    pushState(verbose);
-    on.exit(popState(verbose));
-  }
-
-
-  mkdirs(path);
-
-  # Get chip type (used to annotate the plot)
-  chipType <- getChipType(this);
-
-  plotSampleLayers(this, FUN=function(..., array, chromosome, unit, verbose=FALSE) {
-    verbose && enter(verbose, "Plotting transparent image");
-
-    suppressWarnings({
-      # Create empty plot
-      newPlot(this, ..., xlab="", ylab="", yaxt="n", flavor="ce", unit=unit);
-
-      # Extract raw CN estimates
-      rawCns <- extractRawCopyNumbers(this, array=array, chromosome=chromosome, verbose=less(verbose, 5));
-      verbose && print(verbose, rawCns, level=-50);
-
-      # Plot raw CNs data points
-      points(rawCns, xScale=1/10^unit, col=col, ...);
-
-      # Add number-of-loci annotation to graph
-      n <- nbrOfLoci(rawCns, na.rm=TRUE);
-      stext(text=sprintf("n=%d", n), side=4, pos=0, line=0, cex=0.8);
-
-      # Add std dev estimates
-      sd <- sd(rawCns$cn, na.rm=TRUE);
-      mad <- mad(rawCns$cn, na.rm=TRUE);
-      text <- substitute(paste(hat(sigma)==sd, ", ", hat(sigma)["MAD"]==mad, sep=""), list(sd=sprintf("%.3g", sd), mad=sprintf("%.3g", mad)));
-      stext(text=text, side=3, pos=0.5, line=-2);  
-    });
-
-    # Add chip-type annotation
-    stext(chipType, side=4, pos=1, line=0, cex=0.8);
-
-    verbose && exit(verbose);
-  }, path=path, ...);
-}) # plotRawCopyNumbers()
-
-
 
 
 
