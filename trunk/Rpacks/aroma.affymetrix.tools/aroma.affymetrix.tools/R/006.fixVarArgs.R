68f95640893a894dd1bc95f12e4303393940fa2b
hb <unknown@noreply.com>
2007-10-23 18:46:35 +0000

    Created an empty aroma.affymetrix.tools.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@701 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/Rpacks/aroma.affymetrix.tools/aroma.affymetrix.tools/R/006.fixVarArgs.R
@@ -0,0 +1,11 @@
+# Added '...' to some base functions. These will later be
+# turned into default functions by setMethodS3().
+# Example: colSums <- appendVarArgs(colSums)
+
+
+
+############################################################################
+# HISTORY:
+# 2007-10-23 [HB]
+# o Created to please R CMD check.
+############################################################################
---
+++ trunk/Rpacks/aroma.affymetrix.tools/aroma.affymetrix.tools/R/999.package.R
@@ -0,0 +1,33 @@
+#########################################################################/**
+# @RdocPackage aroma.affymetrix.tools
+#
+# \description{
+#  @eval "packageDescription('R.methodsS3')$Description"
+#
+#   This package should be considered to be in an alpha or beta phase.
+#   You should expect the API to be changing over time.
+# }
+#
+# \section{Requirements}{
+#   This package requires \pkg{aroma.affymetrix.tools}.
+# }
+#
+# \section{Installation and updates}{
+#
+#   To install this package, see instructions at 
+#   \url{http://www.braju.com/R/}.
+# } 
+#
+# \author{
+#   Mark Robinson <mrobinson@wehi.edu.au>
+# }
+#
+# \section{License}{
+#   The releases of this package is licensed under 
+#   LGPL version 2.1 or newer.
+# }
+# 
+# \references{
+# }
+#*/#########################################################################
+
