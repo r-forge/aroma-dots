1c32dd55a6138f6ae2736e0df047593c82cfe6bf
hb <hb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-11-29 07:49:35 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@743 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/AffymetrixNetAffxCsvFile.R
+++ trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/AffymetrixNetAffxCsvFile.R
@@ -60,7 +60,7 @@ setMethodS3("readDataUnitChromosomePosition", "AffymetrixNetAffxCsvFile", functi
 
 
 
-setMethodS3("readDataUnitFragmentLength", "AffymetrixNetAffxCsvFile", function(this, colClassPatterns=c("*"="NULL", "^probe[sS]etI[dD]$"="character", "^fragmentLength.*"="character"), enzymes=1, con=NULL, ..., verbose=FALSE) {
+setMethodS3("readDataUnitFragmentLength", "AffymetrixNetAffxCsvFile", function(this, colClassPatterns=c("*"="NULL", "^probe[sS]etI[dD]$"="character", "^fragment.*Length.*"="character"), enzymes=1, con=NULL, ..., verbose=FALSE) {
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # Validate arguments
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -86,42 +86,122 @@ setMethodS3("readDataUnitFragmentLength", "AffymetrixNetAffxCsvFile", function(t
   data <- readData(this, colClassPatterns=colClassPatterns, ..., verbose=less(verbose));
 
   # Extract fragment lengths
-  verbose && enter(verbose, "Extracting fragment lengths from (lengths, start, stop)");
+  verbose && enter(verbose, "Extracting fragment lengths from ([enzyme], lengths, start, stop)");
 
-  cc <- grep("^fragmentLength", colnames(data))[1];
+  cc <- grep("^fragment.*Length", colnames(data))[1];
   fln <- data[[cc]];
   data[[cc]] <- NULL;
+  # Remove all white spaces
+  fln <- gsub(" ", "", fln, fixed=TRUE);
+  # Replace '///' with ';'
+  fln <- gsub("///", ";", fln, fixed=TRUE);
+  # Replace '//' with ','
+  fln <- gsub("//", ",", fln, fixed=TRUE);
   if (isVisible(verbose, level=-50))
     verbose && str(verbose, fln[1:min(10,length(fln))], level=-50);
   gc <- gc();
 
+  # Are enzyme names specified?
+  verbose && enter(verbose, "Inferring if enzyme names are specified");
+  hasNames <- NA;
+  for (kk in seq(along=fln)) {
+    unit <- fln[kk]; 
+    if (nchar(unit) > 0) {
+      hasNames <- (regexpr("^[a-zA-Z]", unit) != -1);
+      break;
+    }
+  }
+  if (is.na(hasNames))
+    throw("INTERNAL ERROR: Failed to parse CSV file for fragment lengths.");
+  verbose && cat(verbose, "Has enzyme names: ", hasNames);
+  verbose && exit(verbose);
+  
   # Split by enzymes first
-  fln <- strsplit(fln, split="///", fixed=TRUE);
+  fln <- strsplit(fln, split=";", fixed=TRUE);
   if (isVisible(verbose, level=-50))
     verbose && str(verbose, fln[1:min(10,length(fln))], level=-50);
   gc <- gc();
 
-  # Keep only requested enzymes
-  fln <- base::lapply(fln, FUN=.subset, enzymes);
-  gc <- gc();
+  if (hasNames) {
+    verbose && enter(verbose, "Identifying number of enzymes");
+    nbrOfEnzymes <- max(sapply(fln, length));
+    verbose && cat(verbose, "Max number of enzymes: ", nbrOfEnzymes);
+    verbose && exit(verbose);
+    
+    verbose && enter(verbose, "Splitting into subunits and padding with NAs");
+    keep <- 1:nbrOfEnzymes;
+    fln <- base::lapply(fln, FUN=function(unit) {
+       # Pad missing enzymes with trailing NAs
+       unit <- .subset(unit, keep);
+       # Split values
+       strsplit(unit, split=",", fixed=TRUE);
+    });
+    if (isVisible(verbose, level=-50))
+      verbose && str(verbose, fln[1:min(10,length(fln))], level=-50);
+    verbose && exit(verbose);
+    
+    # Extract the name for each enzyme
+    verbose && enter(verbose, "Extracting enzyme names");
+    enzymeIdxs <- base::lapply(fln, FUN=function(unit) {
+      base::sapply(unit, FUN=.subset, 1, USE.NAMES=FALSE);
+    });
+    enzymeIdxs <- unlist(enzymeIdxs, use.names=FALSE);
+    # Replace '---' with NAs
+    enzymeIdxs[enzymeIdxs %in% c("---")] <- NA;
+    allNames <- na.omit(sort(unique(enzymeIdxs)));
+    verbose && cat(verbose, "Identified enzymes: ",
+                                    paste(allNames, collapse=", "));
+    # Map names to indices
+    enzymeIdxs <- match(enzymeIdxs,allNames);
+    # Put into an ExJ matrix
+    enzymeIdxs <- matrix(enzymeIdxs, nrow=nbrOfEnzymes);
+    verbose && exit(verbose);
+    if (isVisible(verbose, level=-50))
+      verbose && str(verbose, enzymeIdxs, level=-50);
+    verbose && exit(verbose);
 
-  # Extract the fragment length for each enzyme
-  fln <- base::lapply(fln, FUN=function(unit) {
-    parts <- strsplit(unit, split="//", fixed=TRUE);
-    value <- base::sapply(parts, FUN=.subset, 1, USE.NAMES=FALSE);
-    value <- trim(value);
-    value;
-  })
-  if (isVisible(verbose, level=-50))
-    verbose && str(verbose, fln[1:min(10,length(fln))], level=-50);
-  gc <- gc();
+    # Extract the fragment length for each enzyme
+    verbose && enter(verbose, "Extracting fragment lengths");
+    fln <- base::lapply(fln, FUN=function(unit) {
+      base::sapply(unit, FUN=.subset, 2, USE.NAMES=FALSE);
+    });
+    fln <- unlist(fln, use.names=FALSE);
+    fln <- as.integer(fln);
+    # Put into an ExJ matrix
+    fln <- matrix(fln, nrow=nbrOfEnzymes);
+    verbose && exit(verbose);
 
-  # Turn into an integer matrix
-  fln <- unlist(fln, use.names=FALSE);
-  fln <- as.integer(fln);
-  gc <- gc();
-  fln <- matrix(fln, ncol=nbrOfEnzymes, byrow=TRUE);
-  gc <- gc();
+    verbose && enter(verbose, "Sorting data by enzyme");
+    # Reorganize as an JxE matrix (transposed compared with 'fln'!)
+    fln2 <- matrix(NA, nrow=ncol(fln), ncol=nbrOfEnzymes);
+    for (ee in 1:nbrOfEnzymes) {
+      for (rr in 1:nbrOfEnzymes) {
+        # Identify all indices that have enzyme 'ee' in row 'rr'
+        idxs <- which(enzymeIdxs[rr,] == ee);
+        if (length(idxs) > 0)
+          fln2[idxs,ee] <- fln[rr,idxs];
+      }
+    }
+    # Keep only requested enzymes
+    fln <- fln2[,enzymes];
+    rm(enzymeIdxs, fln2);
+    verbose && exit(verbose);
+  } else {
+    # Extract the fragment length for each enzyme
+    verbose && enter(verbose, "Extracting fragment lengths");
+    fln <- base::lapply(fln, FUN=function(unit) {
+      # Keep only requested enzymes
+      unit <- .subset(unit, enzymes);
+      parts <- strsplit(unit, split=",", fixed=TRUE);
+      base::sapply(parts, FUN=.subset, 1, USE.NAMES=FALSE);
+    });
+    verbose && exit(verbose);
+    
+    # Reorganize as an JxE integer matrix
+    fln <- unlist(fln, use.names=FALSE);
+    fln <- as.integer(fln);
+    fln <- matrix(fln, ncol=nbrOfEnzymes, byrow=TRUE);
+  }
 	
   if (isVisible(verbose, level=-10))
     verbose && str(verbose, fln, level=-10);
@@ -142,8 +222,6 @@ setMethodS3("readDataUnitFragmentLength", "AffymetrixNetAffxCsvFile", function(t
   colnames(data) <- c("unitName", names);
   attr(data, "header") <- NULL;
 
-  gc <- gc();
-
   verbose && exit(verbose);
 
   data;
@@ -153,6 +231,10 @@ setMethodS3("readDataUnitFragmentLength", "AffymetrixNetAffxCsvFile", function(t
 
 ############################################################################
 # HISTORY:
+# 2007-11-28
+# o Updated readDataUnitFragmentLength() of AffymetrixNetAffxCsvFile to
+#   also handle "enzyme data" columns that contain named or non-named
+#   multiple enzymes.
 # 2007-09-16
 # o Now readDataUnitChromosomePosition() of AffymetrixNetAffxCsvFile also
 #   recognizes column name 'probeStartPosition'.
