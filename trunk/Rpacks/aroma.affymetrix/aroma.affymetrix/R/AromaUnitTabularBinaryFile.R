ab18c888eb3b1b19374b0ed1d52ce5f3a104bda5
hb <unknown@noreply.com>
2007-12-10 21:38:55 +0000

    # 2007-12-10 # o Currently a AromaUnitTabularBinaryFile (e.g. AromaUgpFile) does not #   contain information about the "fullname" chip type, but only the basic #   chip-type name, e.g. we cannot infer the full chip-type name from #   'GenomeWideSNP_5,Full,r2.ugp', but only 'GenomeWideSNP_5'. The fullname #   should be the same as the full chip-type name of the CDF used to define #   the the unit map, e.g. 'GenomeWideSNP_5,Full.CDF'. #   We should add a header (or footer) field in the file format that #   indicates the full chip type. #   However, until that is done, the best we can do is to turn to the ad #   hoc solution of scanning for the CDF with the longest matching fullname, #   if both 'GenomeWideSNP_5,Full.CDF' and 'GenomeWideSNP_5.CDF' exists, #   the we match the former to 'GenomeWideSNP_5,Full,r2.ugp'.  The fullname #   chip type of the UGP is then full chip-type name of the CDF.  NOTE, #   there is major drawback with this.  If the user deletes the "full" CDF, #   the above approach would all of a sudden return a different full name!
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@767 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/AromaUnitTabularBinaryFile.R
+++ trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/AromaUnitTabularBinaryFile.R
@@ -1,8 +1,19 @@
 setConstructorS3("AromaUnitTabularBinaryFile", function(...) {
   extend(AromaTabularBinaryFile(...), "AromaUnitTabularBinaryFile",
-    .cdf = NULL
+    "cached:.cdf" = NULL
   );
 })
+
+
+setMethodS3("clearCache", "AromaUnitTabularBinaryFile", function(this, ...) {
+  # Clear all cached values.
+  for (ff in c(".cdf")) {
+    this[[ff]] <- NULL;
+  }
+
+  # Then for this object
+  NextMethod(generic="clearCache", object=this, ...); 
+}, private=TRUE)
 
 
 setMethodS3("getFilenameExtension", "AromaUnitTabularBinaryFile", static=TRUE, abstract=TRUE);
@@ -12,18 +23,74 @@ setMethodS3("nbrOfUnits", "AromaUnitTabularBinaryFile", function(this, ...) {
 })
 
 
-setMethodS3("getChipType", "AromaUnitTabularBinaryFile", function(this, ...) {
-  getName(this, ...);
-})
+setMethodS3("getChipType", "AromaUnitTabularBinaryFile", function(this, fullname=TRUE, ...) {
+  if (fullname) {
+    # Currently these files do not contain fullname chiptype information.
+    # Instead we have to search for a match CDF and use its chip type.
+    # Note, the CDF returned will depend of which CDF exists in the search
+    # path, if at all.  AD HOC! /HB 2007-12-10
+    cdf <- getCdf(this, ...);
+    chipType <- getChipType(cdf, fullname=fullname);
+  } else {
+    chipType <- getName(this, ...);
+  }
+
+  chipType;
+});
 
 
-setMethodS3("getCdf", "AromaUnitTabularBinaryFile", function(this, ...) {
+setMethodS3("getCdf", "AromaUnitTabularBinaryFile", function(this, ..., force=FALSE, verbose=FALSE) {
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Argument 'verbose':
+  verbose <- Arguments$getVerbose(verbose);
+  if (verbose) {
+    pushState(verbose);
+    on.exit(popState(verbose));
+  } 
+
   cdf <- this$.cdf;
-  if (is.null(cdf)) {
-    chipType <- getChipType(this);
-    cdf <- AffymetrixCdfFile$fromChipType(chipType);
+  if (force || is.null(cdf)) {
+    # Generate all possible fullname 'chipTypes' and search for the existance
+    # of a CDF with the longest name *and* that have the same number of units.
+    # This is AD HOC! We should really store the full chiptype of the
+    # corresponding CDF in the internal file header. /HB 2007-12-10
+    verbose && enter(verbose, "Searching for a match CDF");
+    verbose && cat(verbose, "Filename: ", getFilename(this));
+    name <- getName(this, ...);
+    tags <- getTags(this, collapse=NULL, ...);
+    cdf <- NULL;
+    # Number of units the matching CDF must have.
+    nbrOfUnits <- nbrOfUnits(this);
+    for (kk in rev(c(0,seq(along=tags)))) {
+      cdfTags <- tags[seq(length=kk)];
+      fullname <- paste(c(name, cdfTags), collapse=",");
+      verbose && printf(verbose, "Trying '%s'...", fullname);
+      tryCatch({
+        cdf <- AffymetrixCdfFile$fromChipType(name, tags=cdfTags);
+        # Does it have the correct number of units?
+        if (nbrOfUnits(cdf) != nbrOfUnits)
+          cdf <- NULL;
+      }, error = function(ex) {})
+  
+      # Found a CDF?
+      if (!is.null(cdf)) {
+        verbose && writeRaw(verbose, "found.\n");
+        break;
+      }
+      verbose && writeRaw(verbose, "no match.\n");
+    }
+    verbose && exit(verbose);
+  
+    if (is.null(cdf)) {
+      throw("Failed to locate a CDF for ", class(this)[1], 
+            " that have ", nbrOfUnits, " units: ", getFullName(this));
+    }
+  
     this$.cdf <- cdf;
   }
+
   cdf;
 })
 
@@ -150,6 +217,23 @@ setMethodS3("importFromGenomeInformation", "AromaUnitTabularBinaryFile", abstrac
 
 ############################################################################
 # HISTORY:
+# 2007-12-10
+# o Currently a AromaUnitTabularBinaryFile (e.g. AromaUgpFile) does not
+#   contain information about the "fullname" chip type, but only the basic
+#   chip-type name, e.g. we cannot infer the full chip-type name from 
+#   'GenomeWideSNP_5,Full,r2.ugp', but only 'GenomeWideSNP_5'. The fullname
+#   should be the same as the full chip-type name of the CDF used to define
+#   the the unit map, e.g. 'GenomeWideSNP_5,Full.CDF'.
+#   We should add a header (or footer) field in the file format that 
+#   indicates the full chip type.  
+#   However, until that is done, the best we can do is to turn to the ad
+#   hoc solution of scanning for the CDF with the longest matching fullname,
+#   if both 'GenomeWideSNP_5,Full.CDF' and 'GenomeWideSNP_5.CDF' exists,
+#   the we match the former to 'GenomeWideSNP_5,Full,r2.ugp'.  The fullname
+#   chip type of the UGP is then full chip-type name of the CDF.  NOTE,
+#   there is major drawback with this.  If the user deletes the "full" CDF,
+#   the above approach would all of a sudden return a different full name!
+# o Added clearCache().
 # 2007-09-14
 # o Renames createFromCdf() to allocateFromCdf().
 # 2007-09-13
