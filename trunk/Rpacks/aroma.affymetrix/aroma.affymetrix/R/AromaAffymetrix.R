435250f947a315b0a1898a516f7af570600ca657
hb <hb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-12-14 21:48:11 +0000

    Replaced log(x, base=2) with log2(x). Rebuilt Rdoc:s.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@777 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/AromaAffymetrix.R
+++ trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/AromaAffymetrix.R
@@ -42,8 +42,110 @@ setMethodS3("fixSearchPath", "AromaAffymetrix", function(this, ..., verbose=FALS
   invisible(pkgsMoved);
 })
 
+
+setMethodS3("update", "AromaAffymetrix", function(object, patch=TRUE, ..., verbose=FALSE) {
+  # To please R CMD check
+  this <- object;
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Argument 'verbose':
+  verbose <- Arguments$getVerbose(verbose);
+  if (verbose) {
+    pushState(verbose);
+    on.exit(popState(verbose));
+  }
+
+
+  verbose && enter(verbose, "Checking for and install updates");
+  verbose && cat(verbose, "Package: ", getName(this));
+  verbose && printf(verbose, "Current version: v%s (%s)\n", 
+                                        getVersion(this), getDate(this));
+
+
+  state <- 0;
+
+  url <- "http://www.braju.com/R/hbLite.R";
+  verbose && enter(verbose, "Trying to download update script");
+  verbose && cat(verbose, "URL: ", url);
+  hbInstall <- NULL;
+  tryCatch({
+    suppressWarnings({
+      source(url);
+    })
+    state <- 1;
+    verbose && exit(verbose);
+  }, error = function(ex) {
+    verbose && exit(verbose, suffix="failed");
+    throw(ex);
+  })
+
+  verbose && enter(verbose, "Launching update command");
+  verbose && printf(verbose, "Call: hbInstall(\"%s\")", getName(this));
+  tryCatch({
+    hbInstall(getName(this));
+    state <- 2;
+  }, error = function(ex) {
+    verbose && exit(verbose, suffix="failed");
+    throw(ex);
+  })
+
+  verbose && cat(verbose, "Package has been updated.");
+
+  if (patch) {
+    patch(this, ..., verbose=verbose);
+  }
+
+  verbose && exit(verbose);
+
+  invisible(state);
+})
+
+
+
+setMethodS3("patch", "AromaAffymetrix", function(this, ..., verbose=FALSE) {
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Argument 'verbose':
+  verbose <- Arguments$getVerbose(verbose);
+  if (verbose) {
+    pushState(verbose);
+    on.exit(popState(verbose));
+  }
+
+
+  verbose && enter(verbose, "Checking for and install patches");
+  verbose && cat(verbose, "Package: ", getName(this));
+  verbose && printf(verbose, "Current version: v%s (%s)\n", 
+                                        getVersion(this), getDate(this));
+
+
+  state <- 0;
+
+  verbose && enter(verbose, "Trying to download patches");
+  tryCatch({
+    downloadPackagePatch(getName(this), verbose=verbose);
+    state <- 1;
+    verbose && exit(verbose);
+  }, error = function(ex) {
+    verbose && exit(verbose, suffix="failed");
+    throw(ex);
+  })
+
+  verbose && cat(verbose, "Package has been patched.");
+  
+  verbose && exit(verbose);
+
+  invisible(state);
+})
+
+
 ############################################################################
 # HISTORY:
+# 2007-12-13
+# o Added update() and patch() to the AromaAffymetrix Package class.
 # 2007-03-06
 # o Created.
 ############################################################################
