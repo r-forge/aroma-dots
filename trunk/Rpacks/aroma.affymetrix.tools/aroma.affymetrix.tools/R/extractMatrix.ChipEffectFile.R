a7c0b73e983051ba5659023afda7ba884ff7ed71
epurdom <unknown@noreply.com>
2008-02-11 21:46:21 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@796 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/Rpacks/aroma.affymetrix.tools/aroma.affymetrix.tools/R/extractMatrix.ChipEffectFile.R
@@ -0,0 +1,34 @@
+setMethodS3("extractMatrix","ChipEffectFile",
+function (this, units = NULL, ..., field = c("theta", "sdTheta"), 
+    returnUgcMap = FALSE, verbose = FALSE) 
+{
+    cdf <- getCdf(this)
+    if (is.null(units)) {
+        nunits <- nbrOfUnits(cdf)
+    }
+    else {
+        units <- Arguments$getIndices(units, range = c(1, nbrOfUnits(cdf)))
+        nunits <- length(units)
+    }
+    field <- match.arg(field)
+    verbose <- Arguments$getVerbose(verbose)
+    if (verbose) {
+        pushState(verbose)
+        on.exit(popState(verbose))
+    }
+    verbose && enter(verbose, "Getting data for the array set")
+    verbose && enter(verbose, "Getting unit-to-cell map")
+    ugcMap <- getCellMap(this, units = units, verbose = less(verbose))
+    ugcMap <- subset(ugcMap, ...)
+    verbose && exit(verbose)
+    if (nrow(ugcMap) == 0) throw("Nothing to return.")
+    df <- matrix(NA, nrow = nrow(ugcMap), ncol = 1)
+    gc <- gc()
+    verbose && print(verbose, gc)
+    verbose && enter(verbose, "Retrieving sample thetas")
+    df[, 1] <- getDataFlat(this, units = ugcMap, fields = field, verbose = less(verbose))[, field]
+    verbose && exit(verbose)
+    verbose && exit(verbose)
+    if (returnUgcMap) attr(df, "unitGroupCellMap") <- ugcMap
+    df
+})
