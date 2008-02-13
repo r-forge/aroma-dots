958df7e62c766fd348c241c910ab0d64ab5ffd9f
hb <hb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-02-13 08:30:14 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@800 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/private.patchMatrix.R
+++ trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/private.patchMatrix.R
@@ -1,4 +1,8 @@
 .patchMatrix <- function(...) {
+  # Nothing to do?
+  if (compareVersion(as.character(getRversion()), "2.7.0") >= 0)
+    return();
+
   # A more memory efficient matrix() function when 'dimnames' is NULL.
   matrix.patch <- function(data=NA, nrow=1, ncol=1, byrow=FALSE, dimnames=NULL) {
     data <- as.vector(data);
@@ -26,6 +30,8 @@
 
 ############################################################################
 # HISTORY:
+# 2008-02-12 [HB]
+# o In R v2.7.0, the internal code of matrix() fixes the above problem.
 # 2007-07-04 [HB]
 # o Created.  This patches matrix() so that it avoid copying if x[1,1] <- 1
 #   is applied directly after create a matrix without dimension names.  The
