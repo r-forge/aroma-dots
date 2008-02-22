f42cfef020127a04f963ed60ea263e6e6b73fc1c
epurdom <epurdom@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-02-22 21:53:59 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@820 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Rpacks/aroma.affymetrix.tools/aroma.affymetrix.tools/R/createProject.R
+++ trunk/Rpacks/aroma.affymetrix.tools/aroma.affymetrix.tools/R/createProject.R
@@ -1,6 +1,11 @@
+#files -- names of files (with or without .cel) to search for and include in new project. 
+#project Tag -- name to append for new project
+#coreName -- beginning string to look for;if coreName=NULL, then search all subfolders under searchDir
+#searchDir -- which directory to look for raw data
+#targetDir -- where to put the folder of shortcuts that defines new project
+#add -- if true, will continue even if target project folder already exists (with warning).
+
 createProject<-function(files,projectTag,chipType,coreName=NULL,add=FALSE,searchDir="rawData",targetDir="probeData"){
-#if coreName=NULL, then search all subfolders under rawData
-#doesn't search probeData; creates project in probeData
     if(.Platform$OS.type=="windows") stop("Sorry, cannot create symbolic links in windows")
     files<-gsub(".[Cc][Ee][Ll]$","",files)
     if(is.null(coreName)) projectFilename<-projectTag
@@ -13,34 +18,18 @@ createProject<-function(files,projectTag,chipType,coreName=NULL,add=FALSE,search
     else dir.create(projectPath,recursive=TRUE)
     allRawDir<-list.files(searchDir,full=TRUE)#gives full pathname rawData/... etc.
     allRawDir<-allRawDir[file.info(allRawDir)$isdir]
-#    print(allRawDir)
     if(!is.null(coreName)){#only those directories that match the coreName
         allRawDir<-allRawDir[grep(paste("^",searchDir,"/",coreName,sep=""),allRawDir)] 
     }
-#    print(allRawDir)
     allFiles<-list.files(file.path(allRawDir,chipType),full=TRUE)
-    allbasename<-basename(allFiles)	
+    allbasename<-basename(allFiles) 
     CELIndex<-grep(".[Cc][Ee][Ll]$",allbasename)
     projectIndex<-match(files,gsub(".[Cc][Ee][Ll]$","",allbasename[CELIndex]))
     if(any(is.na(projectIndex))) warning("not all of files are found in the designated raw data folders")
     projectIndex<-projectIndex[!is.na(projectIndex)]
-	projectFiles<-allFiles[CELIndex][projectIndex]
+    projectFiles<-allFiles[CELIndex][projectIndex]
 projectFiles<-file.path(getwd(),projectFiles)
     if(length(projectIndex)>0) file.symlink(from=projectFiles,to=projectPath)
 
-	return()
-
-#    for(kk in 1:length(allRawDir)){
-#        dataDir<-file.path(allRawDir[kk],chipType)
-#        CELfiles<-list.files(dataDir)
-#        CELfiles<-CELfiles[grep(".[Cc][Ee][Ll]$",CELfiles)]     
-#	if(length(CELfiles)>0){   
-#        	projectFiles<-CELfiles[gsub(".[Cc][Ee][Ll]$","",CELfiles) %in% files]
-#        	projectFiles<-file.path(getwd(),dataDir,projectFiles)
- #       	print(projectFiles)
-#        	if(length(projectFiles)>0) file.symlink(from=projectFiles,to=projectPath)
-#	}    
-#    }
-    
-    
+    return() 
 }
