9154c77551d352ce3d0de4a8258b0627cdc93fbd
hb <unknown@noreply.com>
2008-02-28 03:35:03 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@839 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/UflSnpInformation.R
+++ trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/UflSnpInformation.R
@@ -96,11 +96,17 @@ setMethodS3("readData", "UflSnpInformation", function(this, nrow=NULL, ..., verb
 })
 
 setMethodS3("getDataColumns", "UflSnpInformation", function(this, ...) {
+  cat("getDataColumns()...\n");
   ufl <- getAromaUflFile(this);
+  print(ufl);
+  print(colnames);
+  str(22222222222);
   names <- colnames(ufl);
+  str(22222222222);
 str(names);
   names <- gsub("^length", "fragmentLength", names);
 str(names);
+  cat("getDataColumns()...done\n");
   names;
 }, private=TRUE)
 	
