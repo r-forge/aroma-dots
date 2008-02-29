985571316ebbe705682131afa44f7deafc590c50
epurdom <epurdom@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-02-29 19:41:58 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@854 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/mergeBoxplotStats.list.R
+++ trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/R/mergeBoxplotStats.list.R
@@ -18,7 +18,7 @@
 #
 # \value{
 #   Returns a @list structure in a format that is returned by
-#   @see "grDevices::boxplot.stats".
+#   @see "grDevices::boxplot".
 # }
 #
 # @examples "../incl/mergeBoxplotStats.list.Rex"
