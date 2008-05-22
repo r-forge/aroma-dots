2bf15fb9c9c27268d751cd3730e1ebd45f223248
hb <unknown@noreply.com>
2008-05-22 00:42:15 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@932 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Rpacks/aroma.cn/aroma.cn/R/AromaTotalCnBinaryFile.R
+++ trunk/Rpacks/aroma.cn/aroma.cn/R/AromaTotalCnBinaryFile.R
@@ -27,9 +27,47 @@ setConstructorS3("AromaTotalCnBinaryFile", function(...) {
 })
 
 
+setMethodS3("extractRawCopyNumbers", "AromaTotalCnBinaryFile", function(this, chromosome, range=NULL, ..., verbose=FALSE) {
+  # Argument 'verbose':
+  verbose <- Arguments$getVerbose(verbose);
+  if (verbose) {
+    pushState(verbose);
+    on.exit(popState(verbose));
+  } 
+
+
+  verbose && enter(verbose, "Extracting RawCopyNumbers");
+
+  verbose && cat(verbose, "Chromosome: ", chromosome);
+  verbose && enter(verbose, "Identifying units on chromosome");
+  ugp <- getAromaUgpFile(this, ..., verbose=less(verbose,50));
+  verbose && print(verbose, ugp);
+  units <- getUnitsAt(ugp, chromosome=chromosome, range=range, ..., verbose=less(verbose,5));
+  verbose && cat(verbose, "Units:");
+  verbose && str(verbose, units);  
+
+  verbose && cat(verbose, "Genomic positions:");
+  pos <- getPositions(ugp, units=units);
+  verbose && str(verbose, pos);  
+  verbose && exit(verbose);
+
+  verbose && enter(verbose, "Extracting data");
+  M <- extractMatrix(this, rows=units, drop=TRUE, verbose=less(verbose,5));
+  verbose && str(verbose, M);  
+  rawCNs <- RawCopyNumbers(x=pos, cn=M);
+  verbose && exit(verbose);
+
+  verbose && exit(verbose);
+
+  rawCNs;
+})
+
+
 
 ############################################################################
 # HISTORY:
+# 2008-05-21
+# o Added extractRawCopyNumbers().
 # 2008-05-11
 # o Created.
 ############################################################################
