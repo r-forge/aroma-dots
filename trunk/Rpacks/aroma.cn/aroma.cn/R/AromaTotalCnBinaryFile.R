e30ddaac6eaef1cb3195061a3e54287eafbaf417
hb <hb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-06-20 09:05:01 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@958 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Rpacks/aroma.cn/aroma.cn/R/AromaTotalCnBinaryFile.R
+++ trunk/Rpacks/aroma.cn/aroma.cn/R/AromaTotalCnBinaryFile.R
@@ -55,6 +55,12 @@ setMethodS3("extractRawCopyNumbers", "AromaTotalCnBinaryFile", function(this, ch
   M <- extractMatrix(this, rows=units, drop=TRUE, verbose=less(verbose,5));
   verbose && str(verbose, M);  
   rawCNs <- RawCopyNumbers(x=pos, cn=M, chromosome=chromosome);
+
+  # Add annotation data
+  rawCNs$platform <- getPlatform(this);
+  rawCNs$chipType <- getChipType(this);
+  rawCNs$fullname <- getFullName(this);
+
   verbose && exit(verbose);
 
   verbose && exit(verbose);
@@ -66,6 +72,9 @@ setMethodS3("extractRawCopyNumbers", "AromaTotalCnBinaryFile", function(this, ch
 
 ############################################################################
 # HISTORY:
+# 2008-06-12
+# o Now extractRawCopyNumbers() adds annotation data to the returned object,
+#   i.e. platform, chipType, and fullname.
 # 2008-05-21
 # o Added extractRawCopyNumbers().
 # 2008-05-11
