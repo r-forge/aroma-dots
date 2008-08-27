dc9a20048af97d4746405631243840430e3ef0c1
hb <hb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-08-27 18:32:54 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@1103 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/inst/testScripts/system/chipTypes/GenomeWideSNP_6/test20080827,6.0,CRMAv2.R
@@ -0,0 +1,69 @@
+library("aroma.affymetrix");
+
+log <- Arguments$getVerbose(-4, timestamp=TRUE);
+
+dataSetName <- "HapMap270,6.0,CEU,testSet";
+chipType <- "GenomeWideSNP_6,Full";
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setup of annotation files
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# CDF
+cdf <- AffymetrixCdfFile$byChipType(chipType);
+
+# Assert existence of probe-sequence annotation files
+acs <- AromaCellSequenceFile$byChipType(getChipType(cdf, fullname=FALSE));
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Tests for setting up CEL sets and locating the CDF file
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+csR <- AffymetrixCelSet$fromName(dataSetName, cdf=cdf);
+print(csR);
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Allelic cross-talk calibration tests
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+acc <- AllelicCrosstalkCalibration(csR);
+print(acc);
+csC <- process(acc, verbose=log);
+print(csC);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Base-position normalization
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+bpn <- BasePositionNormalization(csC, target="zero", shift=+300, tags="*,z");
+print(bpn);
+
+csN <- process(bpn, verbose=log);
+print(csN);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Allele-specific chip effect estimates
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+plm <- AvgCnPlm(csN, mergeStrands=TRUE, combineAlleles=TRUE);
+print(plm);
+fit(plm, verbose=log);
+ces <- getChipEffectSet(plm);
+print(ces);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Fragment-length normalization test
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+fln <- FragmentLengthNormalization(ces, target="zero", tags="*,z");
+print(fln);
+cesN <- process(fln, verbose=verbose);
+print(cesN);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Extract (thetaA, thetaB) for copy-neutral chromosomes
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+cdf <- getCdf(cesN);
+gi <- getGenomeInformation(cdf);
+units <- getUnitsOnChromosome(gi, 1:22);
+theta <- extractTheta(cesN, units=units, drop=TRUE);
+str(theta);
+print(theta[1:10,]);
