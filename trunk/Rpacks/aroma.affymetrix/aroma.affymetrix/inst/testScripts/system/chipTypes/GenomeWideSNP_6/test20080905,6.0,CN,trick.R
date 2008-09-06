486c70883b2298764be40408356243d17fc37bf9
hb <unknown@noreply.com>
2008-09-06 00:02:38 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@1131 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/inst/testScripts/system/chipTypes/GenomeWideSNP_6/test20080905,6.0,CN,trick.R
@@ -0,0 +1,59 @@
+library("aroma.affymetrix");
+
+log <- Verbose(threshold=-50, timestamp=TRUE);
+
+dataSetName <- "HapMap270,6.0,CEU,testSet";
+chipType <- "GenomeWideSNP_6";
+
+# Expected sample names
+sampleNames <- c("NA06985", "NA06991", "NA06993", 
+                 "NA06994", "NA07000", "NA07019");
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setting up CEL set and locating the CDF file
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+cdf <- AffymetrixCdfFile$fromChipType(chipType, tags="Full");
+print(cdf);
+
+csR <- AffymetrixCelSet$fromName(dataSetName, cdf=cdf, verbose=log);
+print(csR);
+stopifnot(identical(getNames(csR), sampleNames));
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Allelic-crosstalk calibration
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+acc <- AllelicCrosstalkCalibration(csR);
+print(acc);
+
+csC <- process(acc, verbose=log);
+print(csC);
+stopifnot(identical(getNames(csC), getNames(csR)));
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Averaging probe-level model
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+plm <- AvgCnPlm(csC, mergeStrands=TRUE, combineAlleles=TRUE, shift=300);
+print(plm);
+
+ces <- getChipEffectSet(plm);
+print(ces);
+
+units <- fitCnProbes(plm, verbose=log);
+str(units);
+fit(plm, verbose=log);
+
+
+theta <- extractMatrix(ces, units=1000:1002);
+
+fln <- FragmentLengthNormalization(ces);
+cesFln <- process(fln, verbose=log);
+print(cesFln);
+
+cnr <- CbsModel(cesFln);
+print(cnr);
+
+ce <- ChromosomeExplorer(cnr);
+print(ce);
+process(ce, arrays=1, chromosomes=c(19,23), verbose=log);
