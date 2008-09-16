51edaa130cde11837390391df6ce481ba0e0e965
hb <hb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-09-16 06:49:33 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@1148 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/Rpacks/aroma.affymetrix/buildAnnotationData/Mapping250K_Sty,UFL,na26.R
@@ -0,0 +1,111 @@
+if (interactive()) savehistory();
+library("aroma.affymetrix");
+log <- Verbose(threshold=-50, timestamp=TRUE);
+
+source("../aroma.affymetrix/R/AffymetrixNetAffxCsvFile.R");
+source("../aroma.affymetrix/R/AromaUflFile.R");
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Settings
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+naVersion <- "26";
+user <- "HB";
+datestamp <- "20080915";
+
+chipType <- "Mapping250K_Sty";
+nbrOfEnzymes <- 1;
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Setup required annotation files
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+if (!exists("cdf")) {
+  cdf <- AffymetrixCdfFile$byChipType(chipType);
+  rm(csv);
+}
+print(cdf);
+
+if (!exists("csv")) {
+  tags <- sprintf(".na%s", naVersion);
+  pathname <- AffymetrixNetAffxCsvFile$findByChipType(chipType, tags=tags);
+  if (isFile(pathname)) {
+    csv <- AffymetrixNetAffxCsvFile(pathname);
+  }
+  rm(tags);
+}
+print(csv);
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Import UFL from CSV files
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+tags <- sprintf("na%s,%s%s", naVersion, user, datestamp);
+ufl <- NULL;
+tryCatch({
+  ufl <- AromaUflFile$byChipType(getChipType(cdf), tags=tags);
+}, error = function(ex) {})
+if (is.null(ufl)) {
+  ufl <- AromaUflFile$allocateFromCdf(cdf, tags=tags);
+}
+print(ufl);
+
+
+units <- importFrom(ufl, csv, enzymes="NspI", verbose=log);
+str(units);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Update the file footer
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+if (!exists("srcFileTags", mode="list")) {
+  srcFileTags <- list();
+  srcFiles <- list(cdf, csv);
+  for (kk in seq(along=srcFiles)) {
+    srcFile <- srcFiles[[kk]];
+    tags <- list(
+      filename=getFilename(srcFile), 
+      filesize=getFileSize(srcFile), 
+      checksum=getChecksum(srcFile)
+    );
+    srcFileTags[[kk]] <- tags;
+  }
+  print(srcFileTags);
+}
+
+footer <- readFooter(ufl);
+footer$createdOn <- format(Sys.time(), "%Y%m%d %H:%M:%S", usetz=TRUE);
+footer$createdBy = list(
+  fullname = "Henrik Bengtsson", 
+  email = "hb@stat.berkeley.edu"
+);
+names(srcFileTags) <- sprintf("srcFile%d", seq(along=srcFileTags));
+footer$srcFiles <- srcFileTags;
+writeFooter(ufl, footer);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Statistics
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+print(ufl);
+##  AromaUflFile:
+##  Name: Mapping250K_Sty
+##  Tags: na26,HB20080915
+##  Pathname: annotationData/chipTypes/Mapping250K_Sty/Mapping250K_Sty,na26,HB20080915.ufl
+##  File size: 466.18kB
+##  RAM: 0.00MB
+##  Number of data rows: 238378
+##  File format: v1
+##  Dimensions: 238378x1
+##  Column classes: integer
+##  Number of bytes per column: 2
+##  Footer: <platform>Affymetrix</platform><chipType>Mapping250K_Sty</chipType><createdOn>20080915 23:21:32 PDT</createdOn><createdBy><fullname>Henrik Bengtsson</fullname><email>hb@stat.berkeley.edu</email></createdBy><srcFiles><srcFile1><filename>Mapping250K_Sty.cdf</filename><filesize>184371335</filesize><checksum>8c7ffa88bb444d953214f601bb998e51</checksum></srcFile1><srcFile2><filename>Mapping250K_Sty.na26.annot.csv</filename><filesize>412941971</filesize><checksum>eb1ced7bd45aa7c6f6edf9fb708c7441</checksum></srcFile2></srcFiles>
+##  Chip type: Mapping250K_Sty
+##  Platform: Affymetrix
+
+x <- summaryOfUnits(ufl);
+print(x);
+##                  snp cnp affxSnp other  total
+##  enzyme1-only 144868   0       0     0 144868
+##  missing       93436   0       0    74  93510
+##  total        238304   0       0    74 238378
