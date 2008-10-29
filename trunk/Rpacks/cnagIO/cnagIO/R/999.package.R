c2a13bf89ec9d8973ec2aca4f3aee8d9dbc6bb35
hb <hb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-10-29 00:40:08 +0000

    Created package cnagIO.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@1188 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/Rpacks/cnagIO/cnagIO/R/999.package.R
@@ -0,0 +1,35 @@
+#########################################################################/**
+# @RdocPackage cnagIO
+#
+# \description{
+#   @eval "packageDescription('cnagIO')$Description" 
+#
+#   This package should be considered to be in an alpha or beta phase.
+# }
+#
+# \section{Installation and updates}{
+#   To install this package, see instructions at 
+#   \url{http://www.braju.com/R/}.
+# } 
+#
+# \author{
+#   Henrik Bengtsson.
+# }
+#
+# \section{To get started}{
+#   To get started, see:
+#   \enumerate{
+#     \item \code{\link{readCfhUnits}}() - reads a CNAG CFH file.
+#     \item \code{\link{readCfnUnits}}() - reads a CNAG CFN file.
+#   }
+# }
+#
+# \section{License}{
+#   The releases of this package is licensed under 
+#   LGPL version 2.1 or newer.
+# }
+# 
+# \references{
+#  [1] The CNAG software, \url{www.genome.umin.jp/}, Oct 2008.
+# }
+#*/#########################################################################
