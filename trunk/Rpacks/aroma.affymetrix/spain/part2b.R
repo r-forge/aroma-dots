a1da71aa6719d476614660837e1c9fd0e2a5f59f
hb <hb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-11 07:57:25 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@1249 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/Rpacks/aroma.affymetrix/spain/part2b.R
@@ -0,0 +1,67 @@
+##########################################################################
+# Data set:
+# GSE8605/
+#   Mapping10K_Xba142/
+#    GSM226867.CEL, ..., GSM226876.CEL [10 files]
+# URL: http://www.ncbi.nlm.nih.gov/projects/geo/query/acc.cgi?acc=GSE8605
+##########################################################################
+if (interactive()) savehistory();
+library("aroma.affymetrix");
+log <- Arguments$getVerbose(-8, timestamp=TRUE);
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setup annotation data
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+cdf <- AffymetrixCdfFile$byChipType("Mapping50K_Hind240");
+print(cdf);
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setup raw data
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+csR <- AffymetrixCelSet$byName("HapMap270,100K,CEU,5trios", cdf=cdf);
+print(csR);
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Correct for crosstalk between alleles
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+acc <- AllelicCrosstalkCalibration(csR, model="CRMAv2", tags="*,v2");
+print(acc);
+csC <- process(acc, verbose=log);
+print(csC);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Correct for probe-sequence dependent effects
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+bpn <- BasePositionNormalization(csC, target="zero");
+print(bpn);
+csN <- process(bpn, verbose=log);
+print(csN);
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Summarize probesets
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+plm <- RmaCnPlm(csN, mergeStrands=TRUE, combineAlleles=TRUE);
+print(plm);
+fit(plm, verbose=log);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Correct for PCR fragment-length effects
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ces <- getChipEffectSet(plm);
+print(ces);
+fln <- FragmentLengthNormalization(ces, target="zero");
+print(fln);
+cesN <- process(fln, verbose=log);
+print(cesN);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Get total chip effect estimates, i.e. theta = thetaA + thetaB
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+cbs <- CbsModel(cesN);
+print(cbs);
+
+ce <- ChromosomeExplorer(cbs);
+process(ce, verbose=log);
