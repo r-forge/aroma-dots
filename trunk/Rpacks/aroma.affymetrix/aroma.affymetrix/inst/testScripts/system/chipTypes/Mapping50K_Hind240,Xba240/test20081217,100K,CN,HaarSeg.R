889801d22c51fb7199da7e29b5216f895ea1f293
hb <hb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-17 16:45:16 +0000

    Added more redundancy tests.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@1258 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/inst/testScripts/system/chipTypes/Mapping50K_Hind240,Xba240/test20081217,100K,CN,HaarSeg.R
@@ -0,0 +1,84 @@
+library(aroma.affymetrix)
+log <- Arguments$getVerbose(-4);
+timestampOn(log);
+.Machine$float.eps <- sqrt(.Machine$double.eps);
+
+dataSetName <- "HapMap270,100K,CEU,testSet";
+chipTypes <- c("Mapping50K_Hind240", "Mapping50K_Xba240");
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Tests for setting up CEL sets and locating the CDF file
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+csRawList <- list();
+for (chipType in chipTypes) {
+  cs <- AffymetrixCelSet$fromName(dataSetName, chipType=chipType, verbose=log);
+  print(cs);
+  csRawList[[chipType]] <- cs;
+}
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Allelic cross-talk calibration tests
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+csList <- csRawList;
+csAccList <- list();
+for (chipType in names(csList)) {
+  cs <- csList[[chipType]];
+  acc <- AllelicCrosstalkCalibration(cs);
+  print(acc);
+  csAcc <- process(acc, verbose=log);
+  print(csAcc);
+  csAccList[[chipType]] <- csAcc;
+}
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Probe-level modelling test (for CN analysis)
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+csList <- csAccList;
+cesCnList <- list();
+for (chipType in names(csList)) {
+  cs <- csList[[chipType]];
+  plm <- RmaCnPlm(cs, mergeStrands=TRUE, combineAlleles=TRUE, shift=300);
+  print(plm);
+  fit(plm, ram=1/2, verbose=log);
+  ces <- getChipEffectSet(plm);
+  print(ces);
+  cesCnList[[chipType]] <- ces;
+}
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Fragment-length normalization test
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+cesCnList <- cesCnList;
+cesFlnList <- list();
+for (chipType in names(csList)) {
+  ces <- cesCnList[[chipType]];
+  fln <- FragmentLengthNormalization(ces);
+  print(fln);
+  cesFln <- process(fln, verbose=verbose);
+  print(cesFln);
+  cesFlnList[[chipType]] <- cesFln;
+}
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Glad model test
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+seg <- HaarSegModel(cesFlnList);
+print(seg);
+
+fit(seg, arrays=1, chromosomes=19, verbose=log);
+
+# Tests the case where one of the set does not have observations.
+fit(seg, arrays=nbrOfArrays(seg), chromosomes=19, verbose=log);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# ChromosomeExplorer test
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ce <- ChromosomeExplorer(seg);
+print(ce);
+process(ce, arrays=1:2, chromosomes=c(19,23), verbose=log);
+
