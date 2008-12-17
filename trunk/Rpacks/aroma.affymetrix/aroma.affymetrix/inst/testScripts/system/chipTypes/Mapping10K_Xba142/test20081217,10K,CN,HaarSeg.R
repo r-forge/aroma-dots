dca1e6c63a0e15c3675ce76f3929d2f5ca8bef81
hb <unknown@noreply.com>
2008-12-17 16:45:16 +0000

    Added more redundancy tests.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@1258 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/Rpacks/aroma.affymetrix/aroma.affymetrix/inst/testScripts/system/chipTypes/Mapping10K_Xba142/test20081217,10K,CN,HaarSeg.R
@@ -0,0 +1,61 @@
+library(aroma.affymetrix)
+log <- Arguments$getVerbose(-4);
+timestampOn(log);
+.Machine$float.eps <- sqrt(.Machine$double.eps);
+
+dataSetName <- "Jeremy_2007-10k";
+chipType <- "Mapping10K_Xba142";
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Tests for setting up CEL sets and locating the CDF file
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+cs <- AffymetrixCelSet$fromName(dataSetName, chipType=chipType, verbose=log);
+keep <- 1:6;
+cs <- extract(cs, keep);
+print(cs);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Allelic cross-talk calibration tests
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+acc <- AllelicCrosstalkCalibration(cs);
+print(acc);
+csAcc <- process(acc, verbose=log);
+print(csAcc);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Probe-level modelling test (for CN analysis)
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+plm <- RmaCnPlm(csAcc, mergeStrands=TRUE, combineAlleles=TRUE, shift=300);
+print(plm);
+
+fit(plm, verbose=log);
+ces <- getChipEffectSet(plm);
+print(ces);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Fragment-length normalization test
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+fln <- FragmentLengthNormalization(ces);
+print(fln);
+cesFln <- process(fln, verbose=verbose);
+print(cesFln);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Glad model test
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+seg <- HaarSegModel(cesFln);
+print(seg);
+fit(seg, arrays=1, chromosomes=19, verbose=log);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# ChromosomeExplorer test
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ce <- ChromosomeExplorer(seg, zooms=2^(0:3));
+print(ce);
+process(ce, chromosomes=c(19,23), verbose=log);
+## process(ce, verbose=log);
