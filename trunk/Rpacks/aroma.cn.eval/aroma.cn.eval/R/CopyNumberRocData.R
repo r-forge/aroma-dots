572c05f6dc1849b206b50271000e624576df62bf
hb <hb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-02-08 00:41:07 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@1338 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Rpacks/aroma.cn.eval/aroma.cn.eval/R/CopyNumberRocData.R
+++ trunk/Rpacks/aroma.cn.eval/aroma.cn.eval/R/CopyNumberRocData.R
@@ -124,8 +124,20 @@ setMethodS3("nbrOfChannels", "CopyNumberRocData", function(this, ...) {
 })
 
 
-setMethodS3("getData", "CopyNumberRocData", function(this, ...) {
-  this$.data;
+setMethodS3("getData", "CopyNumberRocData", function(this, channels=NULL, ...) {
+  # Argument 'channels':
+  if (!is.null(channels)) {
+    nbrOfChannels <- nbrOfChannels(this);
+    channels <- Arguments$getIndices(channels, range=c(1, nbrOfChannels));
+  }
+  
+  res <- this$.data;
+
+  if (!is.null(channels)) {
+    res <- res[,channels,drop=FALSE];
+  }
+  
+  res;
 })
 
 setMethodS3("setData", "CopyNumberRocData", function(this, Y, ...) {
@@ -143,24 +155,43 @@ setMethodS3("setData", "CopyNumberRocData", function(this, Y, ...) {
   invisible(this);
 })
 
-setMethodS3("getPositions", "CopyNumberRocData", function(this, ...) {
+setMethodS3("getPositions", "CopyNumberRocData", function(this, channels=NULL, ...) {
+  # Argument 'channels':
+  if (!is.null(channels)) {
+    nbrOfChannels <- nbrOfChannels(this);
+    channels <- Arguments$getIndices(channels, range=c(1, nbrOfChannels));
+  }
+  
   res <- this$.positions;
+
   if (is.null(res)) {
     nbrOfPositions <- nbrOfLoci(this);
     res <- seq(length=nbrOfPositions);
   }
+
+  if (is.matrix(res)) {
+    if (!is.null(channels)) {
+      res <- res[,channels,drop=FALSE];
+    }
+  }
+
   res;
 })
 
-setMethodS3("getTruth", "CopyNumberRocData", function(this, ...) {
+setMethodS3("getTruth", "CopyNumberRocData", function(this, channels=NULL, ...) {
   nbrOfLoci <- nbrOfLoci(this);
   nbrOfChannels <- nbrOfChannels(this);
 
-  x <- getPositions(this);
+  if (is.null(channels)) {
+    channels <- seq(length=nbrOfChannels);
+  } else {
+    channels <- Arguments$getIndices(channels, range=c(1, nbrOfChannels));
+  }
 
   # Allocate results
-  T <- matrix(0L, nrow=nbrOfLoci, ncol=nbrOfChannels);
-  for (cc in seq(length=nbrOfChannels)) {
+  T <- matrix(0L, nrow=nbrOfLoci, ncol=length(channels));
+  for (cc in channels) {
+    x <- getPositions(this, channels=cc);
     T[,cc] <- getState(this, x=x, channel=cc, ...);
   } # for (cc ...)
 
@@ -177,16 +208,14 @@ setMethodS3("getState", "CopyNumberRocData", function(this, x=NULL, channel, ...
   # Argument 'channel':
   channel <- Arguments$getInteger(channel, range=c(1, nbrOfChannels(this)));
 
-
-  if (is.null(x)) {
-    x <- getPositions(this);
-  }
-
   fcn <- this$.truth;
   if (is.list(fcn)) {
     fcn <- fcn[[channel]];
   }
 
+  if (is.null(x)) {
+    x <- getPositions(this, channels=channel);
+  }
   t <- fcn(x, channel=channel, ...);
   t <- as.integer(t);
 
@@ -212,7 +241,7 @@ setMethodS3("hasState", "CopyNumberRocData", function(this, state, ...) {
 
 
 
-setMethodS3("plotTracks", "CopyNumberRocData", function(this, ..., xlim=NULL, ylim=c(-5,5), pch=19, xlab="Position", ylab="Signal") {
+setMethodS3("plotTracks", "CopyNumberRocData", function(this, ..., channels=NULL, xlim=NULL, ylim=c(-5,5), pch=19, xlab="Position", ylab="Signal") {
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
   # Local functions
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
@@ -225,20 +254,32 @@ setMethodS3("plotTracks", "CopyNumberRocData", function(this, ..., xlim=NULL, yl
   } # colorFcn()
 
 
-  Y <- getData(this);
-  T <- getTruth(this);
-  x <- getPositions(this);
-
-  if (is.null(xlim)) {
-    xlim <- range(x, na.rm=TRUE);
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  nbrOfChannels <- nbrOfChannels(this);
+  if (is.null(channels)) {
+    channels <- seq(length=nbrOfChannels);
+  } else {
+    channels <- Arguments$getIndices(channels, range=c(1, nbrOfChannels));
   }
 
-  iis <- seq(length=ncol(Y));
-  layout(matrix(iis, ncol=1));
+
+  if (is.null(xlim)) {
+    x <- getPositions(this);
+    xlim <- range(x, na.rm=TRUE);
+    rm(x);
+  }
+
+  layout(matrix(seq(along=channels), ncol=1));
   par(mar=c(3,3,1,1)+0.1, pch=pch);
-  for (ii in iis) {
+  for (cc in channels) {
     plot(NA, xlim=xlim, ylim=ylim);
-    points(x, Y[,ii], col=colorFcn(T[,ii]));
+    x <- getPositions(this, channel=cc);
+    Y <- getData(this, channel=cc);
+    T <- getState(this, x=x, channel=cc);
+    col <- colorFcn(T);
+    points(x, Y, col=col);
   }
 })
 
@@ -253,7 +294,11 @@ setMethodS3("extractSubset", "CopyNumberRocData", function(this, rows, ...) {
   data <- getData(this);
   positions <- getPositions(this);
   data <- data[rows,,drop=FALSE];
-  positions <- positions[rows];
+  if (is.matrix(positions)) {
+    positions <- positions[rows,,drop=FALSE];
+  } else {
+    positions <- positions[rows];
+  }
 
   res <- clone(this);
   clearCache(res);
@@ -336,11 +381,7 @@ setMethodS3("smoothByState", "CopyNumberRocData", function(this, xOut=NULL, ...,
 
 
   verbose && enter(verbose, "Smoothing data set");
-  Y <- getData(this);
   x <- getPositions(this);
-
-  # Identify CN states
-  T <- getTruth(this);
 
   if (is.null(xOut)) {
     xOut <- x;
@@ -356,8 +397,10 @@ setMethodS3("smoothByState", "CopyNumberRocData", function(this, xOut=NULL, ...,
   for (cc in seq(length=nbrOfChannels)) {
     verbose && enter(verbose, sprintf("Channel #%d of %d", cc, nbrOfChannels));
 
-    Ycc <- Y[,cc];
-    Tcc <- T[,cc];
+    Ycc <- getData(this, channels=cc);
+    Tcc <- getState(this, x=x, channel=cc);
+    x <- getPositions(this, channels=cc);
+
     states <- unique(as.vector(Tcc));
     states <- sort(states, na.last=FALSE);
     verbose && cat(verbose, "CN states:");
@@ -423,294 +466,6 @@ setMethodS3("smoothByState", "CopyNumberRocData", function(this, xOut=NULL, ...,
 
   res;
 }) # smoothByState()
-
-
-
-
-setMethodS3("scanTpAtFp", "CopyNumberRocData", function(this, fpRate, hs=seq(from=1, to=10, by=0.1), ..., force=FALSE, cache=TRUE, verbose=FALSE) {
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  # Validate arguments
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  # Argument 'verbose':
-  verbose <- Arguments$getVerbose(verbose);
-  if (verbose) {
-    pushState(verbose);
-    on.exit(popState(verbose));
-  }
-
-
-  verbose && enter(verbose, "Estimating the TP rate at a given FP rate for various amounts of smoothing");
-  verbose && cat(verbose, "Smooth levels:");
-  verbose && print(verbose, hs);
-
-  verbose && enter(verbose, "Loading earlier estimates from file cache");
-  key <- list(method="extractSmoothRocData", class=class(this)[1], checksum=getChecksum(this), fpRate=fpRate);
-  dirs <- c("CopyNumberRocData", "scanTpAtFp");
-  fit <- loadCache(key=key, dirs=dirs);
-  verbose && exit(verbose);
-
-#  # Merge with memory cache  
-#  fit <- this$.scanTpAtFpFit;
-
-  if (force)
-    fit <- NULL;
-
-  data <- getData(this, raw=TRUE);
-  truth <- getTruth(this, raw=TRUE);
-  if (!is.matrix(truth))
-    truth <- matrix(truth, nrow=nrow(data), ncol=ncol(data), byrow=TRUE);
-
-  fit <- scanRocTpAtFp(truth=truth, data=data, fpRate=fpRate, hs=hs, fit=fit, verbose=verbose);
-
-#  this$.scanTpAtFpFit <- fit;
-
-  # Cache results?
-  if (cache)
-    saveCache(fit, key=key, dirs=dirs);
-
-  verbose && enter(verbose, "Extract smooth levels of interest");
-  keep <- whichVector(fit[,"h"] %in% hs);
-  fit <- fit[keep,,drop=FALSE];
-  verbose && print(verbose, fit);
-  verbose && exit(verbose);
-
-  verbose && exit(verbose);
-
-  fit;
-})
-
-
-
-setMethodS3("findTpAtFpPerRow", "CopyNumberRocData", function(this, fpRate=0.01, rows=NULL, ..., skip=FALSE, force=FALSE, verbose=FALSE) {
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  # Validate arguments
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  dim <- dim(this);
-  if (is.null(dim)) {
-    throw("Cannot fit TP rate per data row. Data is not specified as a matrix: ", dim);
-  }
-
-  # Argument 'rows':
-  if (!is.null(rows)) {
-    rows <- Arguments$getIndices(rows, range=c(1, dim[1]));
-  }
-
-  # Argument 'verbose':
-  verbose <- Arguments$getVerbose(verbose);
-  if (verbose) {
-    pushState(verbose);
-    on.exit(popState(verbose));
-  }
-
-
-  verbose && enter(verbose, "Estimating the TP rate per data row");
-  verbose && cat(verbose, "Rows:");
-  verbose && str(verbose, rows);
-
-  # Load cached results
-  cache <- loadCachedResult(this, method="findTpAtFpPerRow", fpRate=fpRate, rows=NULL, skip=skip);
-
-  # Get existing estimates
-  if (is.null(this$.tpDensityList))
-    this$.tpDensityList <- list();
-  fpRateStr <- sprintf("fpRate=%f", fpRate);
-  tpDensity <- this$.tpDensityList[[fpRateStr]];
-  if (is.null(tpDensity)) {
-    if (force || is.null(cache$result)) {
-      tpDensity <- rep(-1, dim[1]);
-    } else {
-      tpDensity <- cache$result;
-    }
-    this$.tpDensityList[[fpRateStr]] <- tpDensity;
-  }
-
-  # Keep subset of interest?
-  if (!is.null(rows))
-    tpDensity <- tpDensity[rows];
-
-  verbose && cat(verbose, "Existing TP-rate estimates:");
-  verbose && str(verbose, tpDensity);
-  verbose && summary(verbose, tpDensity);
-
-  # Identify data rows to update
-  if (force) {
-    rrs <- seq(along=tpDensity);
-  } else {
-    rrs <- whichVector(is.finite(tpDensity) & tpDensity < 0);
-  }
-
-  # Nothing more to do?
-  n <- length(rrs);
-  if (n == 0) {
-    verbose && cat(verbose, "Already done!");
-    verbose && exit(verbose);
-    return(tpDensity);
-  }
-
-  # Extract truth and data
-  verbose && enter(verbose, "Extracting truth and data");
-  truth <- getTruth(this, ordered=FALSE, complete=FALSE, rows=rows);
-  dimnames(truth) <- NULL;
-  verbose && cat(verbose, "Truth:");
-  verbose && str(verbose, truth);
-  data <- getData(this, ordered=FALSE, complete=FALSE, rows=rows);
-  dimnames(data) <- NULL;
-  verbose && cat(verbose, "Data:");
-  verbose && str(verbose, data);
-  verbose && exit(verbose);
-
-  # For each data row
-  verbose && enter(verbose, "Processing all data rows");
-  verbose && cat(verbose, "Number of data rows to do: ", n);
-  for (kk in seq(along=rrs)) {
-    rr <- rrs[kk];
-
-    # Store every 100th iteration
-    if (kk %% 500 == 0) {
-      if (is.null(rows)) {
-        this$.tpDensityList[[fpRateStr]] <- tpDensity;
-      } else {
-        this$.tpDensityList[[fpRateStr]][rows] <- tpDensity;
-      }
-      verbose && printf(verbose, "Data row #%d of %d\n", kk, n);
-    }
-
-    t <- truth[rr,];
-    d <- data[rr,];
-    fit <- findRocTpAtFp(truth=t, data=d, fpRate=fpRate, hasNAs=TRUE, isOrdered=FALSE, .checkArgs=FALSE);
-#    print(data.frame(truth=t, data=d));
-#    print(fit);
-    tpRate <- fit[["tpRateEst"]]; 
-    tpDensity[rr] <- tpRate;
-  } # for (kk in ...)
-  verbose && exit(verbose);
-
-  verbose && enter(verbose, "Storing estimates in memory cache");
-  # Store estimates
-  if (is.null(rows)) {
-    this$.tpDensityList[[fpRateStr]] <- tpDensity;
-  } else {
-    this$.tpDensityList[[fpRateStr]][rows] <- tpDensity;
-  }
-  verbose && exit(verbose);
-
-  verbose && exit(verbose);
-
-  # Return updates
-  res <- tpDensity;
-
-##  verbose && str(verbose, tpDensity);
-##  verbose && summary(verbose, tpDensity);
-
-  # Cache result?
-  doCache <- (is.null(rows) || length(rows) > 1000);
-  doCache <- TRUE;
-  if (doCache) {
-    if (!is.null(rows)) {
-      tpDensity <- rep(-1, dim[1]);
-      tpDensity[rows] <- res;
-    }
-
-##    verbose && str(verbose, tpDensity);
-##    verbose && summary(verbose, tpDensity);
-
-    if (is.null(cache$key)) {
-      cache <- loadCachedResult(this, method="findTpAtFpPerRow", fpRate=fpRate, rows=NULL, skip=FALSE);
-    }
-
-##    verbose && str(verbose, tpDensity);
-##    verbose && summary(verbose, tpDensity);
-
-    saveCache(tpDensity, key=cache$key, dirs=cache$dirs);
-  }
-
-  res;
-}) # findTpAtFpPerRow()
-
-
-
-setMethodS3("drawTpRateDensity", "CopyNumberRocData", function(this, fpRate=0.01, rows=NULL, ..., adjust=1, lwd=2, col=par("col"), cex=0.7, annotate=!add, add=FALSE, force=FALSE, verbose=FALSE) {
-  require("aroma.core") || throw("Package not loaded: aroma.core"); # stext()
-
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  # Validate arguments
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  tpRates <- findTpAtFpPerRow(this, fpRate=fpRate, rows=rows, force=force, verbose=verbose);
-  n <- length(tpRates);
-
-  # Filter out non-finite estimates
-  keep <- is.finite(tpRates);
-  tpRates <- tpRates[keep];
-  rm(keep);
-
-  callRate <- length(tpRates)/n;
-  d <- density(tpRates, from=0, to=1, adjust=adjust);
-
-  if (add) {
-    lines(d, lwd=lwd, col=col, ...);
-  } else {
-    tpLab <- "TP rate";
-    ylab <- "Density (integrates to one)";
-    plot(d, lwd=lwd, col=col, main="", xlab=tpLab, ylab=ylab, ...);
-  }
-
-  if (annotate) {
-    text <- sprintf("Call rate: %.3f%% (n=%d)", 100*callRate, n);
-    stext(side=3, pos=1, text, cex=cex);
-  }
-
-  invisible(d);
-})
-
-
-
-setMethodS3("plotTpRateDensity", "CopyNumberRocData", function(this, fpRate=0.01, rows=NULL, ..., col=par("col"), cex=0.7, breaks=NULL, offset=0, width=1, xlab="TP rate", ylab="Count", annotate=!add, add=FALSE, force=FALSE, verbose=FALSE) {
-  require("R.basic") || throw("Package not loaded: R.basic"); # hist()
-  require("aroma.core") || throw("Package not loaded: aroma.core"); # stext()
-
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  # Validate arguments
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  # Argument 'breaks':
-  if (!is.null(breaks)) {
-    breaks <- Arguments$getDoubles(breaks);
-  }
-
-  # Argument 'offset':
-  offset <- Arguments$getDouble(offset, range=c(0,Inf));
-
-  # Argument 'width':
-  width <- Arguments$getDouble(width, range=c(0,Inf));
-
-  tpRates <- findTpAtFpPerRow(this, fpRate=fpRate, rows=rows, 
-                                             force=force, verbose=verbose);
-  n <- length(tpRates);
-
-  # Filter out non-finite estimates
-  keep <- is.finite(tpRates);
-  tpRates <- tpRates[keep];
-  rm(keep);
-
-  res <- hist(tpRates, breaks=breaks, offset=offset, width=width, 
-              freq=TRUE, col=col, border=NA, add=add, 
-              xlab=xlab, ylab=ylab, main="");
-
-  if (annotate) {
-    callRate <- length(tpRates)/n;
-    text <- sprintf("Call rate: %.3f%% (n=%d)", 100*callRate, n);
-    stext(side=3, pos=1, text, cex=cex);
-  }
-
-  invisible(res);
-})
-
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-#
-#   END: Methods that requires matrix data
-#
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
 
