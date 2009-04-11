7520cc33d29ecc555d834223a75a9c1a96811ce1
hb <unknown@noreply.com>
2009-04-11 21:54:00 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@1429 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Rpacks/aroma.cn.eval/aroma.cn.eval/inst/vignettes/GSE13372,HCC1143/R/getCnList.R
+++ trunk/Rpacks/aroma.cn.eval/aroma.cn.eval/inst/vignettes/GSE13372,HCC1143/R/getCnList.R
@@ -1,15 +1,31 @@
-getCnList <- function(cnList, ..., verbose=TRUE) {
+getCnList <- function(cnList, ..., what=c("log2ratios", "ratios"), verbose=TRUE) {
+  # Nothing todo?
   if (!is.null(cnList)) {
     return(cnList);
   }
+
+  # Arguments 'what':
+  what <- match.arg(what);
+
   cnList <- extractListOfCopyNumbers(dsList, region, truth=truth,
                             targetChipType=targetChipType, verbose=verbose);
+
+  # CN ratios
+  if (what == "ratios") {
+    cnList <- lapply(cnList, FUN=function(cn) {
+      cn$y <- 2*2^cn$y;
+      cn;
+    });
+  }
+
   cnList;
 } # getCnList()
 
 
 ############################################################################
 # HISTORY:
+# 2009-04-09
+# o Added argument 'what'.
 # 2009-02-23
 # o Created.
 ############################################################################
