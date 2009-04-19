42c146933b9b355a00d2b615023e170ecd898163
hb <hb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-04-19 04:53:15 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@1437 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/Rpacks/aroma.tcga/aroma.tcga/R/999.package.R
@@ -0,0 +1,37 @@
+#########################################################################/**
+# @RdocPackage aroma.tcga
+#
+# \description{
+#   @eval "getDescription(aroma.tcga)" 
+#
+#   This package should be considered to be in an alpha or beta phase.
+#   You should expect the API to be changing over time.
+#   Consider this package a private package.
+# }
+#
+# \section{Installation and updates}{
+#
+#   To install this package, see instructions at 
+#   \url{http://www.braju.com/R/}.
+# } 
+#
+# \author{
+#   Henrik Bengtsson.
+# }
+#
+# \section{License}{
+#   The releases of this package is licensed under 
+#   LGPL version 2.1 or newer.
+#
+#   The development code of the packages is under a private licence 
+#   (where applicable) and patches sent to the author fall under the
+#   latter license, but will be, if incorporated, released under the
+#   "release" license above.
+# }
+# 
+# \references{
+#  Some of the reference below can be found at 
+#  \url{http://www.maths.lth.se/bioinformatics/publications/}.\cr
+# }
+#*/#########################################################################
+
