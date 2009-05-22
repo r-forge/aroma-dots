0f3551e8e69d35f365ff0b6d0c911c68c4f5b599
hb <unknown@noreply.com>
2009-05-22 05:40:10 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@1502 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Rpacks/aroma.tcga/redundancyTests/R/MageTabFile.R
+++ trunk/Rpacks/aroma.tcga/redundancyTests/R/MageTabFile.R
@@ -6,45 +6,6 @@ setConstructorS3("MageTabFile", function(..., .verify=TRUE) {
   this;
 })
 
-setConstructorS3("MageTabDataMatrixFile", function(..., .verify=TRUE) {
-  this <- extend(MageTabFile(..., .verify=FALSE), "MageTabDataMatrixFile");
-
-#  if (.verify)
-#    verify(this, ..., verbose=verbose);
-  this;
-})
-
-
-
-setMethodS3("getHeader", "MageTabDataMatrixFile", function(this, ..., force=FALSE) {
-  hdr <- this$.fileHeader;
-  if (force || is.null(hdr)) {
-    hdr <- readRawHeader(this, ...);
-    if (hasColumnHeader(this)) {
-      header <- list();
-      for (kk in 1:2) {
-        tmp <- hdr$topRows[[kk]];
-        header[[tmp[1]]] <- tmp[-1];
-      }
-      hdr$dataMatrixHeader <- header;
-      colnames <- sprintf("%s,%s", header[[1]], header[[2]]);
-      colnames <- gsub("(^,|,$)", "", colnames);
-      colnames <- gsub("(Rtum/Rnorm)", "Ratio", colnames, fixed=TRUE);
-      colnames <- c(names(header)[2], colnames);
-      hdr$columns <- colnames;
-      hdr$skip <- hdr$skip + 1L;
-    }
-    this$.fileHeader <- hdr;
-  }
-  hdr;
-})
-
-
-
-setMethodS3("getReadArguments", "MageTabDataMatrixFile", function(this, ..., colClassPatterns=c("*"="character", "(Signal)$"="double")) {
-  NextMethod("getReadArguments", this, ..., colClassPatterns=colClassPatterns);
-}, protected=TRUE);
-
 
 
 ############################################################################
