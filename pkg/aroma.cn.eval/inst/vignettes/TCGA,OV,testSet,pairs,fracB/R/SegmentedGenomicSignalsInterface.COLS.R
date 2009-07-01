a3cb733997c5ddb3bad6157c99ab58cbbf25b1f3
henrikb (Henrik Bengtsson) <henrikb@braju.com>
2009-07-01 02:09:12 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@1591 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.cn.eval/inst/vignettes/TCGA,OV,testSet,pairs,fracB/R/SegmentedGenomicSignalsInterface.COLS.R
+++ pkg/aroma.cn.eval/inst/vignettes/TCGA,OV,testSet,pairs,fracB/R/SegmentedGenomicSignalsInterface.COLS.R
@@ -13,53 +13,10 @@ setMethodS3("getStateColorMap", "SegmentedGenomicSignalsInterface", function(thi
   colorMap;
 })
 
-setMethodS3("setStateColorMap", "SegmentedGenomicSignalsInterface", function(this, colorMap, ...) {
-  # Argument 'colorMap':
-  names <- names(colorMap);
-  colorMap <- Arguments$getCharacters(colorMap);
-  names(colorMap) <- names;
-  if (is.null(names(colorMap))) {
-    throw("Argument 'colorMap' must be a named vector.");
-  }
-  this$.stateColorMap <- colorMap;
-})
-
-
-setMethodS3("getStateColors", "SegmentedGenomicSignalsInterface", function(this, na.rm=FALSE, ...) {
-  colorMap <- getStateColorMap(this);
-  if (na.rm) {
-    colorMap["NA"] <- as.character(NA);
-  }
-  hasDefColor <- is.element("*", names(colorMap));
-
-  states <- getStates(this);
-  print(table(states, exclude=NULL));
-  uStates <- sort(unique(states), na.last=TRUE);
-  uStates <- na.omit(uStates);
-
-  naColor <- as.character(colorMap["NA"]);
-  cols <- rep(naColor, times=length(states));
-  for (kk in seq(along=uStates)) {
-    state <- uStates[kk];
-    key <- sprintf("%s", state);
-
-    if (!is.element(key, names(colorMap))) {
-      if (!hasDefColor) {
-        throw("State does not exist in color map: ", state);
-      }
-      key <- "*";
-    }
-    col <- colorMap[key];
-    idxs <- whichVector(states == state);
-    cols[idxs] <- col;
-  } # for (kk ...)
-
-  cols;
-})
-
-
 ############################################################################
 # HISTORY:
+# 2009-06-30
+# o Moved most functions to aroma.core.
 # 2009-06-29
 # o Created.
 ############################################################################
