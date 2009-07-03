5730340cf2021a9ad7def6274332fa874d47b878
henrikb (Henrik Bengtsson) <henrikb@braju.com>
2009-07-03 06:20:58 +0000

    Now the vignette generates PNG devices.  Added figDevice() and pngDev().  Verified to work.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@1600 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/aroma.cn.eval/inst/vignettes/TCGA,OV,testSet,pairs,fracB/R/figDevice.R
@@ -0,0 +1,51 @@
+setMethodS3("figDevice", "default", function(label, width, height=aspect*width, aspect=1, device, extension, ..., path="figures", safe=TRUE, force=FALSE) {
+  # Argument 'label':
+  label <- Arguments$getCharacter(label);
+
+  # Argument 'device':
+  if (!is.function(device)) {
+    throw("Argument 'device' is not a function: ", mode(device));
+  }
+
+  # Argument 'extension':
+  extension <- Arguments$getCharacter(extension);
+
+  # Argument 'path':
+  path <- Arguments$getWritablePath(path);
+
+
+  fullname <- label;
+  # Encode?
+  if (safe) {
+    fullname <- gsub(".", "_", label, fixed=TRUE);
+  }
+
+  filename <- sprintf("%s.%s", fullname, extension);
+  pathname <- Arguments$getWritablePathname(filename, path=path);
+  res <- FALSE;
+  if (force || !isFile(pathname)) {
+    devNew(device, pathname, width=width, height=height, ..., label=label);
+    res <- TRUE;
+  }
+
+  res <- Object(res);
+  res$label <- label;
+  res$fullname <- fullname;
+  res$pathname <- pathname;
+
+  invisible(res);
+}, protected=TRUE) # figDevice()
+
+
+pngDev <- function(..., width=840, device=findPngDevice()) {
+  figDevice(..., width=width, device=device, extension="png");
+} # pngDev()
+
+
+
+############################################################################
+# HISTORY:
+# 2009-07-02
+# o Added generic figDevice().
+# o Created.
+############################################################################
