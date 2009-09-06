49f8d072fa24feacace08cdb2515fc0d36983cfa
henrikb <henrikb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-06 21:42:50 +0000

    Package: EBImage Version: 999.999.999 Date: 2009-09-06 Title: DUMMY DUMMY DUMMY Author: Henrik Bengtsson <henrikb@braju.com> Maintainer: Henrik Bengtsson <henrikb@braju.com> Depends: R (>= 2.9.0) Description: DO NOT INSTALL THIS PACKAGE.  THIS IS AN EMPTY PACKAGE WITH THE SAME NAME AS AN EXISTING BIOC PACKAGE. THE ONLY PURPOSE OF THIS PACKAGE IS TO PREVENT THE R-FORGE BUILD SERVERS TO HALT THE BUILDING OF AROMA.CORE AND AROMA.AFFYMETRIX.  THIS DUMMY PACKAGE WILL NOT BE NEEDED WHEN R-FORGE SETS THE ENVIRONMENT VARIABLE R_check_force_suggests=false License: Private LazyLoad: TRUE
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@1630 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/EBImage/R/EBImage-package.R
@@ -0,0 +1,13 @@
+#########################################################################/** 
+# @RdocPackage "EBImage"
+#
+# @title "Dummy package EBImage"
+# 
+# \description{
+# \preformatted{
+# @include "../DESCRIPTION"
+# }
+# }
+# 
+# @keyword package
+#*/######################################################################### 
