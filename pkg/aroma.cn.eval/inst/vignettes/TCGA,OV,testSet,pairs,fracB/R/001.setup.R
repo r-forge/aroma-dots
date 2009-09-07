31aef59f7f3da4343b2cec8e00d0bce606b154ab
pierre (Pierre Neuvial) <pierre.neuvial@genopole.cnrs.fr>
2009-09-07 01:14:07 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@1632 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.cn.eval/inst/vignettes/TCGA,OV,testSet,pairs,fracB/R/001.setup.R
+++ pkg/aroma.cn.eval/inst/vignettes/TCGA,OV,testSet,pairs,fracB/R/001.setup.R
@@ -3,8 +3,8 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 rootPath <- "totalAndFracBData"
 dataSet <- "TCGA,OV,testSet,pairs,Broad,ismpolish";
-dataSet <- "TCGA,OV,testSet,pairs,Broad,ASCN";
-dataSet <- "TCGA,OV,testSet,pairs,Broad,ACC,ra,-XY,BPN,-XY,AVG,FLN,-XY";
+## dataSet <- "TCGA,OV,testSet,pairs,Broad,ASCN";
+## dataSet <- "TCGA,OV,testSet,pairs,Broad,ACC,ra,-XY,BPN,-XY,AVG,FLN,-XY";
 chipType <- "GenomeWideSNP_6";
 targetChipType <- chipType;
 
@@ -22,14 +22,17 @@ regions <- c(
              "TCGA-04-1336:Chr2@120.0-230.0,cp=176.0+/-2.0,s=0/2",
              "TCGA-23-1027:Chr2@108-140,cp=124+/-0.5,s=0/1",
              "TCGA-23-1027:Chr2@125.0-157.0,cp=141.0+/-0.5,s=1/3",
-             "TCGA-23-1027:Chr2@25-75.0,cp=50.0+/-0.5,s=0/1", ## "FALSE BREAKPOINT"
+             "TCGA-23-1027:Chr2@25.0-75.0,cp=50.0+/-0.5,s=0/1", ## "FALSE BREAKPOINT"
              "TCGA-23-1027:Chr1@165-245.0,cp=201.0+/-0.5,s=0/1",
              "TCGA-23-1027:Chr1@0-120,cp=60+/-0.5,s=0/1",   ## CN LOH within a deletion (+more)
              "TCGA-23-1027:Chr10@80-109,cp=94+/-0.5,s=0/2", ## deletion
-             "TCGA-23-1027:Chr10@106.5-113.5,cp=110+/-0.5,s=2/3" ## deletion -> CN LOH
+##              "TCGA-23-1027:Chr10@106.4-113.6,cp=110+/-0.25,s=2/3", ## deletion -> CN LOH
+             "TCGA-23-1027:Chr10@106.5-113.5,cp=110+/-0.5,s=2/3", ## deletion -> CN LOH
+             "TCGA-23-1027:Chr2@0-100.0,cp=50.0+/-0.5,s=0/1" ## "FALSE BREAKPOINT #2"
              );
 
 ## regions <- regions[1:5]
-##regions <- regions[9]
+## regions <- regions[9]
 regions <- regions[c(3:4, 8:9)]
+## regions <- regions[10]
 
