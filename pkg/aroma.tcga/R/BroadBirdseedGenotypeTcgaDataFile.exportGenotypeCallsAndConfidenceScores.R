2a4852690a2a4f7325c64008b260541601aa1d8e
henrikb (Henrik Bengtsson) <henrikb@braju.com>
2009-11-03 04:10:57 +0000

    Version: 0.2.2 [2009-11-02] o Added exportGenotypeCallsAndConfidenceScores() to   BroadBirdseedGenotypeTcgaDataFileSet.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@1715 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.tcga/R/BroadBirdseedGenotypeTcgaDataFile.exportGenotypeCallsAndConfidenceScores.R
+++ pkg/aroma.tcga/R/BroadBirdseedGenotypeTcgaDataFile.exportGenotypeCallsAndConfidenceScores.R
@@ -14,10 +14,11 @@
 #
 # \arguments{
 #   \item{dataSet}{@character, data set name.}
-#   \item{chipType}{@character, chip type.}
+#   \item{unf}{A @see "aroma.core::UnitNamesFile".}
 #   \item{...}{arguments passed to 
 #      \code{AromaUnitGenotypeCallFile$allocateFromUnitNamesFile(...)}.}
 #   \item{rootPath}{@character, root path for output files.}
+#   \item{force}{If @TRUE, existing output files are rewritten.}
 #   \item{verbose}{@see "Verbose" object.}
 # }
 #
@@ -36,25 +37,24 @@
 #   latter is a p-value.
 # }
 #
-# \note{
-#   The implementation requires \pkg{aroma.affymetrix} only for the
-#   inference of a @see "aroma.core::UnitNamesFile".  However it is tied
-#   to Affymetrix data anyway because Birdseed only supports Affymetrix data.
-# }
-#
 #*/###########################################################################
-setMethodS3("exportGenotypeCallsAndConfidenceScores", "BroadBirdseedGenotypeTcgaDataFile", function(this, dataset, chipType, ..., rootPath="callData", verbose=FALSE) {
+setMethodS3("exportGenotypeCallsAndConfidenceScores", "BroadBirdseedGenotypeTcgaDataFile", function(this, dataSet, unf, ..., rootPath="callData", force=FALSE, verbose=FALSE) {
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # Validate arguments
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # Argument 'dataSet':
   dataSet <- Arguments$getCharacter(dataSet, length=c(1,1));
 
-  # Argument 'chipType':
-  chipType <- Arguments$getCharacter(chipType, length=c(1,1));
+  # Argument 'unf':
+  if (!inherits(unf, "UnitNamesFile")) {
+    throw("Argument 'unf' is not a UnitNamesFile: ", class(unf)[1]);
+  }
 
   # Argument 'rootPath':
   rootPath <- Arguments$getWritablePath(rootPath);
+
+  # Argument 'force':
+  force <- Arguments$getLogical(force);
 
   # Argument 'verbose':
   verbose <- Arguments$getVerbose(verbose);
@@ -63,143 +63,179 @@ setMethodS3("exportGenotypeCallsAndConfidenceScores", "BroadBirdseedGenotypeTcga
     on.exit(popState(verbose));
   }
 
+
+  verbose && enter(verbose, "Exporting ", class(this)[1]);
+  chipType <- getChipType(unf, fullname=FALSE);
+  verbose && cat(verbose, "Chip type: ", chipType);
+  
   path <- file.path(rootPath, dataSet, chipType);
+  path <- Arguments$getWritablePath(path);
+  verbose && cat(verbose, "Path: ", path);
 
   fullname <- getFullName(this);
+  verbose && cat(verbose, "Fullname: ", fullname);
+
   verbose && enter(verbose, "Exporting genotype calls and confidence scores for sample ", fullname);
-  # Nothing to do ?
-  genoFilename <- sprintf("%s,genotypes.acf", fullname);
-  genoPathname <- Arguments$getWritablePathname(genoFilename, path=path, mustNotExist=FALSE);
-  confFilename <- sprintf("%s,confidenceScores.asb", fullname);
-  confPathname <- Arguments$getWritablePathname(confFilename, path=path, mustNotExist=FALSE);
-  
-  if (isFile(genoPathname) && isFile(confPathname)) {
-    acf <- AromaUnitSignalBinaryFile(genoPathname);
+
+  filename <- sprintf("%s,genotypes.acf", fullname);
+  acfPathname <- Arguments$getWritablePathname(filename, path=path, mustNotExist=FALSE);
+  if (!force && isFile(acfPathname)) {
+    acf <- AromaUnitGenotypeCallFile(acfPathname);
     verbose && cat(verbose, "Already exported: ", getPathname(acf));
-    asf <- AromaUnitSignalBinaryFile(confPathname);
+  } else {
+    acf <- NULL;
+  }
+
+  filename <- sprintf("%s,confidenceScores.asb", fullname);
+  asfPathname <- Arguments$getWritablePathname(filename, path=path, mustNotExist=FALSE);
+  if (!force && isFile(asfPathname)) {
+    asf <- AromaUnitSignalBinaryFile(asfPathname);
     verbose && cat(verbose, "Already exported: ", getPathname(asf));
   } else {
-    require("aroma.affymetrix") || 
-      throw("Package not loaded: aroma.affymetrix");
-    # Affy-specific
-    verbose && enter(verbose, "Retrieving UnitNamesFile from chipType");
-    unf <- AffymetrixCdfFile$byChipType(chipType, verbose=verbose);
-    verbose && exit(verbose);
-    
-    verbose && enter(verbose, "Defining file header");
-    srcFile <- list(filename = fullname,
-                    fileSize = getFileSize(this),
-                    checkSum = getChecksum(this));
-    verbose && exit(verbose);
-      
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    # Transforming data
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    verbose && enter(verbose, "Extracting calls and confidence scores");
-    calls <- extractCalls(this);
-    verbose && str(verbose, calls);
-    verbose && print(verbose, summary.factor(calls));
-    
-    conf <- 1-extractConfidenceScores(this); # input data are p-values...
-    verbose && str(verbose, conf);
-    
-    verbose && enter(verbose, "Ordering units according to UnitNamesFile");
-    data <- readDataFrame(df);
-    units <- indexOf(unf, names=data[, "CompositeElement REF"]);
-    stopifnot(!anyMissing(units));
-    verbose && exit(verbose);
-    
-    rm(data);
-    verbose && exit(verbose);
+    asf <- NULL;
+  }
 
-    verbose && enter(verbose, "Extracting genotype calls");
-    filename <- sprintf("%s,genotypes.acf", fullname);
+  res <- list(acf=acf, asf=asf);
+
+  # Nothing to do?
+  if (!is.null(res$acf) && !is.null(res$asf)) {
+    verbose && cat(verbose, "Nothing to do.");
+    verbose && cat(verbose, "Existing data files:");
+    verbose && print(verbose, res);
+    verbose && exit(verbose);
+    return(invisible(res));
+  }
+
+
+
+  verbose && enter(verbose, "Building file footer");
+  srcFile <- list(filename = fullname,
+                  fileSize = getFileSize(this),
+                  checkSum = getChecksum(this));
+  verbose && exit(verbose);
+      
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Transforming data
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  verbose && enter(verbose, "Reading data");
+  verbose && enter(verbose, "Extracting calls and confidence scores");
+  calls <- extractCalls(this);
+  verbose && str(verbose, calls);
+  verbose && print(verbose, summary.factor(calls));
     
-    pathname <- Arguments$getWritablePathname(filename, path=path, mustNotExist=FALSE);
+  conf <- 1-extractConfidenceScores(this); # input data are p-values...
+  verbose && str(verbose, conf);
+  verbose && exit(verbose);
     
-    if (isFile(pathname)) {
-      acf <- AromaUnitSignalBinaryFile(pathname);
-      verbose && cat(verbose, "Already exported: ", getPathname(acf));
-    } else {
-      
-      # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-      # Writing data
-      # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-      pathnameT <- sprintf("%s.tmp", pathname);
-      pathnameT <- Arguments$getWritablePathname(pathnameT, mustNotExist=TRUE);
-      on.exit({
-        file.remove(pathnameT);
-      }, add=TRUE);
-      
-      acf <- AromaUnitGenotypeCallFile$allocateFromUnitNamesFile(unf, filename=pathnameT, overwrite=FALSE, ...);
-      
-      updateGenotypes(acf, units=units, calls=calls, encoding="birdseed", verbose=verbose);
-      verbose && exit(verbose);
-      
-      footer <- readFooter(acf);
-      footer$srcFile <- srcFile;
-      writeFooter(acf, footer);
-      
-      # Rename temporary file
-      res <- file.rename(pathnameT, pathname);
-      if (!isFile(pathname)) {
-        throw("Failed to rename temporary file: ", pathnameT, " -> ", pathname);
-      }
+  verbose && enter(verbose, "Ordering according to UnitNamesFile");
+  data <- readDataFrame(this);
+  unitNames <- data[,"CompositeElement REF", drop=TRUE];
+  rm(data);
+  verbose && cat(verbose, "Unit names:");
+  verbose && str(verbose, unitNames);
+  units <- indexOf(unf, names=unitNames);
+  verbose && cat(verbose, "Units:");
+  verbose && str(verbose, units);
+  stopifnot(!anyMissing(units));
+  verbose && exit(verbose);
+  verbose && exit(verbose);
+
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Step 1. Genotype calls
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  if (is.null(res$acf)) {
+    verbose && enter(verbose, "Exporting genotype calls");
+    pathname <- acfPathname;
+    pathnameT <- sprintf("%s.tmp", pathname);
+    pathnameT <- Arguments$getWritablePathname(pathnameT, mustNotExist=TRUE);
+    on.exit({
       if (isFile(pathnameT)) {
-        throw("Failed to remove temporary file: ", pathnameT);
-      }
-    }
-    
-    verbose && enter(verbose, "Extracting confidence scores");
-    filename <- sprintf("%s,confidenceScores.asb", fullname);
-    
-    srcFile$filename <- filename;
-    
-    pathname <- Arguments$getWritablePathname(filename, path=path, mustNotExist=FALSE);
-    
-    if (isFile(pathname)) {
-      asf <- AromaUnitSignalBinaryFile(pathname);
-      verbose && cat(verbose, "Already exported: ", getPathname(asf));
-    } else {
-      pathnameT <- sprintf("%s.tmp", pathname);
-      pathnameT <- Arguments$getWritablePathname(pathnameT, mustNotExist=TRUE);
-      on.exit({
         file.remove(pathnameT);
-      }, add=TRUE);
+      }
+    }, add=TRUE);
       
-      asf <- AromaUnitSignalBinaryFile$allocateFromUnitNamesFile(unf, filename=pathnameT, types="double", size=4, signed=TRUE, overwrite=FALSE, ...);
-      naValue <- as.integer(NA);
-      asf[, 1] <- naValue;
-      asf[units, 1] <- conf;
-      isNC <- (conf==0);
-      if (sum(isNC)) {
-        idxs <- units[whichVector(isNC)];
-        asf[idxs, 1] <- naValue;
-      }
-      footer <- readFooter(asf);
-      footer$srcFile <- srcFile;
-      writeFooter(asf, footer);
+    acf <- AromaUnitGenotypeCallFile$allocateFromUnitNamesFile(unf, 
+             filename=pathnameT, overwrite=force, ...);
       
-      # Rename temporary file
-      res <- file.rename(pathnameT, pathname);
-      if (!isFile(pathname)) {
-        throw("Failed to rename temporary file: ", pathnameT, " -> ", pathname);
-      }
-      if (isFile(pathnameT)) {
-        throw("Failed to remove temporary file: ", pathnameT);
-      }
+    updateGenotypes(acf, units=units, calls=calls, encoding="birdseed", 
+                    verbose=verbose);
+    verbose && exit(verbose);
+      
+    footer <- readFooter(acf);
+    footer$srcFile <- srcFile;
+    writeFooter(acf, footer);
+      
+    # Rename temporary file
+    file.rename(pathnameT, pathname);
+    if (!isFile(pathname) || isFile(pathnameT)) {
+      throw("Failed to rename temporary file: ", pathnameT, " -> ", pathname);
     }
+
+    acf <- AromaUnitGenotypeCallFile(acfPathname);
+    res$acf <- acf;
     verbose && exit(verbose);
   }
+    
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Step 2. Confidence scores
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  if (is.null(res$asf)) {
+    verbose && enter(verbose, "Exporting confidence scores");
+
+    pathname <- asfPathname;
+    pathnameT <- sprintf("%s.tmp", pathname);
+    pathnameT <- Arguments$getWritablePathname(pathnameT, mustNotExist=TRUE);
+    on.exit({
+      if (isFile(pathnameT)) {
+        file.remove(pathnameT);
+      }
+    }, add=TRUE);
+      
+    asf <- AromaUnitSignalBinaryFile$allocateFromUnitNamesFile(unf, 
+             filename=pathnameT, types="double", size=4, signed=TRUE, 
+             overwrite=force, ...);
+    naValue <- as.integer(NA);
+    asf[, 1] <- naValue;
+    asf[units, 1] <- conf;
+    isNC <- (conf==0);
+    if (sum(isNC)) {
+      idxs <- units[whichVector(isNC)];
+      asf[idxs, 1] <- naValue;
+    }
+    footer <- readFooter(asf);
+    footer$srcFile <- srcFile;
+    writeFooter(asf, footer);
+      
+    # Rename temporary file
+    file.rename(pathnameT, pathname);
+    if (!isFile(pathname) || isFile(pathnameT)) {
+      throw("Failed to rename temporary file: ", pathnameT, " -> ", pathname);
+    }
+
+    asf <- AromaUnitSignalBinaryFile(asfPathname);
+    res$asf <- asf;
+    verbose && exit(verbose);
+  } # if (...)
+
+  verbose && cat(verbose, "Data files written:");
+  verbose && print(verbose, res);
   
   verbose && exit(verbose);
 
-  invisible(list(acf, asf));
+  invisible(res);
 })
 
 
 ############################################################################
 # HISTORY:
+# 2009-11-02
+# o CLEAN UP: Restructured code a bit.  Made less redundant.
+# o Replaced argument 'chipType' with 'unf'; it's more generic.
+# o BUG FIX: If the call file already exists, it was returned as a plain
+#   AromaUnitSignalBinaryFile, not as an AromaUnitGenotypeCallFile.
+# o BUG FIX: Called readDataFrame(df) instead of readDataFrame(this).
+# o BUG FIX: Argument was named 'dataset' and not 'dataSet'.
 # 2009-10-23
 # o CLEANUPS.
 # 2009-06-09
