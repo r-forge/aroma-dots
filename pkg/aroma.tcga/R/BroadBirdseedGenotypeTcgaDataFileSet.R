0232cf816691375f4c40d2ce11221a4a65bfea7c
henrikb <henrikb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-03 04:10:57 +0000

    Version: 0.2.2 [2009-11-02] o Added exportGenotypeCallsAndConfidenceScores() to   BroadBirdseedGenotypeTcgaDataFileSet.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@1715 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.tcga/R/BroadBirdseedGenotypeTcgaDataFileSet.R
+++ pkg/aroma.tcga/R/BroadBirdseedGenotypeTcgaDataFileSet.R
@@ -3,10 +3,78 @@ setConstructorS3("BroadBirdseedGenotypeTcgaDataFileSet", function(...) {
 })
 
 
+setMethodS3("exportGenotypeCallsAndConfidenceScores", "BroadBirdseedGenotypeTcgaDataFileSet", function(this, tags=c("*", "Birdseed"), unf, ..., rootPath="callData", verbose=FALSE) {
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Argument 'tags':
+  if (!is.null(tags)) {
+    tags <- Arguments$getCharacters(tags);
+    tags <- unlist(strsplit(tags, split=","), use.names=FALSE);
+    tags <- tags[nchar(tags) > 0];
+    idxs <- which(tags == "*");
+    tags[idxs] <- getTags(this, collapse=",");
+    tags <- unlist(strsplit(tags, split=","), use.names=FALSE);
+  }
+
+  # Argument 'unf':
+  if (!inherits(unf, "UnitNamesFile")) {
+    throw("Argument 'unf' is not a UnitNamesFile: ", class(unf)[1]);
+  }
+
+  # Argument 'rootPath':
+  rootPath <- Arguments$getWritablePath(rootPath);
+
+  # Argument 'verbose':
+  verbose <- Arguments$getVerbose(verbose);
+  if (verbose) {
+    pushState(verbose);
+    on.exit(popState(verbose));
+  }
+
+
+  verbose && enter(verbose, "Exporting ", class(this)[1]);
+  dataSet <- paste(c(getName(this), tags), collapse=",");
+  verbose && cat(verbose, "Full data set name: ", dataSet);
+
+  chipType <- getChipType(unf, fullname=FALSE);
+  
+  path <- file.path(rootPath, dataSet, chipType);
+  path <- Arguments$getWritablePath(path);
+
+  for (kk in seq(this)) {
+    df <- getFile(this, kk);
+    
+    verbose && enter(verbose, sprintf("Data file #%d ('%s') of %d", 
+                                           kk, getName(df), length(this)));
+
+    dsKK <- exportGenotypeCallsAndConfidenceScores(df, 
+                dataSet=dataSet, unf=unf, rootPath=rootPath, ..., 
+                verbose=less(verbose, 5));
+    verbose && cat(verbose, "Written data file(s):");
+    verbose && print(verbose, dsKK);
+
+    verbose && exit(verbose);
+  } # for (cc ...)
+
+  # Validating everything
+  res <- list();
+  res$acs <- AromaUnitGenotypeCallSet$byPath(path);
+  pattern <- ".*,confidenceScores.asb$";
+  res$ass <- AromaUnitSignalBinarySet$byPath(path, pattern=pattern);
+  verbose && print(verbose, res);
+
+  verbose && exit(verbose);
+
+  invisible(res);
+})
 
 
 ############################################################################
 # HISTORY:
+# 2009-11-02
+# o Added exportGenotypeCallsAndConfidenceScores() to 
+#   BroadBirdseedGenotypeTcgaDataFileSet.
 # 2009-10-25
 # o Created.
 ############################################################################
