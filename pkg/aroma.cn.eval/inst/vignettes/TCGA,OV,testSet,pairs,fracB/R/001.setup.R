08ab7af9e4ec79381a7a1a2984cc5756ac75eb49
pierre <pierre@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-05 08:13:44 +0000

    Can now run both GBM and OV data.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@1923 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.cn.eval/inst/vignettes/TCGA,OV,testSet,pairs,fracB/R/001.setup.R
+++ pkg/aroma.cn.eval/inst/vignettes/TCGA,OV,testSet,pairs,fracB/R/001.setup.R
@@ -4,15 +4,23 @@ library("R.menu");
 # 1. Data sets
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 if (interactive()) {
+  tumorTypes <- c("GBM", "OV");
+  tumorType <- textMenu(tumorTypes, title="Tumor type:", value=TRUE);
+  rm(tumorTypes);
   dataSets <- c(
-    "TCGA,OV,BeadStudio,BAF",
-    "TCGA,OV,BeadStudio,XY",
-    "TCGA,OV,Birdseed,ismpolish",
-    "TCGA,OV,CRMAv2"
+    "TCGA,%s,BeadStudio,BAF",
+    "TCGA,%s,BeadStudio,XY",
+    "TCGA,%s,Birdseed,ismpolish",
+    "TCGA,%s,CRMAv2"
   );
+  dataSets <- sprintf(dataSets, tumorType);
   dataSet <- textMenu(dataSets, title="Data set:", value=TRUE);
   rm(dataSets);
 } else {
+  tumorType <- args$tumorType;
+  if (is.null(tumorType)) {
+    throw("Command line argument --tumorType not given.");
+  }
   dataSet <- args$dataSet;
   if (is.null(dataSet)) {
     throw("Command line argument --dataSet not given.");
@@ -62,10 +70,19 @@ methodPattern <- "^(raw|TBN)";
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Change-points of interest
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-regions <- c(
+if (tumorType=="OV") {
+  regions <- c(
   "TCGA-23-1027:Chr2@108-140,cp=124+/-0.5,s=0/1",
   "TCGA-23-1027:Chr2@125.0-157.0,cp=141.0+/-0.5,s=1/3",
   "TCGA-23-1027:Chr10@80-109,cp=94+/-0.5,s=0/2", ## deletion
   "TCGA-23-1027:Chr10@106.5-113.5,cp=110+/-0.5,s=2/3", ## deletion -> CN LOH
   "TCGA-23-1027:Chr2@55-75.0,cp=65.0+/-0.5,s=0/1" ## "FALSE BREAKPOINT"
 );
+} else {
+  regions <- c(
+               "TCGA-02-0001:Chr2@35-74,cp=57+/-1,s=1/2",
+               "TCGA-02-0001:Chr2@75-110,cp=96+/-1,s=0/1", ## should be (1,2) to (1,3) 
+               "TCGA-02-0001:Chr2@100-130,cp=110+/-1,s=1/0", ## should be (1,3) to (1,1) 
+               "TCGA-02-0001:Chr13@0-70,cp=45+/-1,s=0/2"
+               );
+}
