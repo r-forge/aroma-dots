8913ad1aa5dfcf168dc68b1654395a8d55261f88
henrikb <henrikb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-25 21:27:58 +0000

    Version: 0.0.1 [2011-03-25] o Created.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@2199 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/aroma.illumina/R/IlluminaClusterPositionsTextFile.R
@@ -0,0 +1,10 @@
+setConstructorS3("IlluminaClusterPositionsTextFile", function(...) {
+  extend(TabularTextFile(...), "IlluminaClusterPositionsTextFile");
+})
+
+
+############################################################################
+# HISTORY:
+# 2011-03-25.
+# o Created.  Just a stub.
+############################################################################
