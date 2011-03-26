76b54a8b88a0b94dedc8f598137f70329ad15368
henrikb (Henrik Bengtsson) <henrikb@braju.com>
2011-03-26 05:11:40 +0000

    Can now reproduce the BAFs calculated by Illumina given cluster files.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@2200 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.illumina/R/IlluminaClusterPositionsTextFile.R
+++ pkg/aroma.illumina/R/IlluminaClusterPositionsTextFile.R
@@ -3,8 +3,29 @@ setConstructorS3("IlluminaClusterPositionsTextFile", function(...) {
 })
 
 
+setMethodS3("readCentroidMatrix", "IlluminaClusterPositionsTextFile", function(this, ..., what=c("T", "R")) {
+  # Argument 'what':
+  what <- match.arg(what);
+
+  fieldPattern <- sprintf("(AA|AB|BB)_%s_Mean", what);
+  patterns <- c(reporterId="character", "(AA|AB|BB)_._Mean"="numeric");
+  names(patterns)[2] <- fieldPattern;
+
+  data <- readDataFrame(this, colClassPatterns=patterns, ...);
+  mu <- as.matrix(data[,-1,drop=FALSE]);
+  rownames(mu) <- data[,1,drop=TRUE];
+  colnames(mu) <- gsub(fieldPattern, "\\1", colnames(mu));
+
+  # Sanity check
+  stopifnot(all(colnames(mu) == c("AA", "AB", "BB")));
+
+  mu;
+}) # readCentroidMatrix()
+
+
 ############################################################################
 # HISTORY:
-# 2011-03-25.
+# 2011-03-25
+# o Added readCentroidMatrix().
 # o Created.  Just a stub.
 ############################################################################
