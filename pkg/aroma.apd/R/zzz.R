86bf120b86e3b7943dac11f2a34fdd18c7cfbfe4
henrikb (Henrik Bengtsson) <henrikb@braju.com>
2011-07-24 01:41:21 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@2245 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.apd/R/zzz.R
+++ pkg/aroma.apd/R/zzz.R
@@ -8,21 +8,25 @@
   # Loading/installing affxparser
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
   # Load 'affxparser'
-  res <- require("affxparser");
+  res <- suppressWarnings({
+    require("affxparser");
+  });
 
   # Not installed?
   if (!res) {
     if (interactive()) {
       cat("Package 'affxparser' is not available or could not be loaded. Will now try to install it from Bioconductor (requires working internet connection):\n");
+      # To please R CMD check
+      biocLite <- NULL; rm(biocLite);
       source("http://www.bioconductor.org/biocLite.R");
       biocLite("affxparser");
       # Assert that the package can be successfully loaded
       res <- require("affxparser");
       if (!res) {
-        throw("Package 'affxparser' could not be loaded. Please install it from Bioconductor.");
+        throw("Package 'affxparser' could not be loaded. Please install it from Bioconductor, cf. http://www.bioconductor.org/");
       }
     } else {
-      warning("Package 'affxparser' could not be loaded. Without it ", pkgname, " will not work. Please install it from Bioconductor.");
+      warning("Package 'affxparser' could not be loaded. Without it ", pkgname, " will not work. Please install it from Bioconductor, cf. http://www.bioconductor.org/");
     }
   }
  
