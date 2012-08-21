5a352b2835f5d06cceca7a96adc4128d14a0aa90
tokuyasu (Taku Tokuyasu) <tokbio@gmail.com>
2012-08-21 10:33:00 +0000

    Convert testSystemBowtie2Build into a function (instead of a script); bump up the version number to 0.1.3
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@2458 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.seq/R/testSystemBowtie2Build.R
+++ pkg/aroma.seq/R/testSystemBowtie2Build.R
@@ -3,22 +3,30 @@
 ## Tip:  If using emacs, start it up using
 ## 'bash -c emacs &' from a cmd line
 
-library(R.oo)
-library(R.filesets)
-library(R.utils)
-source("systemBowtie2Build.R")
 
-
-Bt2Bin <- Sys.which("bowtie2")
-Bt2Home <- getParent(Bt2Bin)
-InPathName <- filePath(Bt2Home, "example/reference/lambda_virus.fa", sep="")
-OutPath <- "./tmp"
-if (!file.exists(OutPath))
+testSystemBowtie2Build <- function(
+                          OutPath = "./tmp",
+                          ...
+                          )
 {
-    mkdirs(OutPath)
-}
-IndexPrefix <- "lambda_virus"
+    library(R.oo)
+    library(R.filesets)
+    library(R.utils)
+    ## source("systemBowtie2Build.R")
 
-## systemBowtie2Build(inPathname=InPathName, outPath="./", name="lambda_virus")
-systemBowtie2Build(inPathname=InPathName, outPath=OutPath, name=IndexPrefix)
+    args <- list(...)
+
+    Bt2Bin <- Sys.which("bowtie2")
+    Bt2Home <- getParent(Bt2Bin)
+    InPathName <- filePath(Bt2Home, "example/reference/lambda_virus.fa", sep="")
+    if (!file.exists(OutPath))
+    {
+        mkdirs(OutPath)
+    }
+    IndexPrefix <- "lambda_virus"
+
+    systemBowtie2Build(inPathname=InPathName, outPath=OutPath, name=IndexPrefix)
+}
+
+
 
