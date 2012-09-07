68958b78d0aacf75a74b574deba9360b8a6a3384
tokuyasu <tokuyasu@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-07 21:31:26 +0000

    systemBowtie2Build - some mods/comments from our last meeting; bowtie2Build - start building on top of nearly complete "system" wrapper.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@2563 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.seq/R/bowtie2Build.R
+++ pkg/aroma.seq/R/bowtie2Build.R
@@ -10,83 +10,119 @@
 #  => calls =>
 #  system("bowtie2-build path/to/lambda_virus.fa lambda_virus");
 #
-setMethodS3("bowtie2Build", "default", function(inPathname, outPath=NULL, name=NULL, ..., overwrite=FALSE, verbose=FALSE) {
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  # Validate arguments
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  # Argument 'inPathname':
-  inPathname <- Arguments$getReadablePathname(inPathname);
 
-  # Argument 'outPath':
-  if (is.null(outPath)) {
-    outPath <- dirname(inPathname);   # e.g. /path/to
-  }
-  outPath <- Arguments$getWritablePath(outPath);
+## [
+## Q's on the plate:
+## - Arg list for bowtie2Build.R: inPathname can be pathname or list?
+## - How to do file checking / error handling in bowtie2Build.R?  Does Arguments$getReadablePathname do a stop()?
+## - Indent convention = 2 spaces?
+## - use dirname instead of getParent?
+## - see [] comments below
+## ]
 
-  # Argument 'name':
-  if (is.null(name)) {
-    inFilename <- basename(inPathname);   # e.g. lambda_virus.fa
-    name <- gsub("[.][^.]$", ".bam", inFilename);  # e.g. lambda_virus
-  }
-  name <- Arguments$getCharacter(name);
+setMethodS3("bowtie2Build", "default", function(inPathname, outPath=NULL, outName=NULL, ..., overwrite=FALSE, verbose=FALSE) {
+    ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    ## Validate arguments
+    ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    ## Argument 'inPathname':
+    ## inPathname <- Arguments$getReadablePathname(inPathname);
 
-  # Additional arguments
-  args <- list(...);
+    ## Check if inPathname is readable
+    if (is.list(inPathname)) {
+        tmp <- sapply(inPathname, function(x)
+                  {
+                      tryCatch(Arguments$getReadablePathname(x), error=function(e) {e}, warning=function(w) {})
+                      ## [ Q: Does this check if x is a *file*, not a dir? ]
+                  })
+        if (sum(tmp)==0) {throw(paste(inPathname, "contains unreadable file(s)"))}
+    } else {
+        ## Return error msg but leave out warnings for now
+        tryCatch(inPathname <- Arguments$getReadablePathname(inPathname), error=function(e) {e}, warning=function(w) {})
+    }
 
-  # Argument 'verbose':
-  verbose <- Arguments$getVerbose(verbose);
-  if (verbose) {
-    pushState(verbose);
-    on.exit(popState(verbose));
-  }
+    ## Check outPath
+    if (is.null(outPath)) {
+        ifelse (is.list(inPathname),
+                outPath <- dirname(inPathname[[1]]),  ## [ hack ]
+                outPath <- dirname(inPathname)  ## e.g. /path/to
+                )
+    }
+    outPath <- Arguments$getWritablePath(outPath);
 
-  verbose && enter(verbose, "Running bowtie2-build");
-  verbose && cat(verbose, "Input pathname: ", inPathname);
-  verbose && cat(verbose, "Arguments:");
-  verbose && str(verbose, args);
+    inFiles <- paste(inPathname, collapse=",")
 
-
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  # Locate external software
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  verbose && enter(verbose, "Locating external software");
-  command <- "bowtie2-build";
-  verbose && cat(verbose, "Command: ", command)
-
-  bin <- Sys.which(command);
-  verbose && cat(verbose, "Located pathname: ", bin)
-
-  # Assert existence
-  if (identical(bin, "") || !isFile(bin)) {
-    throw("Failed to located external software (via the system PATH): ", command);
-  }
-  verbose && exit(verbose);
+    ## Argument 'outName':
+    if (is.null(outName)) {
+        ## inFilename <- basename(inPathname);   ## e.g. lambda_virus.fa
+        ifelse(is.list(inPathname),
+               inFilename <- basename(inPathname[[1]]),
+               inFilename <- basename(inPathname) ## e.g. lambda_virus.fa
+               )
+        ## outName <- gsub("[.][^.]$", ".bam", inFilename);  ## [ Uh, .bam?  Where did this come from? ]
+        outName <- gsub("\\.fa$", "", inFilename);  ## e.g. lambda_virus
+        ## [ NEED TO TEST: Have to make sure this does not overwrite the input files! ]
+    }
+    outName <- Arguments$getCharacter(outName);
+    ## [ Should this be Arguments$getWriteablePathname() ? ]
 
 
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  # Call external software
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  verbose && enter(verbose, "Calling external software");
+    ## Additional arguments
+    args <- list(...);
 
-  # Argument to bowtie2-index (maybe)
-  outPrefix <- file.path(outPath, name); # e.g. path/to/lambda_virus
+    ## Argument 'verbose':
+    verbose <- Arguments$getVerbose(verbose);
+    if (verbose) {
+        pushState(verbose);
+        on.exit(popState(verbose));   ## [ << what is this ]
+    }
 
-  cmdSwitches <- args$cmdSwitches
-  binWithArgs <- paste(bin, cmdSwitches, inPathname, name)
+    verbose && enter(verbose, "Running bowtie2-build");
+    verbose && cat(verbose, "Input pathname: ", inPathname);  ## [ FIX: inPathname can be a list ]
+    verbose && cat(verbose, "Arguments:");
+    verbose && str(verbose, args);
 
-  ## cmd <- sprintf("%s", shQuote(binWithArgs));
-  cmd <- sprintf("%s", binWithArgs)  ## In emacs/ess, shQuote() version with single quotes added does not run
 
-  verbose && cat(verbose, "System call: ", cmd)
+    ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    ## Locate external software
+    ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    verbose && enter(verbose, "Locating external software");
+    command <- "bowtie2-build";
+    verbose && cat(verbose, "Command: ", command)
 
-  res <- system(cmd, ...);
+    bin <- Sys.which(command);
+    verbose && cat(verbose, "Located pathname: ", bin)
 
-  verbose && exit(verbose);
+    ## Assert existence
+    if (identical(bin, "") || !isFile(bin)) {
+        throw("Failed to located external software (via the system PATH): ", command);
+    }
+    verbose && exit(verbose);
 
-  verbose && exit(verbose);
 
-  invisible(res);
-}) # bowtie2-build()
+    ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    ## Call external software
+    ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    verbose && enter(verbose, "Calling external software");
+
+    ## Argument to bowtie2-index (maybe)
+    outPrefix <- file.path(outPath, outName); ## e.g. path/to/lambda_virus
+
+    cmdSwitches <- args$cmdSwitches
+    ## binWithArgs <- paste(bin, cmdSwitches, inPathname, outName)
+    binWithArgs <- paste(bin, cmdSwitches, inFiles, outName)
+
+    ## cmd <- sprintf("%s", shQuote(binWithArgs));
+    cmd <- sprintf("%s", binWithArgs)  ## In emacs/ess, shQuote() version with single quotes added does not run
+
+    verbose && cat(verbose, "System call: ", cmd)
+
+
+
+    res <- system(cmd, ...);
+    verbose && exit(verbose);
+    verbose && exit(verbose);
+    invisible(res);
+}) ## bowtie2-build()
 
 
 ############################################################################
@@ -94,3 +130,4 @@ setMethodS3("bowtie2Build", "default", function(inPathname, outPath=NULL, name=N
 # 2012-07-18
 # o Created bowtie2-build() stub.
 ############################################################################
+
