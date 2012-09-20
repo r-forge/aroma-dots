75e261fd704d095439d1ee00ee4132f195149200
tokuyasu <tokuyasu@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-20 00:55:01 +0000

    option (aka switch) handling now works; .optionsList2String helper function appended at end
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@2606 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.seq/R/systemBowtie2Build.R
+++ pkg/aroma.seq/R/systemBowtie2Build.R
@@ -8,7 +8,7 @@ if (bDebugSystemBowtie2Build)
     library(R.utils)
 }
 
-## Usage:  systemBowtie2Build referenceIn="" bt2IndexBase="" f=1 c=1 ...
+## Usage:  systemBowtie2Build referenceIn, bt2IndexBase, optionsList, <unsupported option>=1, ...)
 
 setMethodS3("systemBowtie2Build", "default",
             function(
@@ -16,112 +16,55 @@ setMethodS3("systemBowtie2Build", "default",
             ## Required args for bowtie2 index builder
             referenceIn,             ## comma-separated string = list of files with ref sequences
             bt2IndexBase,            ## write .bt2 data to files with this dir/basename
-            ## *** Bowtie 2 indexes work only with v2 (not v1).  Likewise for v1 indexes. ***
+            ## *** Bowtie 2 indexes work only with v2 (not v1). ***
 
-            ## Unsupported options [ flags should be binary (boolean)-valued; check the integer-valued ones? ]
-            cmdSwitchList=list(
+            ## Supported options ( bowtiew2 options w/o values are boolean; nonlogicals are integers, so far )
+            optionsList=list(
             f = FALSE,               ## reference files are Fasta (default)
             c = FALSE,               ## reference sequences given on cmd line (as <seq_in>)
             a = FALSE,               ## disable automatic -p/--bmax/--dcv memory-fitting
             noauto = FALSE,          ## disable automatic -p/--bmax/--dcv memory-fitting
             p = FALSE,               ## use packed strings internally; slower, uses less mem
             packed = FALSE,          ## use packed strings internally; slower, uses less mem
-            bmax = NULL,            ## <int> = max bucket sz for blockwise suffix-array builder
-            bmaxdivn = NULL,        ## <int> = max bucket sz as divisor of ref len (default: 4)
-            dcv = NULL,             ## <int> = diff-cover period for blockwise (default: 1024)
+            bmax = NULL,             ## <int> = max bucket sz for blockwise suffix-array builder
+            bmaxdivn = NULL,         ## <int> = max bucket sz as divisor of ref len (default: 4)
+            dcv = NULL,              ## <int> = diff-cover period for blockwise (default: 1024)
             nodc = FALSE,            ## disable diff-cover (algorithm becomes quadratic)
             r = FALSE,               ## don't build .3/.4.bt2 (packed reference) portion
             noref = FALSE,           ## don't build .3/.4.bt2 (packed reference) portion
-##            three = FALSE,           ## just build .3/.4.bt2 (packed reference) portion  ## IS THIS ALLOWED?
-            ## - The actual bowtie2-build switch is '-3'
-            `3` = FALSE,
+            `3` = FALSE,             ## just build .3/.4.bt2 (packed reference) portion
             justref = FALSE,         ## just build .3/.4.bt2 (packed reference) portion
-            o = NULL,               ## <int>: SA is sampled every 2^offRate BWT chars (default: 5)
-            offrate = NULL,         ## <int>: SA is sampled every 2^offRate BWT chars (default: 5)
-            t = NULL,               ## <int>: # of chars consumed in initial lookup (default: 10)
-            ftabchars = NULL,       ## <int>: # of chars consumed in initial lookup (default: 10)
-            seed = NULL,            ## <int>: ## seed for random number generator
+            o = NULL,                ## <int>: SA is sampled every 2^offRate BWT chars (default: 5)
+            offrate = NULL,          ## <int>: SA is sampled every 2^offRate BWT chars (default: 5)
+            t = NULL,                ## <int>: # of chars consumed in initial lookup (default: 10)
+            ftabchars = NULL,        ## <int>: # of chars consumed in initial lookup (default: 10)
+            seed = NULL,             ## <int>: ## seed for random number generator
             q = FALSE,               ## verbose output (for debugging)
             quiet = FALSE,           ## verbose output (for debugging)
             h = FALSE,               ## print detailed description of tool and its options
             help = FALSE,            ## print detailed description of tool and its options
             usage = FALSE,           ## print this usage message
-            version = FALSE         ## print version information and quit
+            version = FALSE          ## print version information and quit
             ),
-            ## overwrite=FALSE, ## Put these args one level up, in bowtie2Build
-            ## verbose=FALSE
-            bin="bowtie2-build",     ## full pathname to bowtie2-build executable
+
+            ## full pathname to bowtie2-build executable
+            bin="bowtie2-build",
             ...
             )
         {
             ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-            argsList <- as.list(sys.call()) ## This automatically leaves out the NULL args
-            argsList <- argsList[-1]  ## remove the function name
 
-            ## ############################################
-            ## ## Dummy test code
-            ## if (F) {
-            ## cmdSwitchList$q <- TRUE
-            ## cmdSwitchList$version <- TRUE
-            ## cmdSwitchList$t <- 5
-            ## }
-            ##
-            ## cmdSwitchList <- cmdSwitchList[!sapply(cmdSwitchList, is.null)]
-            ## cmdSwitchLogical <- cmdSwitchList[sapply(cmdSwitchList, function(x) {identical(x, TRUE)})]
-            ## ## cmdSwitchVec <- sapply(cmdSwitchList, function(x) {x})
-            ##
-            ##
-            ## if (F) { ## DEPRECATED
-            ## optionVals <- unlist(cmdSwitchList)  ## NULL's automatically removed; [ THIS IS BAD BECAUSE VECTOR HAS TO BE SET TO A SINGLE TYPE! ]
-            ## optionNames <- names(optionVals)
-            ## optionsLogical <- optionNames[sapply(optionVals, function(x) {identical(x, TRUE)})]
-            ## optionsLogical <- sapply(optionNames[sapply(optionVals, function(x) {identical(x, TRUE)})])
-            ## if (length(optionsLogical) > 0)
-            ## {
-            ## hyphenVec <- sapply(optionsLogical, function(x)
-            ## {
-            ## ifelse(nchar(x) == 1, "-", "--")
-            ## })
-            ## optionsLogicalStr <- paste(paste(hyphenVec, optionsLogical, sep=""))
-            ## } else {
-            ## optionsLogicalStr <- NULL
-            ## }
-            ##
-            ##
-            ## optionsNonLogic <- optionNames[!is.logical(optionVals)]  ## something like this...
-            ## ##strList <- lapply(1:length(optionVals), function(x)
-            ## ##              {
-            ## optionsStr <- paste(paste("-", namesSet))
-            ## }
-            ## ############################################
+            ## Create string of supported options, and unsupported options; paste together
+            cmdOptions <- .optionsList2String(optionsList)
+            cmdOptionsAdd <- .optionsList2String(list(...))
+            cmdOptions <- paste(cmdOptions, cmdOptionsAdd)
 
-
-            if (length(argsList) > 0)  ## [ always true as it stands ]
-            {
-                switchVals <- unlist(argsList)  ## not used
-                switchNames <- names(argsList)
-                indsDrop <- which(switchNames=="bt2IndexBase"|switchNames=="referenceIn")  ## [ must be a shorter way to do this ]
-                ## [ Not needed if move these to one level up in the code ]
-                ## indsDrop <- c(indsDrop, which(switchNames=="overwrite"|switchNames=="verbose"))  ## non-bowtie2 args
-                if (length(indsDrop) > 0)
-                {
-                    switchNames <- switchNames[-indsDrop]
-                    switchVals <- switchVals[-indsDrop]
-                }
-                switchNames <- sub("^three$", "3", switchNames)  ## fix the one numerical option that can't be passed as is
-                hyphenVec <- sapply(switchNames, function(x) {ifelse(nchar(x) == 1, "-", "--")})
-                cmdSwitches <- paste(paste(hyphenVec, switchNames, sep=""), switchVals, collapse=" ")
-
-                ## [ This is not correct yet - some switches take on (integer) values ]
-
-            } else {
-                cmdSwitches <- ""
-            }
-
+            ## Set up command line
             binWithArgs <- sprintf("%s", bin)
-            binWithArgs <- sprintf("%s %s", binWithArgs, cmdSwitches)
+            binWithArgs <- sprintf("%s %s", binWithArgs, cmdOptions)
             binWithArgs <- sprintf("%s %s", binWithArgs, referenceIn)
             binWithArgs <- sprintf("%s %s", binWithArgs, bt2IndexBase)
+            binWithArgs <- gsub("  +", " ", binWithArgs)  ## prettify
 
             ## cmd <- sprintf("%s", binWithArgs)  ## In emacs/ess, shQuote() version with single quotes added does not run
             cmd <- binWithArgs
@@ -156,9 +99,49 @@ setMethodS3("systemBowtie2Build", "default",
 ############################################################################
 # HISTORY:
 # 2012-08-22
-# o TT:  First implementation of low-level system wrapper, including all bowtie2-build switches; not tested
+# o TT:  First implementation of low-level system wrapper, including all bowtie2-build options; not tested
 # 2012-08-21
 # o TT:  Implemented working version (turns out this was closer in intent to bowtie2Build.R
 # 2012-08-20
 # o HB:  Created systemBowtie2Build stub
 ############################################################################
+
+
+.optionsList2String <- function(optionsList=list())
+{
+    ## Return if nothing to process
+    if (length(optionsList) < 1) { return(NULL) }
+
+    ## Split into logical (boolean) and non-logical (name-value pair) options
+    tmpLogical <- unlist(optionsList[sapply(optionsList, is.logical)])
+    tmpNonLogical <- unlist(optionsList[!sapply(optionsList, is.logical)])  ## NULLs are auto-removed
+
+    ## For logical options: 1) get the names of the TRUE ones, 2) add a hyphen (or two)
+    tmpLogical <- tmpLogical[tmpLogical==TRUE]
+    nms <- names(tmpLogical)
+    optionsLogical <-
+        sapply(nms, function(nm)
+           {
+               str <- ifelse(nchar(nm)==1,
+                             paste("-", nm, sep=""),
+                             paste("--", nm, sep=""))
+               return(str)
+           })
+
+    ## For "non-logical" options:
+    ##  1) create two vectors: names and values; 2) paste together with appropriate hypenation
+    nms <- names(tmpNonLogical)
+    vals <- as.vector(tmpNonLogical)  ## (Could do some sanity checking of the vals here)
+    hyphenVec <-
+        sapply(nms, function(nm)
+           {
+               ifelse(nchar(nm)==1, "-", "--")
+           })
+    optionsNonLogical <- paste(paste(hyphenVec, nms, sep=""), vals, sep="=")
+    ## - This works appropriately even when tmpNonLogical is NULL
+
+    ## Combine vectors into a string and return
+    cmdOptions <- paste(c(optionsLogical, optionsNonLogical), collapse=" ")
+    return(cmdOptions)
+}
+
