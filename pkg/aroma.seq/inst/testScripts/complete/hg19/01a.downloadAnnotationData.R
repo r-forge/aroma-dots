e2f79ba555ad06168bc1f3e5aab6aa9dc02614b4
henrikb <henrikb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-25 05:39:14 +0000

    Added test script for running BWA index on human_g1k_v37.fasta.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@2651 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/aroma.seq/inst/testScripts/complete/hg19/01a.downloadAnnotationData.R
@@ -0,0 +1,33 @@
+path <- system.file("testScripts/R", package="aroma.seq");
+pathname <- file.path(path, "downloadUtils.R");
+source(pathname);
+
+library("aroma.seq");
+verbose <- Arguments$getVerbose(-8, timestamp=TRUE);
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Local functions
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+verbose && enter(verbose, "Downloading annotation data");
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Annotation data
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+fa <- download1000GenomesHumanReferenceFile();
+print(fa);
+## FastaReferenceFile:
+## Name: human_g1k_v37
+## Tags:
+## Full name: human_g1k_v37
+## Pathname: annotationData/organisms/Human/human_g1k_v37.fasta
+## File size: 2.94 GB (3153506519 bytes)
+## RAM: 0.00 MB
+## Total sequence length: NA
+## Number of sequences: 84
+## Sequence names: [84] 1 dna:chromosome chromosome:GRCh37:1:1:249250621:1, 
+## 2 dna:chromosome chromosome:GRCh37:2:1:243199373:1, 3 dna:chromosome
+## chromosome:GRCh37:3:1:198022430:1, ..., GL000192.1 dna:supercontig
+## supercontig::GL000192.1:1:547496:1
+
+
+verbose && exit(verbose);
