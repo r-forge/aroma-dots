49c720033703c67784eb38030628ca679e7be86d
henrikb (Henrik Bengtsson) <henrikb@braju.com>
2012-09-25 04:10:37 +0000

    Adding scripts to the systems test for downloading of bowtie2 example data.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@2649 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/aroma.seq/inst/testScripts/bwa/01a.downloadAnnotationData.R
@@ -0,0 +1,27 @@
+path <- system.file("testScripts/R", package="aroma.seq");
+pathname <- file.path(path, "downloadUtils.R");
+source(pathname);
+
+library("aroma.seq");
+verbose <- Arguments$getVerbose(-8, timestamp=TRUE);
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Local functions
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+verbose && enter(verbose, "Downloading annotation data");
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Annotation data
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+path <- "annotationData/organisms/LambdaPhage/";
+filename <- "lambda_virus.fa";
+pathname <- Arguments$getReadablePathname(filename, path=path, mustExists=FALSE);
+
+if (!isFile(pathname)) {
+  downloadBowtie2ExampleData();
+}
+
+fa <- FastaReferenceFile(filename, path=path);
+print(fa);
+
+verbose && exit(verbose);
