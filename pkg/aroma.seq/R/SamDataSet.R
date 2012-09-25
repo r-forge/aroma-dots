73e3210cbb26ad112a68389d13bf07a1feb454a8
henrikb <henrikb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-25 20:55:30 +0000

    Added SamDataFile and SamDataSet classes.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@2654 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/aroma.seq/R/SamDataSet.R
@@ -0,0 +1,45 @@
+###########################################################################/**
+# @RdocClass SamDataSet
+#
+# @title "The SamDataSet class"
+#
+# \description{
+#  @classhierarchy
+#
+#  An SamDataSet object represents a set of @see "SamDataFile":s.
+# }
+# 
+# @synopsis
+#
+# \arguments{
+#   \item{files}{A @list of @see "SamDataFile":s.}
+#   \item{...}{Arguments passed to @see "R.filesets::GenericDataFileSet".}
+# }
+#
+# \section{Fields and Methods}{
+#  @allmethods "public"
+# }
+# 
+# @author
+#*/###########################################################################
+setConstructorS3("SamDataSet", function(files=NULL, ...) {
+  extend(GenericDataFileSet(files=files, ...), "SamDataSet");
+})
+
+
+setMethodS3("validate", "SamDataSet", function(this, ...) {
+  NextMethod("validate", this, ...);
+}, protected=TRUE)
+
+
+setMethodS3("getDepth", "SamDataSet", function(this, ...) {
+  1L;
+}, protected=TRUE);
+
+
+
+############################################################################
+# HISTORY:
+# 2012-09-25
+# o Created from BamDataSet.R.
+############################################################################
