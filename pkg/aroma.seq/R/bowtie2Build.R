0843bcf077d0cb6d0c07a2491fc8f03719f9d9bd
tokuyasu (Taku Tokuyasu) <tokbio@gmail.com>
2012-09-25 06:55:14 +0000

    Clean up code, change indent; package check/build works (mostly). Some conflicting changes w/ Henrik's checkin from 1 PM earlier today.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@2652 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.seq/R/bowtie2Build.R
+++ pkg/aroma.seq/R/bowtie2Build.R
@@ -16,63 +16,65 @@ setMethodS3("bowtie2Build", "default", function(inPathnames,   ## vector of file
                                                 outName=NULL,  ## basename of index
                                                 optionsList,   ## list of bowtie2 options, for power users
                                                 ...,
-                                                overwrite=FALSE, verbose=FALSE, command="bowtie2-build") {
+                                                overwrite=FALSE,
+                                                verbose=FALSE,
+                                                command="bowtie2-build") {
 
-    ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    ## Validate arguments
-    ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    inPathnames <- sapply(inPathnames, Arguments$getReadablePathname(x))
-    inPathsStr <- paste(inPathnames, collapse=",")   ## bowtie2 wants a comma-delimited string
+  ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  ## Validate arguments
+  ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  inPathnames <- sapply(inPathnames, Arguments$getReadablePathname)
+  inPathsStr <- paste(inPathnames, collapse=",")   ## bowtie2 wants a comma-delimited string
 
-    ## Default for outPath is the inPath
-    if (is.null(outPath)) {
-        outPath <- dirname(inPathnames[1])
-    }
-    outPath <- Arguments$getWritablePath(outPath);
-    if (is.null(outPath)) {
-        outName <- gsub("\\.fa$", "", basename(inPathnames[1]))   ## e.g. lambda_virus
-        ## - Using the first infile may be confusing, if e.g. it is a subscripted name like "_1"; oh well
-    }
-    outPrefix <- file.path(outPath, outName); ## e.g. path/to/lambda_virus
-    outPrefix <- Arguments$getWriteablePathname(outPrefix, mustNotExist=T)
+  ## Default for outPath is the inPath
+  if (is.null(outPath)) {
+    outPath <- dirname(inPathnames[1])
+  }
+  outPath <- Arguments$getWritablePath(outPath);
+  if (is.null(outPath)) {
+    outName <- gsub("\\.fa$", "", basename(inPathnames[1]))   ## e.g. lambda_virus
+    ## - Using the first infile may be confusing, if e.g. it is a subscripted name like "_1"; oh well
+  }
+  outPrefix <- file.path(outPath, outName); ## e.g. path/to/lambda_virus
+  outPrefix <- Arguments$getWriteablePathname(outPrefix, mustNotExist=T)
 
-    ## Additional arguments
-    args <- list(...);  ## Not used here; just passed along
+  ## Additional arguments
+  args <- list(...);  ## Not used here; just passed along
 
-    ## Argument 'verbose':
-    verbose <- Arguments$getVerbose(verbose);
-    if (verbose) {
-        pushState(verbose);
-        on.exit(popState(verbose));
-    }
-    verbose && enter(verbose, paste("Running", command));
-    verbose && cat(verbose, "Input pathnames: ", inPathsStr);
-    verbose && cat(verbose, "Arguments:");
-    verbose && str(verbose, args);
+  ## Argument 'verbose':
+  verbose <- Arguments$getVerbose(verbose);
+  if (verbose) {
+    pushState(verbose);
+    on.exit(popState(verbose));
+  }
+  verbose && enter(verbose, paste("Running", command));
+  verbose && cat(verbose, "Input pathnames: ", inPathsStr);
+  verbose && cat(verbose, "Arguments:");
+  verbose && str(verbose, args);
 
-    ## Locate external software
-    ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    verbose && enter(verbose, "Locating external software");
-    verbose && cat(verbose, "Command: ", command)
-    bin <- Sys.which(command);
-    verbose && cat(verbose, "Located pathname: ", bin)
+  ## Locate external software
+  ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  verbose && enter(verbose, "Locating external software");
+  verbose && cat(verbose, "Command: ", command)
+  bin <- Sys.which(command);
+  verbose && cat(verbose, "Located pathname: ", bin)
 
-    ## Assert existence
-    if (identical(bin, "") || !isFile(bin)) {
-        throw("Failed to located external software (via the system PATH): ", command);
-    }
-    verbose && exit(verbose);
+  ## Assert existence
+  if (identical(bin, "") || !isFile(bin)) {
+    throw("Failed to located external software (via the system PATH): ", command);
+  }
+  verbose && exit(verbose);
 
-    ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    ## Call external software
-    ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    verbose && enter(verbose, "Calling external software");
+  ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  ## Call external software
+  ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  verbose && enter(verbose, "Calling external software");
 
-    systemBowtie2Build(inPathsStr, outPrefix, optionsList, ..., bin=bin)
+  res <- systemBowtie2Build(inPathsStr, outPrefix, optionsList, ..., bin=bin)
 
-    verbose && exit(verbose);
-    verbose && exit(verbose);
-    invisible(res);
+  verbose && exit(verbose);
+  verbose && exit(verbose);
+  invisible(res);
 }) ## bowtie2-build()
 
 
