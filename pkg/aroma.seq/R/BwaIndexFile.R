f70759d24e779dd46e52aecf9cb626cc16734a1e
henrikb <henrikb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-25 20:25:44 +0000

    Added BwaIndexFile and BwaIndexSet classes.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@2653 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/aroma.seq/R/BwaIndexFile.R
@@ -0,0 +1,63 @@
+###########################################################################/**
+# @RdocClass BwaIndexFile
+#
+# @title "The abstract BwaIndexFile class"
+#
+# \description{
+#  @classhierarchy
+#
+#  A BwaIndexFile object represents a BWA index file.
+# }
+# 
+# @synopsis
+#
+# \arguments{
+#   \item{...}{Arguments passed to @see "R.filesets::GenericDataFile".}
+# }
+#
+# \section{Fields and Methods}{
+#  @allmethods "public"
+# }
+# 
+# @author
+#
+# \references{
+#    ...
+# }
+#
+# \seealso{
+#   An object of this class is typically part of an 
+#   @see "BwaIndexSet".
+# }
+#*/###########################################################################
+setConstructorS3("BwaIndexFile", function(...) {
+  extend(GenericDataFile(...), "BwaIndexFile");
+})
+
+setMethodS3("as.character", "BwaIndexFile", function(x, ...) {
+  # To please R CMD check
+  this <- x;
+
+  s <- NextMethod("as.character", ...);
+  class <- class(s);
+
+  s <- c(s, sprintf("Index prefix: %s", getIndexPrefix(this)));
+
+  class(s) <- class;
+  s;
+})
+
+
+setMethodS3("getIndexPrefix", "BwaIndexFile", function(this, ...) {
+  path <- getPath(df);
+  fullname <- getFullName(df);
+  prefix <- file.path(path, fullname);
+  prefix;
+})
+
+
+############################################################################
+# HISTORY:
+# 2012-09-25
+# o Created.
+############################################################################
