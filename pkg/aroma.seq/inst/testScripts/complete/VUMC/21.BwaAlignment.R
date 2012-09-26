7e42f6db3cc2dce2923a96f1baae305b23dc2301
henrikb (Henrik Bengtsson) <henrikb@braju.com>
2012-09-26 02:05:34 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@2667 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.seq/inst/testScripts/complete/VUMC/21.BwaAlignment.R
+++ pkg/aroma.seq/inst/testScripts/complete/VUMC/21.BwaAlignment.R
@@ -18,7 +18,6 @@ path <- file.path("fastqData", dataSet, platform);
 ds <- FastqDataSet$byPath(path);
 print(ds);
 
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Build BWA index set
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -29,12 +28,19 @@ print(is);
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Single-end alignment
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Process at most two FASTQ files
+ds <- extract(ds, 1:min(2, length(ds)));
+
 # BWA with BWA 'aln' options '-n 2' and '-q 40'.
 alg <- BwaAlignment(ds, indexSet=is, n=2, q=40);
 print(alg);
 
 bs <- process(alg, verbose=-10);
 print(bs);
+
+# Display an example BAM file
+bf <- getFile(bs, 1);
+print(bf);
 
 
 ############################################################################
