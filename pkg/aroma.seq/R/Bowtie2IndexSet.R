0df612d2315b8b729416863abfaf4075ae6d4557
henrikb <henrikb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-28 04:26:24 +0000

    Version: 0.1.9 [2012-09-27] o Added Bowtie2Alignment.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@2682 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.seq/R/Bowtie2IndexSet.R
+++ pkg/aroma.seq/R/Bowtie2IndexSet.R
@@ -25,7 +25,10 @@ setConstructorS3("Bowtie2IndexSet", function(...) {
   extend(AbstractIndexSet(...), "Bowtie2IndexSet");
 })
 
+
 setMethodS3("isComplete", "Bowtie2IndexSet", function(this, ...) {
+  # TODO: The set of index files generated may vary with
+  # bowtie2-index options. /HB 2012-09-27
   knownExts <- c("1", "2", "3", "4", "rev.1", "rev.2");
   knownExts <- sprintf("%s.bt2", knownExts);
   if (length(this) < length(knownExts)) return(FALSE);
@@ -41,9 +44,33 @@ setMethodS3("isComplete", "Bowtie2IndexSet", function(this, ...) {
 })
 
 
+setMethodS3("getSummary", "Bowtie2IndexSet", function(this, ...) {
+  stopifnot(isComplete(this));
+  stopifnot(isCapableOf(aroma.seq, "bowtie2"));
+  prefix <- getIndexPrefix(this);
+  tempfile
+  bfr <- system2("bowtie2-inspect", args=c("--summary", prefix), stdout=TRUE);
+  keys <- gsub("\\t.*", "", bfr);
+  values <- strsplit(bfr, split="\t", fixed=TRUE);
+  values <- lapply(values, FUN="[", -1L);
+  names(values) <- keys;
+  values;
+})
+
+
+setMethodS3("getSequenceNames", "Bowtie2IndexSet", function(this, ...) {
+  stopifnot(isComplete(this));
+  stopifnot(isCapableOf(aroma.seq, "bowtie2"));
+  prefix <- getIndexPrefix(this);
+  system2("bowtie2-inspect", args=c("--names", prefix));
+})
+
 
 ############################################################################
 # HISTORY:
-# 2012-09-25
-# o Created.
+# 2012-09-27
+# o Added getSummary() and getSequenceNames() for Bowtie2IndexSet, which
+#   utilizes 'bowtie2-inspect' executable.
+# 2012-09-27
+# o Created from BwaIndexSet.R.
 ############################################################################
