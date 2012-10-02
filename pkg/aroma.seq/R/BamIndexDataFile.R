64521982bcb3c473963cbef7367c402e17b605b1
henrikb (Henrik Bengtsson) <henrikb@braju.com>
2012-10-02 19:41:59 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@2709 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.seq/R/BamIndexDataFile.R
+++ pkg/aroma.seq/R/BamIndexDataFile.R
@@ -38,28 +38,6 @@ setMethodS3("as.character", "BamIndexDataFile", function(x, ...) {
 })
 
 
-setMethodS3("extractStats", "BamIndexDataFile", function(this, ..., force=FALSE) {
-  stats <- this$.stats;
-
-  if (force || is.null(stats)) {
-    pathname <- getPathname(this);
-    bfr <- systemSamtools("idxstats", pathname, stdout=TRUE, ...);
-    stats <- strsplit(bfr, split="\t", fixed=TRUE);
-    seqName <- sapply(stats, FUN=.subset, 1L);
-    seqLength <- sapply(stats, FUN=.subset, 2L);
-    seqLength <- Arguments$getIntegers(seqLength);
-    countMapped <- sapply(stats, FUN=.subset, 3L);
-    countMapped <- Arguments$getIntegers(countMapped);
-    countUnmapped <- sapply(stats, FUN=.subset, 4L);
-    countUnmapped <- Arguments$getIntegers(countUnmapped);
-    data <- data.frame(length=seqLength, mapped=countMapped, unmapped=countUnmapped);
-    rownames(data) <- seqName;
-    stats <- data;
-    this$.stats <- stats;
-  }
-
-  data;
-})
 
 
 ############################################################################
