55664d99682acce1909226a909b52ff70f6c9878
henrikb (Henrik Bengtsson) <henrikb@braju.com>
2012-10-02 18:17:36 +0000

    Version: 0.1.13 [2012-10-02] o Added PicardDuplicateRemoval.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@2706 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/aroma.seq/inst/testScripts/complete/LambdaVirusExample/31.Bowtie2Alignment,PicardDuplicateRemoval.R
@@ -0,0 +1,58 @@
+#!/usr/bin/env Rscript
+
+############################################################################
+#
+############################################################################
+library("aroma.seq");
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setup
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+path <- "annotationData/organisms/LambdaPhage";
+fa <- FastaReferenceFile("lambda_virus.fa", path=path);
+print(fa);
+
+# Data set
+dataSet <- "LambdaVirusExample";
+platform <- "Generic";
+path <- file.path("fastqData", dataSet, platform);
+ds <- FastqDataSet$byPath(path);
+print(ds);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Build Bowtie2 index set
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+is <- buildBowtie2IndexSet(fa, verbose=-10);
+print(is);
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Single-end alignment
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Bowtie2 with bowtie2 ...
+alg <- Bowtie2Alignment(ds, indexSet=is);
+print(alg);
+
+bs <- process(alg, verbose=-20);
+print(bs);
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Remove duplicated reads using Picard
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+dr <- PicardDuplicateRemoval(bs);
+print(dr);
+
+bs2 <- process(dr, verbose=-20);
+print(bs2);
+
+
+
+############################################################################
+# HISTORY:
+# 2012-10-02
+# o Created.
+############################################################################
