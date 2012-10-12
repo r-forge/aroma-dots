ddd3792a526e0c7d33df822a9e5033a109502ec9
henrikb (Henrik Bengtsson) <henrikb@braju.com>
2012-10-12 02:02:27 +0000

    Version: 0.1.15 [2012-10-11] o Added TotalCnBinnedCounting().
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@2722 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/aroma.seq/inst/testScripts/complete/VUMC/41.TotalCnBinnedCounting.R
@@ -0,0 +1,130 @@
+#!/usr/bin/env Rscript
+
+############################################################################
+#
+############################################################################
+library("aroma.seq");
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setup
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+by <- 50e3;
+byTag <- sprintf("%dkb", by/1e3);
+ugp <- AromaUgpFile$byChipType("GenericHuman", tags=byTag);
+print(ugp);
+
+# Data set
+dataSet <- "AlbertsonD_2012-Bladder_VUMC";
+platform <- "Generic";
+path <- file.path("bamData", dataSet, platform);
+bs <- BamDataSet$byPath(path);
+setFullNamesTranslator(bs, function(names, ...) {
+  names <- gsub(".bowtie.sorted", "", names, fixed=TRUE);
+  names <- gsub("([TN])$", ",\\1", names);
+  names;
+});
+print(bs);
+
+
+chrLabels <- names(getTargets(getFile(bs,1)));
+chrMap <- c(1:25);
+labels <- sprintf("chr%d", chrMap);
+labels[23:25] <- sprintf("chr%s", c("X", "Y", "M"));
+names(chrMap) <- labels;
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Bin reads chromosome by chromosome
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+bc <- TotalCnBinnedCounting(bs, targetUgp=ugp);
+print(bc);
+
+ds <- process(bc, verbose=verbose);
+verbose && print(verbose, ds);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Extract tumor-normal pairs
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+dsT <- extract(ds, sapply(bs, hasTag, "T"));
+dsN <- extract(ds, sapply(bs, hasTag, "N"));
+
+# Extract one pair
+dfT <- getFile(dsT, 1);
+dfN <- getFile(dsN, 1);
+print(dfT);
+print(dfN);
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# CNs chromosome by chromosome
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+chrs <- getChromosomes(ugp);
+for (kk in seq(along=chrs)) {
+  chr <- chrs[kk];
+  verbose && enter(verbose, sprintf("Chromosome #%d ('chr%02d') of %d", kk, chr, length(chrs)));
+
+  cnT <- extractRawCopyNumbers(dfT, chromosome=chr);
+  # Nothing to do?
+  if (nOut == 0) {
+    verbose && cat(verbose, "No target loci on chromosome. Skipping.");
+    verbose && exit(verbose);
+    next;
+  }
+  nOut <- length(xOut);
+  cnN <- extractRawCopyNumbers(dfN, chromosome=chr);
+
+  units <- getUnitsOnChromosome(ugp, chromosome=chr);
+  xOut <- getPositions(ugp, units=units);
+  verbose && printf(verbose, "Target loci: [%d] %s\n", nOut, hpaste(xOut));
+
+  # Nothing to do?
+  if (nOut == 0) {
+    verbose && cat(verbose, "No target loci on chromosome. Skipping.");
+    verbose && exit(verbose);
+    next;
+  }
+
+  # Read (start) positions on current chromosome
+  gr <- GRanges(seqnames=Rle(chrLabel), ranges=IRanges(-500e6, +500e6));
+  xT <- readReadPositions(bfT, which=gr, verbose=-20)$pos;
+  verbose && str(verbose, xT);
+  xN <- readReadPositions(bfN, which=gr, verbose=-20)$pos;
+  verbose && str(verbose, xN);
+
+  # Bin
+  bx <- c(xOut[1]-by/2, xOut+by/2);
+  yTs <- binCounts(xT, bx=bx);
+  yNs <- binCounts(xN, bx=bx);
+
+  # Calculate binned total copy numbers (assuming diploid genome)
+  C <- 2*yTs/yNs;
+  x <- xOut;
+
+  sampleName <- getName(bfT);
+  chrTag <- sprintf("chr%02d", chr);
+  toPNG(sampleName, tags=c(chrTag, byTag, "TCN"), width=1024, aspectRatio=0.3, {
+    par(mar=c(5,4,2,2)+0.1);
+    Clim <- c(0,5);
+    plot(x/1e6,C, ylim=Clim, xlab="Position (Mb)", ylab="CN ratio");
+    stext(side=3, pos=0, sampleName);
+    stext(side=3, pos=1, chrTag);
+    stext(side=3, pos=1, line=-1, cex=0.8, sprintf("n=%d @ %s", sum(is.finite(C)), byTag));
+    stext(side=4, pos=0, cex=0.8, sprintf("nT/nN=%d/%d=%.2f", length(xT), length(xN), length(xT)/length(xN)));
+  });
+ 
+  verbose && exit(verbose);
+} # for (kk ...)
+
+
+
+############################################################################
+# HISTORY:
+# 2012-10-10
+# o Now plotting whole-genome TCN tracks, where data is loaded chromosome
+#   by chromosome. Also utilizing generic UGP files.
+# 2012-10-02
+# o Created.
+############################################################################
