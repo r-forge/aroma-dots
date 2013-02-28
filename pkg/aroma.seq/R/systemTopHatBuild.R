4d3847fe7db7891e4b2626e191fc201438bb33e2
tokuyasu <tokuyasu@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-02-28 19:53:21 +0000

    Change instances of 'Tophat' to 'TopHat' in filenames, code
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@2948 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.seq/R/systemTopHatBuild.R
+++ pkg/aroma.seq/R/systemTopHatBuild.R
@@ -1,7 +1,15 @@
+## source("systemTopHatBuild.R")
+## - 20130215 This code will likely be superseded by systemTopHat.R; call that from a 'tophatBuild.R' (to be written)
+
+if (FALSE) {  ## (for standalone testing)
+  library(R.filesets)
+  source("findTopHat.R")
+}
+
 ###########################################################################/**
-# @RdocDefault systemTophat
+# @RdocDefault systemTopHatBuild
 #
-# @title "Calls the TopHat executable to align input reads"
+# @title "Calls the TopHat executable, specifically to build a transcriptome index"
 #
 # \description{
 #  @get "title".
@@ -17,16 +25,16 @@
 #
 # @author
 #*/###########################################################################
-setMethodS3("systemTophat", "default", function(commandName,
-                                                ...,
-                                                system2ArgsList=list(),  ## For now, explicitly split off arguments to be passed to system2
-                                                .fake=FALSE, verbose=FALSE) {
+setMethodS3("systemTopHatBuild", "default", function( ## ( No 'command' arg in this case )
+                                                     ...,
+                                                     system2Args,  ## explicitly split off arguments to be passed to system2
+                                                     .fake=FALSE, verbose=FALSE) {
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # Validate arguments
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   # Arguments '...':
-  dotArgs <- list(...);  ## list with one item named 'args'; these are the arguments to *tophat*
+  args <- list(...);  ## These are the arguments to *tophat*
 
   # Argument 'verbose':
   verbose <- Arguments$getVerbose(verbose);
@@ -40,36 +48,41 @@ setMethodS3("systemTophat", "default", function(commandName,
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # Locate executable
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  bin <- findCmd(commandName, verbose=less(verbose, 50));
+  bin <- findTopHat(verbose=less(verbose, 50));
   verbose && cat(verbose, "Executable: ", bin);
 
   verbose && cat(verbose, "Arguments passed to system2():");
-  verbose && str(verbose, system2ArgsList)
+  verbose && str(verbose, system2Args);
   verbose && cat(verbose, "Arguments passed to TopHat:");
-  verbose && str(verbose, dotArgs);
+  verbose && str(verbose, args);
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # Setup command line switches
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  ## dotArgs <- trim(dotArgs);
+  ## args <- paste(keys, args, sep=" ");
+  args <- trim(args);
   verbose && cat(verbose, "Command line options:");
-  verbose && print(verbose, dotArgs);
+  verbose && print(verbose, args);
+
+  nms <- optionsList
+  nms <- paste(ifelse(nchar(nms) == 1, "-", "--"), nms, sep="")
+  names(optionsList) <- nms
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # System call
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   verbose && cat(verbose, "System call:");
-  cmd <- sprintf("%s %s", bin, paste(dotArgs, collapse=" "));
+  cmd <- sprintf("%s %s", bin, paste(args, collapse=" "));
   verbose && print(verbose, cmd);
-  verbose && str(verbose, system2ArgsList);
+  verbose && str(verbose, system2Args);
 
   verbose && enter(verbose, "system2() call");
-  callArgs <- list(command=bin, args=paste(names(dotArgs$args), dotArgs$args, sep=" "))
-
+  callArgs <- list(command=bin, args=args);
+  callArgs <- c(callArgs, system2Args);
   verbose && str(verbose, callArgs);
   if (!.fake) {
-    res <- do.call(what=base::system2, args=callArgs);
+    res <- do.call(base::system2, callArgs);
   } else {
     res <- "<fake run>";
   }
@@ -77,7 +90,7 @@ setMethodS3("systemTophat", "default", function(commandName,
   verbose && exit(verbose);
   verbose && exit(verbose);
   res;
-}) # systemTophat()
+}) # systemTopHatBuild()
 
 
 ############################################################################
