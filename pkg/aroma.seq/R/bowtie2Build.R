4587e1c6ff1327dd3a013ae8a167e83c2ed24b6d
tokuyasu <tokuyasu@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-03-08 18:24:11 +0000

    More standardization of var names, handling NULLs, etc. in bowtie2Build; is using paste(collapse) the right way to pass input read files to do.call??
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@2959 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.seq/R/bowtie2Build.R
+++ pkg/aroma.seq/R/bowtie2Build.R
@@ -11,9 +11,9 @@
 #
 # \arguments{
 #   \item{command}{Name of executable}
-#   \item{reads1}{Vector of fastq files to align, paired with reads2}
+#   \item{refReads}{Vector of (fasta) files with reference sequence}
 #   \item{bowtieRefIndexPrefix}{bowtie2 reference index to be built (partial pathname, i.e. minus the .x.bt2 suffix)}
-#   \item{optionsVec}{Vector of named options (do not include names x, 1, 2, U, or S)}
+#   \item{optionsVec}{Vector of named options}
 #   \item{overwrite}{(logical; not used)}
 #   \item{...}{...}
 
@@ -24,16 +24,15 @@
 #
 # @author
 #*/###########################################################################
-setMethodS3("bowtie2-build", "default", function(command='bowtie2-build',
-                                                 refReads=NULL,  ## vector of pathnames
-                                                 bowtieRefIndexPrefix=NULL, ##  ## Index filename prefix (i.e. minus trailing .X.bt2)
-                                                 optionsVec=NULL, ## vector of named options
-                                                 overwrite=FALSE,  ##
-                                                 ..., verbose=FALSE) {
+setMethodS3("bowtie2Build", "default", function(command='bowtie2-build',
+                                                refReads=NULL,  ## vector of pathnames
+                                                bowtieRefIndexPrefix=NULL, ## Index filename prefix (i.e. minus trailing .X.bt2)
+                                                ## bowtieRefIndexPrefix=file.path("refIdx", "refGenome"), ## (20130308 THIS IS CAUSING PROBLEMS)
+                                                optionsVec=NULL, ## vector of named options
+                                                overwrite=FALSE,  ##
+                                                ..., verbose=FALSE) {
 
   ## System usage: bowtie2-build [options]* <reference_in> <bt2_index_base>
-
-  browser()
 
   # Argument 'refReads'
   if (!is.null(refReads))
@@ -53,14 +52,22 @@ setMethodS3("bowtie2-build", "default", function(command='bowtie2-build',
   }
 
   ## Combine the above into "bowtie2 arguments"
-  bowtie2Args <- c(bowtieRefIndexPrefix, unname(refReads))
+  ## bowtie2Args <- c(bowtieRefIndexPrefix, unname(refReads))
+  ## bowtie2Args <- c(unname(refReads), bowtieRefIndexPrefix)   ## reference reads first, index base second
+  bowtie2Args <- c(paste(unname(refReads), collapse=","), bowtieRefIndexPrefix)   ## reference reads first, index base second
 
   ## Add dashes as appropriate to names of "bowtie2 options"
-  bowtie2Options <- optionsVec
-  nms <- names(bowtie2Options)
-  names(bowtie2Options) <- paste(ifelse(nchar(nms) == 1, "-", "--"), nms, sep="")
+  bowtie2Options <- NULL
+  if (!is.null(optionsVec)) {
+    bowtie2Options <- optionsVec
+    nms <- names(bowtie2Options)
+    names(bowtie2Options) <- paste(ifelse(nchar(nms) == 1, "-", "--"), nms, sep="")
+  }
 
   res <- do.call(what=systemBowtie2Build, args=list(command=command, args=c(bowtie2Options, bowtie2Args)))
+
+  ## DEV
+  ## res <- do.call(what=systemBowtie2Build, args=list(command=command, args=c(bowtie2Options, bowtie2Args), verbose=TRUE, system2ArgsList=list(stderr=TRUE)))
 
   return(res)
 })
