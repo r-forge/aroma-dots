a0da1a5a469dc12b980baab4f57d7f2b4b61c6bd
tokuyasu (Taku Tokuyasu) <tokbio@gmail.com>
2013-05-31 13:18:19 +0000

    Level 1 and 2 code for htseq-count
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@3063 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/aroma.seq/R/htseqCount.R
@@ -0,0 +1,79 @@
+###########################################################################/**
+# @RdocDefault htseqCount
+#
+# @title "Calls the htseq-count executable to count input reads on features"
+#
+# \description{
+#  @get "title".
+# }
+#
+# @synopsis
+#
+# \arguments{
+#   \item{command}{Name of executable}
+#   \item{samFile}{sam file containing aligned reads, sorted by *name* if paired end}
+#   \item{gf}{gene feature file, in gff format}
+#   \item{outFile}{name of file to receive htseq-count output}
+#   \item{optionsVec}{Vector of named options to pass to htseq-count}
+#   \item{...}{(Not used)}
+#   \item{verbose}{See @see "R.utils::Verbose".}
+# }
+#
+# @author "TT"
+#
+#*/###########################################################################
+setMethodS3("htseqCount", "default", function(samFile,
+                                              gf,
+                                              outFile=NULL,
+                                              optionsVec=c(s='no', a='10'),  ## Anders et al default
+                                              ..., command='htseq-count',
+                                              verbose=FALSE) {
+
+  ## ( Support a call like this: "htseq-count -s no -a 10 lib_sn.sam gf > countFile")
+
+  # Argument 'samFile'
+  if (!is.null(samFile)) {
+    samFile <- Arguments$getReadablePathname(samFile);
+  } else {
+    throw("Argument samFile is empty; supply sam file, sorted by name")
+  }
+
+  # Argument 'gf'
+  if (!is.null(gf)) {
+    gf <- Arguments$getReadablePathname(gf);
+  } else {
+    throw("Argument gf is empty; supply gene ('transcript') model file")
+  }
+
+  # Argument 'outFile'
+  if (!is.null(outFile)) {
+    outFile <- Arguments$getWritablePathname(outFile);
+  } else {
+    throw("Argument outFile is empty; supply output file name\n")
+  }
+
+
+  htseqArgs <- c(samFile, gf)
+  htseqArgs <- c(htseqArgs, " > ", outFile)
+
+  ## Add dashes as appropriate to names of "htseq options"
+  htseqOptions <- NULL
+  if (!is.null(optionsVec)) {
+    htseqOptions <- optionsVec
+    nms <- names(htseqOptions)
+    names(htseqOptions) <- paste(ifelse(nchar(nms) == 1, "-", "--"), nms, sep="")
+  }
+
+  res <- do.call(what=systemHtseqCount, args=list(command=command, args=c(htseqOptions, htseqArgs)))
+
+  return(res)
+})
+
+
+############################################################################
+# HISTORY:
+# 2013-05-31
+# o TT:  Created
+############################################################################
+
+
