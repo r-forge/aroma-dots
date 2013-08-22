97de5b808b043abba27dda328ef303bbaa8224c3
henrikb <henrikb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-08-22 23:33:43 +0000

    Version: 0.2.22 [2013-08-22] o Added doBWA() and doBowtie2().
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@3151 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/aroma.seq/R/doBWA.R
@@ -0,0 +1,120 @@
+###########################################################################/**
+# @RdocDefault doBWA
+# @alias doBWA.FastqDataSet
+#
+# @title "Read alignment using the Burrows-Wheeler Transform aligner (BWA)"
+#
+# \description{
+#  @get "title" based on [1].
+# }
+#
+# \usage{
+#   @usage doBWA
+#   @usage doBWA,FastqDataSet
+# }
+#
+# \arguments{
+#  \item{dataSet, df}{A @see "FastqDataSet".}
+#  \item{reference}{A @see "FastaReferenceFile" or a @see "BwaIndexSet" specifying the genome reference to align the FASTQ reads to.}
+#  \item{...}{Additional arguments passed to @see "BwaAlignment".}
+#  \item{verbose}{See @see "Verbose".}
+# }
+#
+# \value{
+#   Returns a @see "BamDataSet".
+# }
+#
+# \references{
+#   [1] Li H. and Durbin R., \emph{Fast and accurate short read alignment
+#       with Burrows-Wheeler Transform}. Bioinformatics, 2009.\cr
+#   [2] Li H. and Durbin R., \emph{Fast and accurate long-read alignment
+#       with Burrows-Wheeler Transform}. Bioinformatics, 2010.\cr
+# }
+#
+# @author "HB"
+#
+# \seealso{
+#  For more details, see @see "BwaAlignment".
+# }
+#
+# @keyword internal
+#*/###########################################################################
+setMethodS3("doBWA", "FastqDataSet", function(dataSet, reference, ..., verbose=FALSE) {
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Argument 'reference':
+  if (inherits(reference, "FastaReferenceFile")) {
+  } else if (inherits(reference, "BwaIndexSet")) {
+  } else {
+    throw("Argument 'reference' should either be of class 'FastaReferenceFile' or 'BwaIndexSet': ", class(reference)[1L]);
+  }
+
+  # Argument 'verbose':
+  verbose <- Arguments$getVerbose(verbose);
+  if (verbose) {
+    pushState(verbose);
+    on.exit(popState(verbose));
+  }
+
+
+  verbose && enter(verbose, "BWA");
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Checking requirements
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  verbose && enter(verbose, "BWA/Check requirements");
+  stopifnot(isCapableOf(aroma.seq, "bwa"));
+  verbose && exit(verbose);
+
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Setup
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # BWA 'aln' with options '-n 2' and '-q 40'.
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  verbose && enter(verbose, "BWA/Alignment");
+
+  # Retrieve BWA index set?
+  if (inherits(reference, "BwaIndexSet")) {
+    is <- reference;
+  } else if (inherits(reference, "FastaReferenceFile")) {
+    verbose && enter(verbose, "BWA/Alignment/Retrieving index set");
+    fa <- reference;
+    verbose && print(verbose, fa);
+    is <- buildBwaIndexSet(fa, method="is", verbose=verbose);
+    verbose && print(verbose, is);
+    verbose && exit(verbose);
+    # Not needed anymore
+    fa <- NULL;
+  }
+    # Not needed anymore
+  reference <- NULL;
+
+  alg <- BwaAlignment(dataSet, indexSet=is, ...);
+  verbose && print(verbose, alg);
+
+  bs <- process(alg, verbose=verbose);
+  verbose && print(verbose, bs);
+
+  verbose && exit(verbose);
+
+
+  verbose && exit(verbose);
+
+  bs;
+}) # doQDNASeq()
+
+
+setMethodS3("doBWA", "default", function(...) {
+  throw("The \"default\" method is still not implemented. Please see help('doBWA').");
+})
+
+
+############################################################################
+# HISTORY:
+# 2013-08-22
+# o Created.
+############################################################################
---
+++ pkg/aroma.seq/R/doBowtie2.R
@@ -0,0 +1,118 @@
+###########################################################################/**
+# @RdocDefault doBowtie2
+# @alias doBowtie2.FastqDataSet
+#
+# @title "Read alignment using the Bowtie v2 aligner"
+#
+# \description{
+#  @get "title" based on [1].
+# }
+#
+# \usage{
+#   @usage doBowtie2
+#   @usage doBowtie2,FastqDataSet
+# }
+#
+# \arguments{
+#  \item{dataSet, df}{A @see "FastqDataSet".}
+#  \item{reference}{A @see "FastaReferenceFile" or a @see "Bowtie2IndexSet" specifying the genome reference to align the FASTQ reads to.}
+#  \item{...}{Additional arguments passed to @see "Bowtie2Alignment".}
+#  \item{verbose}{See @see "Verbose".}
+# }
+#
+# \value{
+#   Returns a @see "BamDataSet".
+# }
+#
+# \references{
+#  [1] Bowtie2, John Hopkins University, 2013.
+#      \url{http://bowtie-bio.sourceforge.net/bowtie2/}
+# }
+#
+# @author "HB"
+#
+# \seealso{
+#  For more details, see @see "Bowtie2Alignment".
+# }
+#
+# @keyword internal
+#*/###########################################################################
+setMethodS3("doBowtie2", "FastqDataSet", function(dataSet, reference, ..., verbose=FALSE) {
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Argument 'reference':
+  if (inherits(reference, "FastaReferenceFile")) {
+  } else if (inherits(reference, "BwaIndexSet")) {
+  } else {
+    throw("Argument 'reference' should either be of class 'FastaReferenceFile' or 'BwaIndexSet': ", class(reference)[1L]);
+  }
+
+  # Argument 'verbose':
+  verbose <- Arguments$getVerbose(verbose);
+  if (verbose) {
+    pushState(verbose);
+    on.exit(popState(verbose));
+  }
+
+
+  verbose && enter(verbose, "Bowtie2");
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Checking requirements
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  verbose && enter(verbose, "Bowtie2/Check requirements");
+  stopifnot(isCapableOf(aroma.seq, "bwa"));
+  verbose && exit(verbose);
+
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Setup
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Bowtie2 'aln' with options '-n 2' and '-q 40'.
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  verbose && enter(verbose, "Bowtie2/Alignment");
+
+  # Retrieve Bowtie2 index set?
+  if (inherits(reference, "Bowtie2IndexSet")) {
+    is <- reference;
+  } else if (inherits(reference, "FastaReferenceFile")) {
+    verbose && enter(verbose, "Bowtie2/Alignment/Retrieving index set");
+    fa <- reference;
+    verbose && print(verbose, fa);
+    is <- buildBwaIndexSet(fa, method="is", verbose=verbose);
+    verbose && print(verbose, is);
+    verbose && exit(verbose);
+    # Not needed anymore
+    fa <- NULL;
+  }
+    # Not needed anymore
+  reference <- NULL;
+
+  alg <- Bowtie2Alignment(dataSet, indexSet=is, ...);
+  verbose && print(verbose, alg);
+
+  bs <- process(alg, verbose=verbose);
+  verbose && print(verbose, bs);
+
+  verbose && exit(verbose);
+
+
+  verbose && exit(verbose);
+
+  bs;
+}) # doQDNASeq()
+
+
+setMethodS3("doBowtie2", "default", function(...) {
+  throw("The \"default\" method is still not implemented. Please see help('doBowtie2').");
+})
+
+
+############################################################################
+# HISTORY:
+# 2013-08-22
+# o Created.
+############################################################################
