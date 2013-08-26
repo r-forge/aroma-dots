bcffe1e35b83f1905ef2810a6d67858b202e97ae
tokuyasu <tokuyasu@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-08-26 10:43:42 +0000

    Check-in working but deprecated code that treats paired-end data as a list of FastqDataSets.  This requires modifying AbstractAlignment constructor as well; this will be changed back to the current version after I get this checked in.  For some reason findCmd does not work on my MacBook, so reverted to using Sys.which for now.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@3159 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.seq/R/TopHatAlignment.R
+++ pkg/aroma.seq/R/TopHatAlignment.R
@@ -14,6 +14,7 @@
 # \arguments{
 #  \item{...}{Arguments passed to @see "AbstractAlignment".}
 #  \item{indexSet}{An @see "Bowtie2IndexSet".}
+#  \item{outputDirs}{A placeholder for a vector of output paths (overwritten by the code at present).}
 # }
 #
 # \section{Fields and Methods}{
@@ -32,10 +33,14 @@
 # }
 #*/###########################################################################
 
-setConstructorS3("TopHatAlignment", function(..., indexSet=NULL) {
+setConstructorS3("TopHatAlignment", function(..., indexSet=NULL, outputDirs=NULL) {
   # Validate arguments
   if (!is.null(indexSet)) {
     indexSet <- Arguments$getInstanceOf(indexSet, "Bowtie2IndexSet");
+  }
+  if (!is.null(outputDirs)) {
+    outputDirs <- Arguments$getWritablePath(outputDirs)
+    ## [ NB this is meant to be a vector in the case of multiple samples ]
   }
 
   # Arguments '...':
@@ -50,6 +55,45 @@ setMethodS3("getParameters", "TopHatAlignment", function(this, ...) {
   params <- c(params, getOptionalArguments(this, ...));
   params;
 }, protected=TRUE)
+
+
+setMethodS3("getPath", "TopHatAlignment", function(this, create=TRUE, ...) {
+  # [Create] Return the (sub-)directory tree for the data set
+  
+  # The full path
+  # path <- filePath(rootPath, fullname, platform);
+  path <- "tophat_out"  # Hard code the TopHat default for now
+  
+  if (create) {
+    path <- Arguments$getWritablePath(path);
+  } else {
+    path <- Arguments$getReadablePath(path, mustExist=FALSE);
+  }
+  
+  # Verify that it is not the same as the input path
+  if (is.list(ds)) {
+    inPath <- getPath(ds[[1]])
+  } else {
+    inPath <- getPath(ds);
+  }
+  if (getAbsolutePath(path) == getAbsolutePath(inPath)) {
+    throw("The generated output data path equals the input data path: ", path, " == ", inPath);
+  }
+  
+  path;
+}, protected=TRUE)
+
+setMethodS3("getOutputDataSet", "TopHatAlignment", function(this, ...) {
+  ## Find all existing output data files
+  res <- 
+    sapply(this$outputDirs, function(dir) {
+      BamDataSet$byPath(path=dir)
+    }, simplify=FALSE)
+  
+  ## TODO: Assert completeness
+  res;
+})
+
 
 
 setMethodS3("process", "TopHatAlignment", function(this, ..., skip=TRUE, force=FALSE, verbose=FALSE) {
@@ -90,8 +134,7 @@ setMethodS3("process", "TopHatAlignment", function(this, ..., skip=TRUE, force=F
     pushState(verbose);
     on.exit(popState(verbose));
   }
-
-
+  
   verbose && enter(verbose, "TopHat alignment");
   ds <- getInputDataSet(this);
   verbose && cat(verbose, "Input data set:");
@@ -109,16 +152,28 @@ setMethodS3("process", "TopHatAlignment", function(this, ..., skip=TRUE, force=F
     validate(rgSet);
   }
 
-  res <- do.call(what=tophat, args=list(getIndexPrefix(is),
-                                        reads1=sapply(ds$read1, getPathname),
-                                        reads2=sapply(ds$read2, getPathname)))
-  
+  # The following assumes ds is a list (i.e. paired-end data)
+  fullNames <- sub("_1.fastq", "", basename(getPathnames(ds$read1)))
+  outputDirs <- paste0("tophat_", fullNames)
+  for (i in seq_along(ds$read1))
+  {
+    res <- do.call(what=tophat, args=list(getIndexPrefix(is),
+                                          reads1=getPathnames(ds$read1)[i],
+                                          reads2=getPathnames(ds$read2)[i],
+                                          optionsVec=c("o"=outputDirs[i])))
+  }
+
+  this$outputDirs <- outputDirs  ## [ TAT - This is probably not aroma style]
+
   params <- getParameters(this);
   verbose && cat(verbose, "Additional tophat2 arguments:");
   verbose && str(verbose, params);
-
-  verbose && cat(verbose, "Number of files: ", length(ds));
-  outPath <- getPath(this);
+  if (is.list(ds)) { 
+    verbose && cat(verbose, "Number of input data file pairs: ", length(ds[[1]]));
+  } else {
+    verbose && cat(verbose, "Number of input data files: ", length(ds));
+  }
+  # outPath <- getPath(this); # [ 201308 Punt on this for now; need to resolve getName, etc. for paired end datasets ]
   res <- getOutputDataSet(this, verbose=less(verbose, 1));
 
   verbose && exit(verbose);
--- pkg/aroma.seq/R/systemTopHat.R
+++ pkg/aroma.seq/R/systemTopHat.R
@@ -44,7 +44,8 @@ setMethodS3("systemTopHat", "default", function(commandName="tophat",
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # Locate executable
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  bin <- findCmd(commandName, verbose=less(verbose, 50));
+  # bin <- findCmd(commandName, verbose=less(verbose, 50));
+  bin <- Sys.which("tophat")
   verbose && cat(verbose, "Executable: ", bin);
 
   verbose && cat(verbose, "Arguments passed to system2():");
@@ -71,7 +72,7 @@ setMethodS3("systemTopHat", "default", function(commandName="tophat",
   callArgs <- list(command=bin, args=paste(names(dotArgs$args), dotArgs$args, sep=" "))
   ## BUG:  This has unacceptable dependence on tophat() call using 'args' as the name for dotArgs
   callArgs <- c(callArgs, system2ArgsList)
-
+  
   verbose && str(verbose, callArgs);
   if (!.fake) {
     res <- do.call(what=base::system2, args=callArgs);
