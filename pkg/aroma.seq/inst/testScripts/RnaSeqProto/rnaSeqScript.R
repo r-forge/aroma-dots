404999766cc51d44b2df5d3963bdc4c92322d06a
tokuyasu (Taku Tokuyasu) <tokbio@gmail.com>
2013-08-29 14:22:08 +0000

    Replace TopHatAlignment with TopHat2Alignment; rnaSeqScript.R has (and should...) run end-to-end now; bump version to 0.2.27.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@3169 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.seq/inst/testScripts/RnaSeqProto/rnaSeqScript.R
+++ pkg/aroma.seq/inst/testScripts/RnaSeqProto/rnaSeqScript.R
@@ -9,6 +9,8 @@
 # - TopHat output probably goes into its own output folder
 # 
 
+bDEBUG <- FALSE
+
 library("aroma.seq")
 ## [- NB:  Order of library loading may be significant for the code to work properly ]
 
@@ -29,6 +31,10 @@ qualityEncoding <- "illumina" # c("sanger", "solexa", "illumina"); cf. qrqc:Read
 #   to get right for the quality assessment step.  In particular, the code bombs if it encounters phred
 #   scores outside range (might be worse if it does not bomb actually).
 bSetupR <- FALSE
+
+# For edgeR
+MinNumberOfReplicates <- 2  # Used in edgeR preprocessing as recommended by Anders et al; NB must be >= 3 for edgeR to work, I think
+Groups <- c("a", "a", "b", "b")  # Sample assignments to (two) groups
 
 
 # R packages, reference genome downloads
@@ -107,11 +113,13 @@ if (!bPairedEnd) {
   fqs <- FastqDataSet$byPath(pathLocalData, pattern="[.](fq|fastq)$")   ## This pattern should already be the default...
   print(fqs)
 } else {
-  fqs1 <- FastqDataSet$byPath(pathLocalData, pattern="[._]1.(fq|fastq)$")  ## (Should allow these to be .gz as wel)
-  fqs2 <- FastqDataSet$byPath(pathLocalData, pattern="[._]2.(fq|fastq)$")
-  print(fqs1)
-  print(fqs2)
-  fqs <- list(read1 = fqs1, read2 = fqs2)   ## [ Is this reasonable?  fqs = list => PE data? ]
+  if (FALSE)  {# First version:  paired end data as a list
+    fqs1 <- FastqDataSet$byPath(pathLocalData, pattern="[._]1.(fq|fastq)$")  ## (Should allow these to be .gz as wel)
+    fqs2 <- FastqDataSet$byPath(pathLocalData, pattern="[._]2.(fq|fastq)$")
+    fqs <- list(read1 = fqs1, read2 = fqs2)   ## [ Is this reasonable?  fqs = list => PE data? ]
+  } else { # New paired-end style FastqDataSet
+    fqs <- FastqDataSet$byPath(pathLocalData, paired=TRUE);
+  }
 }
 
 
@@ -122,13 +130,12 @@ if (!bPairedEnd) {
 # [ LIMITATION:  Following should not be req'd if reference indices already exist ]
 isCapableOf(aroma.seq, "bowtie2")
 
-## TODO:  Run only if index set does not exist
+
+# NB: bowtie2 is run only if the index set does not already exist
 system.time({
   is <- buildBowtie2IndexSet(refFasta, verbose=-10)  # is = 'index set'
   print(is)
 })
-# user  system elapsed 
-# 15.998   0.310  16.946 
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -160,42 +167,57 @@ if (FALSE) {  # Skip for now
 # Step 2: Gather experiment metadata
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-# ... [TBD; move this to after htseq?; probably a manual step; how to capture the experimental design?]
+# [TBD:  This is probably a manual step for the user;
+#  important especially if the input filenames are not sufficient / satisfactory for the user ]
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Step 3: Align reads
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-if (FALSE)  # run tophat 'by hand' first
+# Gene model
+gtfFile <- findFiles(path=pathLocalAnnots, pattern="gtf$", firstOnly=TRUE)
+# - MANUAL STEP:  User must specify which gene model to use, if more than one is available
+
+if (FALSE)  # Example of how to run tophat 'by hand'
 {
-  Reads1 <- getPathnames(fqs[[1]])[1]
-  Reads2 <- getPathnames(fqs[[2]])[1]
-  source("/Users/tokuyasu/SVN/aroma.seq.RS/R/tophat.R")
-  source("/Users/tokuyasu/SVN/aroma.seq.RS/R/systemTopHat.R")
-  ## - Crap, the needed fix is to make bin non-NULL using Sys.which("tophat")
+  fqs1 <- extract(fqs, 1)
+  Reads1 <- getPathname(getFile(fqs1, 1))
+  Reads2 <- getPathname(getMateFile(getFile(fqs1,1)))
+  # Alt1:
+  # Reads1 <- sapply(getFilePairs(fqs1)[,1], function(x) {getPathname(x)})[1]
+  # Reads2 <- sapply(getFilePairs(fqs1)[,2], function(x) {getPathname(x)})[1]
+  # Alt2:
+  # Reads1 <- getPathnames(fqs1)[1]
+  # Reads2 <- getPathname(getMateFile(getFile(fqs1, 1)))    
+  # source("/Users/tokuyasu/SVN/aroma.seq.RS/R/tophat.R")
+  # source("/Users/tokuyasu/SVN/aroma.seq.RS/R/systemTopHat.R")
+  ## - Possible remaining bug for bin assignment => use Sys.which("tophat")
   system.time({
     tophat(getIndexPrefix(is), reads1=Reads1, reads2=Reads2, verbose=TRUE)
   })
+  system.time({
+    tophat(getIndexPrefix(is), reads1=Reads1, reads2=Reads2, optionsVec=c("G"=gtfFile, "o"="tophat_byHand"), verbose=TRUE)
+  })
 }
 
-
-# source("/Users/tokuyasu/SVN/aroma.seq.RS/R/AbstractAlignment.R")
-# # - This is Taku's version, which allows dataset to be a list of FastqDataSet's
-# # source("/Users/tokuyasu/SVN/aroma.seq/R/TopHatAlignment.R")
-# source("/Users/tokuyasu/work/projects/12/CCSP/proto/TopHatAlignment.R")
-# source("/Users/tokuyasu/work/projects/12/CCSP/proto/tophat.R")
-# source("/Users/tokuyasu/work/projects/12/CCSP/proto/systemTopHat.R")  ## This is key - need to be able to find the tophat executable!
-ta <- TopHatAlignment(dataSet=fqs, indexSet=is)
-# - Works! (with Taku's version of AbstractAlignment)
-
-stop("Got to here!")
+source("/Users/tokuyasu/work/projects/12/CCSP/proto/TopHat2Alignment.R")
+if (FALSE) {  # This is DEBUG-only
+  source("/Users/tokuyasu/work/projects/12/CCSP/proto/tophat.R")
+  source("/Users/tokuyasu/work/projects/12/CCSP/proto/systemTopHat.R")  ## This is key - need to be able to find the tophat executable!
+}
+ta <- TopHat2Alignment(dataSet=fqs, indexSet=is)
+# Following is DEBUG-only
+if (bDEBUG) {
+  ta <- TopHat2Alignment(dataSet=extract(fqs, files=c(1,2)), indexSet=is)
+}
 
 # TODO:  Make this run only if not done already
 system.time({
   process(ta, verbose=TRUE)
 })
 
+taout <- getOutputDataSet(ta)  # For now, this is a GenericDataFileSet
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Step 4: Organize, sort and index the BAM files and create SAM files
@@ -205,17 +227,56 @@ system.time({
 
 ## [ ...Step 5 is now Step 8... ]
 
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Step 6: Count reads
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# proposed code ;
-counts <- countReads(pBAM(s), geneModel=..., method='htseq');
-# - ( Presumably 'counts' should be a class object as well? At least store location of any file output. )
 
-## HtseqDataSet, HtseqDataFile
-## - inherited from Generic...
-##  (Does htseq have a suggested way to import in R its export data?)
+if (FALSE) {
+  # Convert bam to sam
+  ods <- getOutputDataSet(ta)
+  acceptedHits <- sapply(sapply(ods, getPathnames, simplify=FALSE), function(x) {x[grep("accepted", x)]})
+  acceptedHitsSam <- sapply(acceptedHits, function(f) {Arguments$getWritablePathname(sub(".bam$", ".sam", f))})
+  sapply(seq_along(acceptedHits), function(i) {samtoolsView(acceptedHits[i], acceptedHitsSam[i])})
+  for (i in seq_along(acceptedHits)) {
+    samtoolsView(acceptedHits[i], acceptedHitsSam[i])
+  }
+  # - ADD TO WISH LIST: Replace acceptedHitsSam w/ in-memory object (say?); i.e. temporary / on-demand rather than
+  #   permanent addition to TopHatAlignment / file system.
+}
 
+# Convert TopHat accept_hits.bam to sam
+obams <- getPathnames(getOutputDataSet(ta))
+osams <- sub(".bam$", ".sam", obams)
+for (i in seq_along(obams)) {
+  samtoolsView(obams[i], osams[i])
+}
+
+system.time({
+  for (i in seq_along(osams)) {
+    samFile <- osams[i]
+    htseqCount(samFile=samFile, gfFile=gtfFile, outFile=sub(".sam$", ".count", samFile))
+  }
+})
+
+# Creating the count matrix 'by hand' may be useful, but skip for now, since readDGE() does this for us
+if (FALSE) {
+  # Load the htseq count files as a TabularTextFileSet
+  db <- TabularTextFileSet$byPath(path=getPath(ta), pattern="[.]count$", recursive=TRUE)
+  # - ISSUE:  This drops the first row, since treated as column names
+  
+  # Get the 2nd column from each htseq count table
+  mat <-
+    sapply(seq_along(db), function(i) {
+      ttf <- getFile(db, i)
+      col2 <- readColumns(ttf,2)
+    }, simplify=TRUE)
+  mat <- sapply(mat, function(x) x)  # Silly but works to convert list to matrix (...)
+  rns <- readColumns(getFile(db, 1), 1)[,1]
+  rownames(mat) <- rns
+  bDrop <- rns %in% c("no_feature", "ambiguous", "too_low_aQual", "not_aligned", "alignment_not_unique")
+  mat <- mat[!bDrop,]
+}
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Step 7a:  Set up edgeR count datastruct and filter reads
@@ -224,8 +285,9 @@ counts <- countReads(pBAM(s), geneModel=..., method='htseq');
 ## Step 1 edgeR Create container for count data and filter features ;
 ## Load the edgeR package and use the utility function, readDGE, to read in the COUNT files created from htseq-count:
 
-library("edgeR");
-counts <- readDGE(samples$countf)$counts;
+library(edgeR);
+hcounts <- sub(".sam$", ".count", osams)
+counts <- readDGE(hcounts)$counts;
 
 ## In edgeR, we recommend removing features without at least 1 read per million in n of the samples, where n is the size of the smallest group of replicates (here, n=3 for the Knockdown group). Filter these as well as non-informative (e. g., non-aligned) features using a command like: ;
 noint <- rownames(counts) %in%
@@ -233,27 +295,31 @@ noint <- rownames(counts) %in%
     "not_aligned","alignment_not_unique")
 cpms <- cpm(counts);
 
-# [ The following command might be messed up ]
-keep <- (rowSums(cpms>1)>=3) && !noint;
+# From Anders et al
+## 'In edgeR, we recommend removing features without at least 1 read per million in n of
+##  the samples, where n is the size of the smallest group of replicates'
+keep <- (rowSums(cpms>1)>=MinNumberOfReplicates) & !noint;
 counts <- counts[keep,];
+
+
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Step 7b: Visualize and create DGEList for edgeR
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ## Visualize and inspect the count table using: ;
-colnames(counts) <- samples$shortname;
-head( counts[,order(samples$condition)], 5 );
+
+
+colnames(counts) <- sub("_1$", "", getFullNames(getInputDataSet(ta)))
+# head( counts[,order(samples$condition)], 5 );
 
 ## Create a DGEList object (s container for RNA-seq count data), as follows: ;
-d <- DGEList(counts=counts, group=samples$condition);
-
+d <- DGEList(counts=counts, group=Groups);
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Step 8 (was Step 5): Inspect alignments with IGV
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # [ ... This is probably a useful but manual step; user intervention required; skip for auto-pipeline ] ;
-
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Run edgeR for differential expression
