bd83d126db6d2970c9ab309530386d2a98027fd8
henrikb (Henrik Bengtsson) <henrikb@braju.com>
2013-09-01 05:09:32 +0000

    Added system test for parallel BWA alignment.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@3178 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/aroma.seq/tests/doBWA,parallel.R
@@ -0,0 +1,47 @@
+library("aroma.seq")
+fullTest <- (Sys.getenv("_R_CHECK_FULL_") != "")
+fullTest <- fullTest && isCapableOf(aroma.seq, "bwa")
+fullTest <- fullTest && isPackageInstalled("BatchJobs")
+if (fullTest) {
+
+setOption(aromaSettings, "devel/BatchJobs", TRUE)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setup (writable) local data directory structure
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+pathD <- system.file("exData", package="aroma.seq")
+for (dir in c("annotationData", "fastqData")) {
+  copyDirectory(file.path(pathD, dir), to=dir, overwrite=FALSE)
+}
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setup FASTA reference file
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+path <- file.path("annotationData", "organisms", "LambdaPhage")
+fa <- FastaReferenceFile("lambda_virus.fa", path=path)
+print(fa)
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setup FASTQ set
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+path <- file.path("fastqData", "TopHat-example", "LambdaPhage")
+fqs <- FastqDataSet$byPath(path, pattern="[.](fq|fastq)$")
+print(fqs)
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Single-end alignment
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+bams <- doBWA(fqs, reference=fa, tags=c("*", "BatchJobs"), verbose=-20)
+print(bams)
+
+} # if (fullTest)
+
+
+############################################################################
+# HISTORY:
+# 2013-08-26
+# o Created from doBowtie2,parallel.R.
+############################################################################
