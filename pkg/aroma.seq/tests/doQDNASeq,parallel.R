c2e9d8a3db8100b24b2d60a95d93aed1986a79ee
henrikb (Henrik Bengtsson) <henrikb@braju.com>
2013-09-04 01:58:48 +0000

    Version: 0.2.29 [2013-09-03] o Added basic support for parallel processing of doQDNASeq() and   PicardDuplicateRemoval.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@3186 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/aroma.seq/tests/doQDNASeq,parallel.R
@@ -0,0 +1,67 @@
+library("aroma.seq")
+fullTest <- (Sys.getenv("_R_CHECK_FULL_") != "")
+fullTest <- fullTest && isCapableOf(aroma.seq, "bwa")
+fullTest <- fullTest && isCapableOf(aroma.seq, "picard")
+fullTest <- fullTest && isPackageInstalled("qdnaseq")
+fullTest <- fullTest && isDirectory("annotationData,aroma.seq,private");
+fullTest <- fullTest && isDirectory("fastqData,aroma.seq,private");
+fullTest <- fullTest && isPackageInstalled("BatchJobs")
+if (fullTest) {
+
+setOption(aromaSettings, "devel/BatchJobs", TRUE)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setup FASTA reference file
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+path <- file.path("annotationData,aroma.seq,private", "organisms", "Human")
+fa <- FastaReferenceFile("human_g1k_v37.fasta", path=path)
+print(fa)
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setup FASTQ set
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+path <- file.path("fastqData,aroma.seq,private", "AlbertsonD_2012-SCC,AB042", "Generic")
+fqs <- FastqDataSet$byPath(path, pattern="[.](fq|fastq)$")
+fqs <- extract(fqs, 1:2)
+print(fqs)
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# QDNASeq on FASTQ files
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+cns <- doQDNASeq(fqs, reference=fa, binWidth=100, verbose=-20)
+print(cns)
+
+# Display individual BAM files
+for (ii in seq_along(cns)) {
+  cn <- getFile(cns, ii)
+  print(cn)
+}
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# QDNASeq on BAM files
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+path <- "bamData/AlbertsonD_2012-SCC,AB042,bwa,is,-dups/Generic/"
+bams <- BamDataSet$byPath(path)
+print(bams)
+
+# QDNASeq on a single BAM file
+bf <- getFile(bams, 1)
+print(bf)
+cn <- doQDNASeq(bf, binWidth=100, verbose=-20)
+print(cn)
+
+# QDNASeq on a BAM file set
+cns <- doQDNASeq(bams, binWidth=100, verbose=-20)
+print(cns)
+
+} # if (fullTest)
+
+
+############################################################################
+# HISTORY:
+# 2013-08-31
+# o Created.
+############################################################################
