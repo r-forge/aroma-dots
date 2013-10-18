c869b518fbf4bdf23d91e19e507eb2462d3084e7
tokuyasu (Taku Tokuyasu) <tokbio@gmail.com>
2013-10-18 21:41:15 +0000

    Update TopHat2Alignment.R and tophat.R to solve "TopHat does not like commas" issue (dir only; deal w/ filenames with commas later).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@3255 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.seq/R/tophat.R
+++ pkg/aroma.seq/R/tophat.R
@@ -14,6 +14,8 @@
 #   \item{bowtieRefIndexPrefix}{bowtie2 reference index (partial pathname, i.e. minus the .x.bt2 suffix)}
 #   \item{reads1}{(required) Vector of fastq filenames to align}
 #   \item{reads2}{(optional) Vector of fastq filenames to align, paired with reads1}
+#   \item{initialTopHatOutDir}{(optional) TopHat output dir used by tophat executable}
+#   \item{finalTopHatOutDir}{(optional) TopHat output dir when this tophat() method exits}
 #   \item{optionsVec}{Vector of named options to pass to tophat}
 #   \item{...}{(Not used)}
 #   \item{verbose}{See @see "R.utils::Verbose".}
@@ -23,15 +25,26 @@
 #
 # @keyword internal
 #*/###########################################################################
-setMethodS3("tophat", "default", function(bowtieRefIndexPrefix=NULL,  ##
+setMethodS3("tophat", "default", function(bowtieRefIndexPrefix=NULL,
                                           reads1=NULL,
                                           reads2=NULL,
+                                          tophatInDir="tophatIn",
+                                          initialTopHatOutDir="tophatAroma",
+                                          finalTopHatOutDir=NULL,
                                           optionsVec=NULL,
                                           ## systems2ArgsList
                                           ..., command='tophat',
                                           verbose=FALSE) {
 
   ## ( Support a call like this: "tophat <options> bowtieRefIndexPrefix reads1 reads2" )
+
+  # Argument 'initialTopHatOutDir'
+  initialTopHatOutDir <- Arguments$getWritablePathname(initialTopHatOutDir)
+
+  # # Argument 'finalTopHatOutDir'
+  if (!is.null(finalTopHatOutDir)) {
+    finalTopHatOutDir <- Arguments$getWritablePathname(finalTopHatOutDir)
+  }
 
   # Argument 'bowtieRefIndexPrefix'
   # - check for bowtie2 reference index  ## TODO: ADD SUPPORT FOR BOWTIE1 INDICES
@@ -46,6 +59,9 @@ setMethodS3("tophat", "default", function(bowtieRefIndexPrefix=NULL,  ##
   if (!is.null(reads1))
     {
       reads1 <- sapply(reads1, FUN=Arguments$getReadablePathname)
+      # Use symbolic link to avoid TopHat issues w/ commas; this will still fail if the basename has commas
+      symLink <- createLink(link=file.path(tophatInDir, basename(reads1)), reads1)
+      reads1ForTopHat <- symLink
     } else {
       throw("Argument reads1 is empty; supply at least one input read file")
     }
@@ -54,20 +70,23 @@ setMethodS3("tophat", "default", function(bowtieRefIndexPrefix=NULL,  ##
   if (!is.null(reads2))
     {
       reads2 <- sapply(reads2, FUN=Arguments$getReadablePathname)
+      symLink <- createLink(link=file.path(tophatInDir, basename(reads2)), reads2)
+      reads2ForTopHat <- symLink
     }
 
   ## Combine the above into "tophat arguments"
-  if (!is.null(reads2)) {
+  if (!is.null(reads2ForTopHat)) {
     tophatArgs <- c(bowtieRefIndexPrefix,
-                    paste(reads1, collapse=","),
-                    paste(reads2, collapse=","))
+                    paste(reads1ForTopHat, collapse=","),
+                    paste(reads2ForTopHat, collapse=","))
   } else {  ## Technically just the above is probably ok
     tophatArgs <- c(bowtieRefIndexPrefix,
-                    paste(reads1, collapse=","))
+                    paste(reads1ForTopHat, collapse=","))
   }
 
   ## Add dashes as appropriate to names of "tophat options"
   tophatOptions <- NULL
+  optionsVec <- c(optionsVec, "o"=initialTopHatOutDir)
   if (!is.null(optionsVec)) {
     tophatOptions <- optionsVec
     nms <- names(tophatOptions)
@@ -75,6 +94,22 @@ setMethodS3("tophat", "default", function(bowtieRefIndexPrefix=NULL,  ##
   }
 
   res <- do.call(what=systemTopHat, args=list(command=command, args=c(tophatOptions, tophatArgs)))
+
+  if (!is.null(finalTopHatOutDir)) {
+    # Move TopHat output to 'final' (e.g. aroma.seq-specified) directory.
+    # Do some gymnastics to get list of files and subdirs w/o the parent dir name
+    dir0 <- getwd()
+    setwd(initialTopHatOutDir)
+    tophatOutFiles <- findFiles(path=".", firstOnly=FALSE, recursive=TRUE)
+    setwd(dir0)
+    sapply(tophatOutFiles, function(f) {renameFile(file.path(initialTopHatOutDir, f),
+                                                   file.path(finalTopHatOutDir, f), recursive=TRUE)})
+    removeDirectory(initialTopHatOutDir, recursive=TRUE)
+
+  }
+
+  # Clean up symbolic links to input files
+  removeDirectory(tophatInDir, recursive=TRUE)
 
   return(res)
 })
--- pkg/aroma.seq/inst/exData/ReferenceGenomes.R
+++ pkg/aroma.seq/inst/exData/ReferenceGenomes.R
@@ -3,13 +3,19 @@
 # Reference genomes and gene models from Ensembl for various organisms
 RefUrlsList  <- 
   list(
+
+    ## Cf. ftp://ftp.ensembl.org/pub/current_fasta/homo_sapiens/dna/
+    HomoSapiens = # ca. 201309
+      c(reference="ftp://ftp.ensembl.org/pub/release-73/fasta/homo_sapiens/dna/Homo_sapiens.GRCh37.73.dna.primary_assembly.fa.gz",
+        geneModel="ftp://ftp.ensembl.org/pub/release-73/gtf/homo_sapiens/Homo_sapiens.GRCh37.73.gtf.gz"),
+
     DrosophilaMelanogaster =  # ca. 201305
       c(reference="ftp://ftp.ensembl.org/pub/release-70/fasta/drosophila_melanogaster/dna/Drosophila_melanogaster.BDGP5.70.dna.toplevel.fa.gz",
         geneModel="ftp://ftp.ensembl.org/pub/release-70/gtf/drosophila_melanogaster/Drosophila_melanogaster.BDGP5.70.gtf.gz"),
     
     SaccharomycesCerevisiae = # ca. 201307
-      c(reference="ftp://ftp.ensembl.org/pub/release-72/fasta/saccharomyces_cerevisiae/dna/Saccharomyces_cerevisiae.EF4.72.dna.toplevel.fa.gz",
-        geneModel="ftp://ftp.ensembl.org/pub/release-72/gtf/saccharomyces_cerevisiae/Saccharomyces_cerevisiae.EF4.72.gtf.gz")
+      c(reference="ftp://ftp.ensembl.org/pub/release-73/fasta/saccharomyces_cerevisiae/dna/Saccharomyces_cerevisiae.EF4.73.dna.toplevel.fa.gz",
+        geneModel="ftp://ftp.ensembl.org/pub/release-73/gtf/saccharomyces_cerevisiae/Saccharomyces_cerevisiae.EF4.73.gtf.gz")
   )
 
 # save(RefUrlsList, file="RefUrlsList_000.RData")
