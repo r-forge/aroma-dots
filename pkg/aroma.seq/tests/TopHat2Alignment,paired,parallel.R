552ca98c27166b6cd07a36e6f9e33b634feeb4a2
henrikb (Henrik Bengtsson) <henrikb@braju.com>
2013-11-02 05:00:30 +0000

    Added system test for parallelized TopHat2Alignment
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@3310 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/aroma.seq/tests/TopHat2Alignment,paired,parallel.R
@@ -0,0 +1,48 @@
+library("aroma.seq")
+
+fullTest <- (Sys.getenv("_R_CHECK_FULL_") != "")
+fullTest <- fullTest && isCapableOf(aroma.seq, "bowtie2")
+fullTest <- fullTest && isCapableOf(aroma.seq, "tophat2")
+if (fullTest) {
+
+setOption(aromaSettings, "devel/BatchJobs", TRUE)
+
+dataSet <- "YeastTest"
+organism <- "SC"
+
+# Setup (writable) local data directory structure
+setupExampleData()
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setup data
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Annotation data
+path <- file.path("annotationData", "organisms", organism)
+fas <- FastaReferenceSet$byPath(path)
+fa <- getFile(fas, 1)  # Presuming there is only one reference fasta file
+print(fa)
+
+# FASTQ data
+path <- file.path("fastqData", dataSet, organism)
+fqs <- FastqDataSet$byPath(path, paired=TRUE)
+print(fqs)
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# TopHat2
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+is <- buildBowtie2IndexSet(fa, verbose=TRUE)  # is = 'index set'
+print(is)
+
+# Align input reads using TopHat
+ta <- TopHat2Alignment(dataSet=fqs, indexSet=is, tags=c("*", "parallel"))
+process(ta, verbose=TRUE)
+
+bams <- getOutputDataSet(ta)
+print(bams)
+
+# Sanity checks
+stopifnot(length(bams) == length(fqs))
+
+} # if (fullTest)
