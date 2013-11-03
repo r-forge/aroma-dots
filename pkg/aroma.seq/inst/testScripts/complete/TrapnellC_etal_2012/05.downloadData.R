29924510eb0f05d6c6b99d72e6dc0dd4c2b0d535
henrikb (Henrik Bengtsson) <henrikb@braju.com>
2013-11-03 01:28:29 +0000

    Added TopHat2 test from Trapnell et al. (2012)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@3322 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/aroma.seq/inst/testScripts/complete/TrapnellC_etal_2012/05.downloadData.R
@@ -0,0 +1,11 @@
+library("R.utils")
+
+# REFERENCES:
+# [1] http://tophat.cbcb.umd.edu/igenomes.shtml
+# [2] http://www.uniprot.org/taxonomy/7227
+url <- "ftp://igenome:G3nom3s4u@ussd-ftp.illumina.com/Drosophila_melanogaster/Ensembl/BDGP5.25/Drosophila_melanogaster_Ensembl_BDGP5.25.tar.gz"
+
+# Using UniProt's Organism Mnemomics
+organism <- "DROME"
+
+
