cfd156ba473d4b30951525f28564cff0f5f50afc
tokuyasu <tokuyasu@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-11-08 01:11:41 +0000

    Add level 2 method for samtools mpileup, built on systemSamtools()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@3327 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/aroma.seq/R/samtoolsMpileup.R
@@ -0,0 +1,65 @@
+###########################################################################/**
+# @RdocDefault samtoolsMpileup
+#
+# @title "Calls the samtools 'mpileup' command"
+#
+# \description{
+#  @get "title".
+# }
+#
+# @synopsis
+#
+# \arguments{
+#   \item{refFile}{input reference file}
+#   \item{bamFile}{input BAM file (currently only one file is supported)}
+#   \item{pathnameD}{output file (default 'mpileup.out')}
+#   \item{...}{Additional arguments specifying samtools 'mpileup' switches
+#     passed to @see "systemSamtools".}
+#   \item{verbose}{See @see "R.utils::Verbose".}
+# }
+#
+# @author "TT"
+#
+# @keyword internal
+#*/###########################################################################
+setMethodS3("samtoolsMpileup", "default", function(refFile, bamFile, pathnameD="mpileup.out", ..., verbose=FALSE) {
+
+  # support a call like this:  system(paste("samtools mpileup -uf", RefFile, BamFile, ">tmp1.out"))
+  
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Argument 'pathname':
+  refFile <- Arguments$getReadablePathname(refFile);
+  bamFile <- Arguments$getReadablePathname(bamFile);
+  
+  # Argument 'pathnameD':
+  pathnameD <- Arguments$getWritablePathname(pathnameD);
+
+  # Argument 'verbose':
+  verbose <- Arguments$getVerbose(verbose);
+  if (verbose) {
+    pushState(verbose);
+    on.exit(popState(verbose));
+  }
+
+  verbose && enter(verbose, "Running samtools 'mpileup'");
+
+  # Assert that input files are not overwritten
+  stopifnot((getAbsolutePath(pathnameD) != getAbsolutePath(refFile)) &&
+              (getAbsolutePath(pathnameD) != getAbsolutePath(bamFile)));
+  
+  res <- systemSamtools("mpileup", ..., refFile, bamFile, "stdout"=pathnameD, verbose=less(verbose, 10));
+
+  verbose && exit(verbose);
+
+  res;
+}) # samtoolsMpileup()
+
+
+############################################################################
+# HISTORY:
+# 2013-10-31
+# o Created as copy of samtoolsView.R [TT]
+############################################################################
