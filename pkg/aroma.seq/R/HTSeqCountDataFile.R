5cb12cfcafdeb32872eae4a935476f0a819f5830
henrikb <henrikb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-01-25 00:46:24 +0000

    Forgot to add two files.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@3495 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/aroma.seq/R/HTSeqCountDataFile.R
@@ -0,0 +1,39 @@
+###########################################################################/**
+# @RdocClass HTSeqCountDataFile
+#
+# @title "The HTSeqCountDataFile class"
+#
+# \description{
+#  @classhierarchy
+#
+#  A HTSeqCountDataFile represents a HT-Seq [1] (htseq-count) output data file.
+# }
+#
+# @synopsis
+#
+# \arguments{
+#   \item{...}{Arguments passed to @see "R.filesets::TabularTextFile".}
+# }
+#
+# \section{Fields and Methods}{
+#  @allmethods "public"
+# }
+#
+# @author "HB"
+#
+# \references{
+#  [1] Simon Anders, \emph{HTSeq: Analysing high-throughput sequencing
+#      data with Python}, EMBL, Jan 2014.
+#      \url{http://www-huber.embl.de/users/anders/HTSeq/} \cr
+# }
+#*/###########################################################################
+setConstructorS3("HTSeqCountDataFile", function(...) {
+  extend(TabularTextFile(..., columnNames=c("transcript", "count"), .verify=FALSE), "HTSeqCountDataFile")
+})
+
+
+##############################################################################
+# HISTORY:
+# 2014-01-24
+# o Created.
+##############################################################################
