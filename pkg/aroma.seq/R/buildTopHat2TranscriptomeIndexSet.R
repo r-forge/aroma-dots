86c1e2b8ae1d701546a02a8ffb3b7b3c33c6aa4c
henrikb (Henrik Bengtsson) <henrikb@braju.com>
2014-02-07 17:51:00 +0000

    Added missing Rd file.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@3512 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.seq/R/buildTopHat2TranscriptomeIndexSet.R
+++ pkg/aroma.seq/R/buildTopHat2TranscriptomeIndexSet.R
@@ -1,7 +1,8 @@
 ###########################################################################/**
 # @set "class=Bowtie2IndexSet"
 # @RdocMethod buildTopHat2TranscriptomeIndexSet
-# 
+# @alias buildTopHat2TranscriptomeIndexSet
+#
 # @title "Calls the TopHat executable to build a transcriptome index on a gene model in a reference genome"
 #
 # \description{
@@ -30,14 +31,14 @@ setMethodS3("buildTopHat2TranscriptomeIndexSet", "Bowtie2IndexSet", function(thi
                                                                              tOutDir=NULL,
                                                                              tiOutPrefix=NULL,
                                                                              ..., verbose=FALSE) {
-  
-  
+
+
   organism <- getOrganism(this)
-  
+
   # Argument gtf
   gtfFile <- getPathname(gtf)
   gtfFile <- Arguments$getReadablePathname(gtfFile)
-  
+
   # Argument tiOutPrefix
   if (is.null(tiOutPrefix)) {
     tiOutDir <- file.path("annotationData", "organisms", organism, "tophat2")
@@ -47,30 +48,30 @@ setMethodS3("buildTopHat2TranscriptomeIndexSet", "Bowtie2IndexSet", function(thi
   }
   Arguments$getWritablePath(tiOutDir)
   stopifnot(!is.null(basename(tiOutPrefix)))
-  
+
   # Argument tOutDir
   if (is.null(tOutDir)) {
     tOutDir <- file.path(tiOutDir, "tophat2Data")
   }
   Arguments$getWritablePath(tOutDir)
-  
+
   # Argument 'verbose':
   verbose <- Arguments$getVerbose(verbose);
   if (verbose) {
     pushState(verbose);
     on.exit(popState(verbose), add=TRUE);
   }
-  
+
   # Check for tophat version
   verT <- attr(findTopHat2(), "version");
   if (verT < '2.0.10') {
     throw("TopHat version >= 2.0.10 required")
   }
-  
+
   refIdxPrefix <- getIndexPrefix(this)
-  
+
   # Call TopHat executable
-  
+
   verbose && enter(verbose, "Building Bowtie2 index set");
 
   stopifnot(isCapableOf(aroma.seq, "bowtie2"));
@@ -79,13 +80,13 @@ setMethodS3("buildTopHat2TranscriptomeIndexSet", "Bowtie2IndexSet", function(thi
   #  'tophat2 -o tOutDir -G gtf --transcriptome-index=tiOutPrefix refIdx' )
   res <- tophat(refIdxPrefix, gtf=gtfFile, optionsVec=c("-o"=tOutDir, "--transcriptome-index"=tiOutPrefix))
   # (0125 TAT - This may not work, because -o is specified in tophat() itself??)
-  
+
   # Locate existing index files
   res <- tryCatch({
     Bowtie2IndexSet$byPrefix(tiOutPrefix);
   }, error=function(ex) Bowtie2IndexSet());
-    
+
   verbose && exit(verbose);
-  
+
   res
 }) # buildTopHat2TranscriptomeIndexSet()
