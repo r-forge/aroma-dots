63b6cecfaa3aa89b681cd29b4302cf49fd7b0320
henrikb (Henrik Bengtsson) <henrikb@braju.com>
2014-02-21 04:34:26 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@3524 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.seq/inst/buildScripts/organisms/MusMusculus/01.mergeFASTA.R
+++ pkg/aroma.seq/inst/buildScripts/organisms/MusMusculus/01.mergeFASTA.R
@@ -14,11 +14,20 @@ print(fas)
 # Sanity check
 stopifnot(length(fas) == 22L)
 
-# Human-readable sorting?
+# Order in which chromosomes are merged?
+# (a) chr1, ..., chr9, chr10, ..., chr19, chrM, chrX, chrY
 fas <- sortBy(fas, "mixedsort")
+# (b) chr1, chr10, chr11, ..., chr19, chr2, ..., chr9, chrM, chrX, chrY
+fas <- sortBy(fas, "lexicographic")
+# (c) chr10, chr11, ..., chr19, chr1, chr2, ..., chr9, chrM, chrX, chrY
+#     This is how it's done in ftp://ussd-ftp.illumina.com/Mus_musculus/
+#     UCSC/mm9/Mus_musculus_UCSC_mm9.tar.gz
+#     Doing it this way creates an identical file
+fas <- sortBy(fas, "lexicographic")
+fas <- extract(fas, order(nchar(getNames(fas)), decreasing=TRUE))
 
 # Merge FASTA files
-fa <- writeFastaReferenceFile(fas, filename="mm9,chr1-22.fa", verbose=TRUE)
+fa <- writeFastaReferenceFile(fas, filename="MusMusculus_mm9_chr1-22.fa", verbose=TRUE)
 print(fa)
 
 
