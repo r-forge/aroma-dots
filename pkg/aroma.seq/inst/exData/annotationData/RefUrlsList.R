ddf6198026c11df27dd1d0f8afebdeeb0454efa1
tokuyasu (Taku Tokuyasu) <tokbio@gmail.com>
2014-03-04 01:59:39 +0000

    Add annotations related to Anders et al Nature Protocol; change ReferenceGenomes.R to RefUrlsList.R; clean up testRnaSeq.R to reflect latest functions, work in progress.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@3551 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.seq/inst/exData/annotationData/RefUrlsList.R
+++ pkg/aroma.seq/inst/exData/annotationData/RefUrlsList.R
@@ -1,24 +1,27 @@
-# source("ReferenceGenomes.R")
+# source("RefUrlsList.R")
 
 # Reference genomes and gene models from Ensembl for various organisms
-RefUrlsList  <- 
+RefUrlsList <- 
   list(
-
-    ## Cf. ftp://ftp.ensembl.org/pub/current_fasta/homo_sapiens/dna/
-    HomoSapiens = # ca. 201309
+    
+    # Cf. ftp://ftp.ensembl.org/pub/current_fasta/homo_sapiens/dna/
+    HS = # homo sapiens, ca. 201309
       c(reference="ftp://ftp.ensembl.org/pub/release-73/fasta/homo_sapiens/dna/Homo_sapiens.GRCh37.73.dna.primary_assembly.fa.gz",
         geneModel="ftp://ftp.ensembl.org/pub/release-73/gtf/homo_sapiens/Homo_sapiens.GRCh37.73.gtf.gz"),
+    
+    DM = # Drosophila Melanogaster, ca. 201305
+      c(reference="ftp://ftp.ensembl.org/pub/release-70/fasta/drosophila_melanogaster/dna/Drosophila_melanogaster.BDGP5.70.dna.toplevel.fa.gz",
+        geneModel="ftp://ftp.ensembl.org/pub/release-70/gtf/drosophila_melanogaster/Drosophila_melanogaster.BDGP5.70.gtf.gz"),
 
-    DrosophilaMelanogaster =  # ca. 201305
+    DM70 = # Used in Anders et al Nature Protocol
       c(reference="ftp://ftp.ensembl.org/pub/release-70/fasta/drosophila_melanogaster/dna/Drosophila_melanogaster.BDGP5.70.dna.toplevel.fa.gz",
         geneModel="ftp://ftp.ensembl.org/pub/release-70/gtf/drosophila_melanogaster/Drosophila_melanogaster.BDGP5.70.gtf.gz"),
     
-    SaccharomycesCerevisiae = # ca. 201307
+    SC = # SaccharomycesCerevisiae, ca. 201307
       c(reference="ftp://ftp.ensembl.org/pub/release-73/fasta/saccharomyces_cerevisiae/dna/Saccharomyces_cerevisiae.EF4.73.dna.toplevel.fa.gz",
         geneModel="ftp://ftp.ensembl.org/pub/release-73/gtf/saccharomyces_cerevisiae/Saccharomyces_cerevisiae.EF4.73.gtf.gz")
   )
-
-# save(RefUrlsList, file="RefUrlsList_000.RData")
+save(RefUrlsList, file="RefUrlsList_000.RData")
 
 
 
