74868c9e580c2c72a11317635a058b1dc1783f06
henrikb <henrikb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-03-10 02:35:26 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@3567 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.seq/R/htseqCount.R
+++ pkg/aroma.seq/R/htseqCount.R
@@ -10,16 +10,33 @@
 # @synopsis
 #
 # \arguments{
-#   \item{pathnameS}{source file containing aligned reads; .sam and .bam supported}
-#   \item{pathnameD}{(Optional) destination file to receive htseq-count output; use pathnameS with .count extension when NULL}
-#   \item{gff}{gene feature file, in gff format}
-#   \item{optionsVec}{Vector of named options to htseq-count}
+#   \item{pathnameS}{An input BAM or SAM file containing aligned reads.}
+#   \item{pathnameD}{(optional) destination file to save htseq-count output.}
+#   \item{gff}{The gene feature file, in GFF/GTF format}
+#   \item{optionsVec}{A named @character @vector of options to htseq-count.}
 #   \item{...}{(Not used)}
-#   \item{command}{Name of executable}
+#   \item{command}{A @character string specifying the name of the executable.}
 #   \item{verbose}{See @see "R.utils::Verbose".}
 # }
 #
-# @author "TT"
+# \value{
+#   Returns what @see "systemHTSeqCount" returns.
+# }
+#
+# \details{
+#   The \code{htseq-count} executable requires (i) a SAM file as input
+#   that (ii) is sorted by name.  However, this method will take of that
+#   internally, iff needed.  That is, it will created a temporary SAM
+#   file that is sorted by query name before passing it to \code{htseq-count}.
+# }
+#
+# \references{
+#  [1] S Anders, TP Pyl, W Huber,
+#      HTSeq - A Python framework to work with high-throughput sequencing data.
+#      bioRxiv 2014. doi: 10.1101/002824.\cr
+# }
+#
+# @author "TT,HB"
 #
 # @keyword internal
 #*/###########################################################################
@@ -30,9 +47,11 @@ setMethodS3("htseqCount", "default", function(pathnameS,
                                               ...,
                                               command='htseq-count',
                                               verbose=FALSE) {
-  
-  ## ( Support a call like this: "htseq-count -s no -a 10 lib_sn.sam gff > countFile")
 
+  ## ( Support a call like this: "htseq-count -s no -a 10 lib_sn.sam gff > countFile")
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Local functions
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   R.utils::use("Rsamtools")
 
   # BACKWARD COMPATIBILITY: Add asSam(), iff missing.
@@ -46,54 +65,90 @@ setMethodS3("htseqCount", "default", function(pathnameS,
     } # asSam()
   }
 
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # Argument 'pathnameS'
   pathnameS <- Arguments$getReadablePathname(pathnameS);
-  
+  # Assert BAM or SAM file
+  if (regexpr(".*[.](bam|sam)$", pathnameS, ignore.case=TRUE) == -1L) {
+    throw("Not a BAM or SAM file: ", pathnameS);
+  }
+
   # Argument 'pathnameD'
   if (is.null(pathnameD)) {
     pathnameD <- sub("[.](bam|sam)$", ".count", pathnameS, ignore.case=TRUE)
-    if (pathnameD==pathnameS) { # should not happen
+    if (pathnameD == pathnameS) { # should not happen
       pathnameD <- paste(pathnameS, ".count", sep="")
     }
   }
-  stopifnot(pathnameD!=pathnameS)
+  stopifnot(pathnameD != pathnameS)
   pathnameD <- Arguments$getWritablePathname(pathnameD, mustNotExist=TRUE);
 
   # Argument 'gff'
   gff <- Arguments$getReadablePathname(gff);
-  
-  ########
-  # This methods uses somewhat convoluted sam / bam manipulations:
-  # (sam input > ) bam input > sorted bam > sorted sam > htseq-count
-  ########
-  
-  # Create/setup bam input for sorting
-  if (regexpr(".*[.]sam$", pathnameS, ignore.case=TRUE) != -1) {  # match .sam
-    inBam <- sub("[.]sam$", ".InTmp.bam", pathnameS, ignore.case=TRUE)
-    inBam <- Arguments$getWritablePathname(inBam, mustNotExist=TRUE)
-    asSam(pathnameS, inBam)
+
+  # Argument 'verbose':
+  verbose <- Arguments$getVerbose(verbose);
+  if (verbose) {
+     pushState(verbose);
+     on.exit(popState(verbose), add=TRUE);
+  }
+
+
+  verbose && enter(verbose, "Running htseqCount()");
+  verbose && cat(verbose, "Input BAM/SAM file: ", sQuote(pathnameS));
+  verbose && cat(verbose, "Genome transcript file: ", sQuote(gff));
+  verbose && cat(verbose, "Output count file: ", sQuote(pathnameD));
+  verbose && cat(verbose, "htseq-count executable: ", command);
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Make sure to provide htseq-count with a SAM file sorted by query name
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # (a) Create/setup BAM input for sorting, iff SAM file is passed
+  isSAM <- (regexpr(".*[.]sam$", pathnameS, ignore.case=TRUE) != -1L);
+  if (isSAM) {
+    verbose && enter(verbose, "Converting BAM to SAM");
+    inBam <- sub("[.]sam$", ",tmp.bam", pathnameS, ignore.case=TRUE);
+    inBam <- Arguments$getWritablePathname(inBam, mustNotExist=TRUE);
+    asSam(pathnameS, inBam);
     on.exit({
       file.remove(inBam)
     }, add=TRUE)
-  } else if (regexpr(".*[.]bam$", pathnameS, ignore.case=TRUE) != -1) {  # match .bam
-    inBam <- pathnameS
+    # Sanity check
+    inBam <- Arguments$getReadablePathname(inBam);
+    verbose && exit(verbose);
   } else {
-    throw("Not a known (.bam/.sam) file extension: ", pathnameS);
+    inBam <- pathnameS;
   }
-  
-  # Sort input bam by name
-  inBamS <- sub("[.]bam$", ",ByNameTmp.bam", inBam)
+
+  # (b) Sort input BAM by name
+  verbose && enter(verbose, "Sorting BAM by query name");
+  inBamS <- sub("[.]bam$", ",byName,tmp.bam", inBam)
   inBamS <- Arguments$getWritablePathname(inBamS, mustNotExist=TRUE)
   sortBam(inBam, sub("[.]bam$", "", inBamS), byQname=TRUE)
-  
-  # Convert sorted bam to sam (needed by htseq-count)
+  # Sanity check
+  inBamS <- Arguments$getReadablePathname(inBamS)
+  verbose && exit(verbose);
+
+  # (c) Convert sorted BAM to SAM (needed by htseq-count)
+  verbose && enter(verbose, "Converting sorted BAM to SAM");
   inSamS <- sub("[.]bam$", ".sam", inBamS)
   inSamS <- Arguments$getWritablePathname(inSamS)
   samtoolsView(inBamS, inSamS)
   on.exit({
     file.remove(inBamS, inSamS)
   }, add=TRUE)
-  
+  # Sanity checks
+  inSamS <- Arguments$getReadablePathname(inSamS)
+  verbose && exit(verbose);
+
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Call htseq-count
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  verbose && enter(verbose, "Calling systemHTSeqCount()");
   htseqArgs <- c(inSamS, gff)
   htseqArgs <- c(htseqArgs, " > ", pathnameD)
   htseqOptions <- NULL
@@ -102,19 +157,29 @@ setMethodS3("htseqCount", "default", function(pathnameS,
     # Deprecate this (dashes now required to be added by the user)
     # nms <- names(htseqOptions); names(htseqOptions) <- paste(ifelse(nchar(nms) == 1, "-", "--"), nms, sep="")
   }
-  
-  res <- do.call(what=systemHTSeqCount, args=list(command=command, args=c(htseqOptions, htseqArgs)))
-  
-  return(res)
+  args <- list(command=command, args=c(htseqOptions, htseqArgs));
+  verbose && cat(verbose, "Arguments passed:");
+  verbose && str(verbose, args);
+
+  res <- do.call(what=systemHTSeqCount, args=args);
+  verbose && exit(verbose);
+
+  verbose && exit(verbose);
+
+  res;
 })
 
 
 ############################################################################
 # HISTORY:
-# 2013-12-16
-# o TT:  local asSam() added (needed for Rsamtools < 1.15.14)
-# 2013-11-29
-# o TT:  Internal sort-by-name added
-# 2013-05-31
-# o TT:  Created
+# 2014-03-09 [HB]
+# o Added verbose output to htseqCount().
+# o Temporary filenames are now using aroma-style tags.
+# o DOCUMENTATION: Added Details section to help.
+# 2013-12-16 [TT]
+# o local asSam() added (needed for Rsamtools < 1.15.14).
+# 2013-11-29 [TT]
+# o Internal sort-by-name added.
+# 2013-05-31 [TT]
+# o Created.
 ############################################################################
