950501911a5cd1b6a38f6bdeb9c37919d0d77ef2
tokuyasu (Taku Tokuyasu) <tokbio@gmail.com>
2014-03-18 20:00:22 +0000

    Cleaned up tmp / final dir handling; eliminated outPathOrg; issue - cannot handle incremental situation (fastqDump once, then fastqDump again) due to renaming to preexisting dir (the dir renameis essential to atomicity though).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@3582 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.seq/R/fastqDump.R
+++ pkg/aroma.seq/R/fastqDump.R
@@ -11,27 +11,48 @@
 #
 # \arguments{
 #   \item{...}{Additional arguments passed to @see "systemFastqDump".}
+#   \item{bSplit3}{If TRUE, use '--split-3' option to produce two fastq files for paired-end data}
+#   \item{bGzip}{If TRUE, gzip output fastq files}
 #   \item{outPath}{Directory where output fastq files will reside at completion}
 #   \item{pathnames}{Zero or more input pathnames.}
-#   \item{split3}{If TRUE, add '--split-3' option to produce two files in case of paired-end data}
+
 #   \item{verbose}{See @see "R.utils::Verbose".}
+
 # }
 #
 # @author "TT"
 #
+# \references{
+# }
+#
 # @keyword internal
 #*/###########################################################################
 setMethodS3("fastqDump", "default", function(...,
-                                             split3=TRUE,
+                                             bSplit3=TRUE,
+                                             bGzip=TRUE,
                                              outPath="fastqData",
                                              pathnames=character(0L), verbose=FALSE) {
+  
+  # Required statement for onExit to be evaluated
+  on.exit(eval(onExit()));
+  
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # Validate arguments
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  
+  # Argument 'bSplit3':
+  stopifnot(is.logical(bSplit3))
+  
+  # Argument 'bGzip':
+  stopifnot(is.logical(bGzip))
+  
+  # Argument 'outPath'
+  outPath <- Arguments$getWritablePath(outPath)
+  
   # Argument 'pathnames':
   pathnames <- lapply(pathnames, FUN=Arguments$getReadablePathname);
   pathnames <- unlist(pathnames, use.names=TRUE);
-
+  
   # Argument 'verbose':
   verbose <- Arguments$getVerbose(verbose);
   if (verbose) {
@@ -48,28 +69,27 @@ setMethodS3("fastqDump", "default", function(...,
   # Generate output atomically by writing to a temporary directory
   # that is renamed upon completion.
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  outPathOrg <- outPath;
-  outPath <- sprintf("%s.TMP", outPath);
-  outPath <- Arguments$getWritablePath(outPath, mustNotExist=TRUE);
-  verbose && cat(verbose, "Temporary output directory: ", outPath);
-  # At the end, assume failure, unless successful (whereupon outPathFinal is changed to outPathOrg)
-  outPathFinal <- sprintf("%s.ERROR", outPath);
+  
+  outPathTmp <- sprintf("%s.TMP", outPath);
+  outPathTmp <- Arguments$getWritablePath(outPathTmp, mustNotExist=TRUE);
+  verbose && cat(verbose, "Temporary output directory: ", outPathTmp);
+  # At the end, assume failure, unless successful (and outPathFinal is changed to outPath)
+  outPathFinal <- sprintf("%s.ERROR", outPathTmp);
   onExit({
-    # removeDirectory(outPathOrg, recursive=FALSE, mustExist=FALSE);
-    file.rename(outPath, outPathFinal);
+    file.rename(outPathTmp, outPathFinal);
   });
   
+    
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # While running, let the output directory be the working directory.
   # Make sure the working directory is restored when exiting.
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  opwd <- setwd(outPath);
+  opwd <- setwd(outPathTmp);
   verbose && cat(verbose, "Original working directory: ", opwd);
   onExit({
     verbose && cat(verbose, "Resetting working directory: ", opwd);
     setwd(opwd);
   });
-  
   
   args <- list(...);
   if (length(pathnames) > 0L) {
@@ -77,22 +97,15 @@ setMethodS3("fastqDump", "default", function(...,
     pathnames <- sprintf('"%s"', pathnames);
     args <- c(list(pathnames), args);
   }
-  if (!is.null(outPath)) {
-    args <- c(args, sprintf('-O "%s"', outPath))
-  }
-  if (split3) {
+  if (bSplit3) {
     args <- c(args, "--split-3")    
   }
 
-  browser()
-  
   verbose && cat(verbose, "Arguments:");
   verbose && str(verbose, args);
   args <- c(args,verbose=less(verbose, 10));
-  
-  browser()
-  
   res <- do.call(systemFastqDump, args=args);
+  
   status <- attr(res, "status"); if (is.null(status)) status <- 0L;
   verbose && cat(verbose, "Results:");
   verbose && str(verbose, res);
@@ -100,20 +113,21 @@ setMethodS3("fastqDump", "default", function(...,
   verbose && str(verbose, status);
   verbose && exit(verbose);
   
-
   # Successful?
   if (status == 0L) {
     # If we get this far, assume it was all successful.
+    fastqFiles <- findFiles(path=".", pattern="[.]fastq$", firstOnly=FALSE)
+    if (bGzip) {
+        fastqFiles <- sapply(fastqFiles, function(f) gzip(f), simplify=TRUE)
+    }
     # Allow the temporary output path to be renamed to the
     # intended output path instead of the "error" one.
-    outPathFinal <- outPathOrg;
+    outPathFinal <- outPath
   }
   
-  browser()
-  
   verbose && exit(verbose);
-
   res;
+
 }) # fastqDump()
 
 
