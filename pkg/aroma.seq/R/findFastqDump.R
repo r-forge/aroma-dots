cc121d7c4c2f9faa3e8d06e57fdf1a91d518bc41
tokuyasu (Taku Tokuyasu) <tokbio@gmail.com>
2014-03-20 09:02:25 +0000

    Use Arguments$getLogical instead of stopifnot; correct versionPattern and make it a function arg.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@3583 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.seq/R/findFastqDump.R
+++ pkg/aroma.seq/R/findFastqDump.R
@@ -11,6 +11,8 @@
 #
 # \arguments{
 #   \item{...}{Not used.}
+#   \item{commandName}{'fastq-dump'; command name to find}
+#   \item{versionPattern}{regexp to use if version not found properly; default should work}
 #   \item{verbose}{See @see "R.utils::Verbose".}
 # }
 #
@@ -18,7 +20,11 @@
 #
 # @keyword internal
 #*/###########################################################################
-findFastqDump <- function(commandName='fastq-dump',...) {
+findFastqDump <- function(...,
+                          commandName='fastq-dump',
+                          versionPattern=".*fastq-dump[ ]*:[ ]*([0-9.]+)",
+                          verbose=FALSE)
+{
   # Argument 'verbose':
   verbose <- Arguments$getVerbose(verbose);
   if (verbose) {
@@ -26,8 +32,8 @@ findFastqDump <- function(commandName='fastq-dump',...) {
     on.exit(popState(verbose));
   }
   verbose && enter(verbose, "Locating FastqDump software");
-  versionPattern <- "fastq-dump[ ]*:[s ]*([0-9.]+)";
-  findExternal(command=commandName, versionPattern=versionPattern, ...);
+  res <- findExternal(command=commandName, versionPattern=versionPattern, ...);
+  res
 } # findFastqDump()
 
 ############################################################################
