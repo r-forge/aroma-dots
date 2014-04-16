3ae4cca163be36e35f4dcc63624d0b9f8a093412
henrikb (Henrik Bengtsson) <henrikb@braju.com>
2014-04-16 21:59:03 +0000

    Tagging tests as either SE or PE.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aroma-dots@3620 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/aroma.seq/tests/TotalCnBinnedCounting,PE.R
+++ pkg/aroma.seq/tests/TotalCnBinnedCounting,PE.R
@@ -1,39 +1,8 @@
-############################################################################
-# DESCRIPTION:
-# This script shows how to (1) align single-end DNAseq reads in FASTQ files
-# to the human genome (FASTA file), (2) count the aligned reads (BAM files)
-# in uniformely distributed 50kb bins (UGP file), (3) normalize the counts
-# for amount of GC-content in each bins (UNC file), and (4) finally
-# segment the normalized DNAseq total copy-number counts using
-# Circular Binary Segmentation (CBS) and (5) generate an interactive
-# Chromosome Explorer report viewable in the browser.
-#
-# REQUIREMENTS:
-# fastqData/
-#  AlbertsonD_2012-SCC/
-#   HomoSapiens/
-#    <sample>_<barcode>_L[0-9]{3}_R[12]_[0-9]{3}.fastq [private data]
-#
-# annotationData/
-#  chipTypes/
-#   GenericHuman/
-#    GenericHuman,50kb,HB20090503.ugp [1]
-#    GenericHuman,50kb,HB20121021.unc [1]
-#  organisms/
-#   HomoSapiens/
-#    human_g1k_v37.fasta [2]
-#
-# REFERENCES:
-# [1] http://aroma-project.org/data/annotationData/chipTypes/GenericHuman/
-# [2] ftp://ftp-trace.ncbi.nih.gov/1000genomes/ftp/technical/reference/
-#                                                   human_g1k_v37.fasta.gz
-############################################################################
 library("aroma.seq")
 setOption(aromaSettings, "devel/parallel", "none")
 
 fullTest <- (Sys.getenv("_R_CHECK_FULL_") != "")
 fullTest <- fullTest && isCapableOf(aroma.seq, "bwa")
-fullTest <- fullTest && isPackageInstalled("BatchJobs")
 if (fullTest) {
 # Setup (writable) local data directory structure
 setupExampleData()
@@ -59,7 +28,7 @@ print(fqs)
 # Single-end alignment
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Build Bowtie2 index set (iff missing)
-is <- buildBowtie2IndexSet(fa, verbose=verbose)
+is <- buildBowtie2IndexSet(fa, verbose=-10)
 print(is)
 
 # In addition to SAM read group data inferred from the Illumina FASTQ
@@ -69,7 +38,7 @@ setSamReadGroup(fqs, SamReadGroup(LB="MPS-034"))
 alg <- Bowtie2Alignment(fqs, indexSet=is)
 print(alg)
 
-bams <- process(alg, verbose=verbose)
+bams <- process(alg, verbose=-10)
 print(bams)
 
 
@@ -103,8 +72,12 @@ print(ugp)
 bc <- TotalCnBinnedCounting(bams, targetUgp=ugp)
 print(bc)
 
-counts <- process(bc, verbose=verbose)
+counts <- process(bc, verbose=-10)
 verbose && print(verbose, counts)
+
+
+data <- extractMatrix(counts, column=1L)
+str(data)
 
 
 } # if (fullTest)
